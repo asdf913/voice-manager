dcacb3a9ab2e53f2d194763ca340398e5588c215
asdf913 <asdf913@gmail.com>
2022-07-05 12:27:32 +0900

    Update "VoiceManager" Add "com.j256.simplemagic:simplemagic" and "org.slf4j:slf4j-api" dependencies

--- voice-manager/src/main/java/VoiceManager.java
+++ voice-manager/src/main/java/VoiceManager.java
@@ -3,23 +3,36 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.text.JTextComponent;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.j256.simplemagic.ContentInfo;
+import com.j256.simplemagic.ContentInfoUtil;
 
 import net.miginfocom.swing.MigLayout;
 
 public class VoiceManager extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 6093437131552718994L;
+
+	private static Logger LOG = LoggerFactory.getLogger(VoiceManager.class);
+
+	private static Pattern PATTERN_CONTENT_INFO_MP3 = Pattern.compile("^MPEG ADTS, layer III.+$");
 
 	private static final String WRAP = "wrap";
 
@@ -74,6 +87,60 @@ public class VoiceManager extends JFrame implements ActionListener {
 				tfFile.setText(StringUtils.defaultString(selectedFile != null ? selectedFile.getAbsolutePath() : null,
 						tfFile.getText()));
 				//
+				if (selectedFile == null) {
+					//
+					JOptionPane.showMessageDialog(null, "No File Selected");
+					//
+				} else if (!selectedFile.exists()) {
+					//
+					JOptionPane.showMessageDialog(null,
+							String.format("File \"%1$s\" does not exist", selectedFile.getAbsolutePath()));
+					//
+				} else if (!selectedFile.isFile()) {
+					//
+					JOptionPane.showMessageDialog(null, "Not A Regular File Selected");
+					//
+				} else if (selectedFile.length() == 0) {
+					//
+					JOptionPane.showMessageDialog(null, "Empty File Selected");
+					//
+				} else {
+					//
+					ContentInfo ci = null;
+					//
+					try {
+						//
+						if ((ci = new ContentInfoUtil().findMatch(selectedFile)) == null) {
+							//
+							JOptionPane.showMessageDialog(null, "com.j256.simplemagic.ContentInfo is null");
+							//
+							return;
+							//
+						} else {
+							//
+							final Matcher matcher = PATTERN_CONTENT_INFO_MP3 != null
+									? PATTERN_CONTENT_INFO_MP3.matcher(ci.getMessage())
+									: null;
+							//
+							if (matcher == null || !matcher.matches()) {
+								JOptionPane.showMessageDialog(null, String.format("File \"%1$s\" is not a MP3 File",
+										selectedFile.getAbsolutePath()));
+							} // if
+								//
+						} // if
+							//
+					} catch (IOException e) {
+						//
+						if (LOG != null) {
+							LOG.error(e.getMessage(), e);
+						} else {
+							e.printStackTrace();
+						} // if
+							//
+					} // try
+						//
+				} // if
+					//
 			} // if
 				//
 		} // if
