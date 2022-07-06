a9e91edd6811fd9de0993eb84b58fd6381dc8f04
asdf913 <asdf913@gmail.com>
2022-07-06 18:44:18 +0900

    Add test case(s)

--- voice-manager/src/main/java/org/springframework/context/support/VoiceManager.java
+++ voice-manager/src/main/java/org/springframework/context/support/VoiceManager.java
@@ -5,7 +5,9 @@ import java.awt.Dimension;
 import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
 import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -326,18 +328,20 @@ public class VoiceManager extends JFrame implements ActionListener, EnvironmentA
 			//
 		} else if (Objects.equals(source, btnCopyRomaji)) {
 			//
-			final Toolkit toolkit = Toolkit.getDefaultToolkit();
+			setContents(getSystemClipboard(Toolkit.getDefaultToolkit()), new StringSelection(getText(tfRomaji)), null);
 			//
-			final Clipboard clipboard = toolkit != null ? toolkit.getSystemClipboard() : null;
-			//
-			if (clipboard != null) {
-				//
-				clipboard.setContents(new StringSelection(getText(tfRomaji)), null);
-				//
-			} // if
-				//
 		} // if
 			//
+	}
+
+	private static Clipboard getSystemClipboard(final Toolkit instance) {
+		return instance != null ? instance.getSystemClipboard() : null;
+	}
+
+	private static void setContents(final Clipboard instance, final Transferable contents, final ClipboardOwner owner) {
+		if (instance != null) {
+			instance.setContents(contents, owner);
+		}
 	}
 
 	private static void setText(final JTextComponent instance, final String text) {
--- voice-manager/src/test/java/org/springframework/context/support/VoiceManagerTest.java
+++ voice-manager/src/test/java/org/springframework/context/support/VoiceManagerTest.java
@@ -1,9 +1,20 @@
 package org.springframework.context.support;
 
+import java.awt.GraphicsEnvironment;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
+import java.awt.datatransfer.Transferable;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
+
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,12 +24,19 @@ import net.miginfocom.swing.MigLayout;
 
 class VoiceManagerTest {
 
-	private static Method METHOD_INIT = null;
+	private static Method METHOD_INIT, METHOD_GET_SYSTEM_CLIP_BOARD, METHOD_SET_CONTENTS = null;
 
 	@BeforeAll
 	private static void beforeAll() throws NoSuchMethodException {
 		//
-		(METHOD_INIT = VoiceManager.class.getDeclaredMethod("init")).setAccessible(true);
+		final Class<?> clz = VoiceManager.class;
+		//
+		(METHOD_INIT = clz.getDeclaredMethod("init")).setAccessible(true);
+		//
+		(METHOD_GET_SYSTEM_CLIP_BOARD = clz.getDeclaredMethod("getSystemClipboard", Toolkit.class)).setAccessible(true);
+		//
+		(METHOD_SET_CONTENTS = clz.getDeclaredMethod("setContents", Clipboard.class, Transferable.class,
+				ClipboardOwner.class)).setAccessible(true);
 		//
 	}
 
@@ -35,24 +53,98 @@ class VoiceManagerTest {
 			//
 		} // if
 			//
-		instance = constructor != null ? constructor.newInstance() : null;
+		instance = constructor != null && !GraphicsEnvironment.isHeadless() ? constructor.newInstance() : null;
 		//
+	}
+
+	@Test
+	void testActionPerformed() throws IllegalAccessException {
+		//
+		Assertions.assertDoesNotThrow(() -> actionPerformed(instance, new ActionEvent("", 0, null)));
+		//
+		final AbstractButton btnConvertToRomaji = new JButton();
+		//
+		if (instance != null) {
+			//
+			FieldUtils.writeDeclaredField(instance, "btnConvertToRomaji", btnConvertToRomaji, true);
+			//
+		} // if
+			//
+		Assertions.assertDoesNotThrow(() -> actionPerformed(instance, new ActionEvent(btnConvertToRomaji, 0, null)));
+		//
+		final AbstractButton btnCopyRomaji = new JButton();
+		//
+		if (instance != null) {
+			//
+			FieldUtils.writeDeclaredField(instance, "btnCopyRomaji", btnCopyRomaji, true);
+			//
+		} // if
+			//
+		Assertions.assertDoesNotThrow(() -> actionPerformed(instance, new ActionEvent(btnCopyRomaji, 0, null)));
+		//
+	}
+
+	private static void actionPerformed(final ActionListener instance, final ActionEvent actionEvent) {
+		if (instance != null) {
+			instance.actionPerformed(actionEvent);
+		} // if
 	}
 
 	@Test
 	void testInit() {
 		//
-		Assertions.assertThrows(IllegalArgumentException.class, () -> init());
-		//
-		instance.setLayout(new MigLayout());
-		//
-		Assertions.assertDoesNotThrow(() -> init());
-		//
+		if (instance != null) {
+			//
+			Assertions.assertThrows(IllegalArgumentException.class, () -> init());
+			//
+			instance.setLayout(new MigLayout());
+			//
+			Assertions.assertDoesNotThrow(() -> init());
+			//
+		} // if
+			//
 	}
 
 	private void init() throws Throwable {
 		try {
 			METHOD_INIT.invoke(instance);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testGetSystemClipboard() throws Throwable {
+		//
+		Assertions.assertNull(getSystemClipboard(null));
+		//
+	}
+
+	private static Clipboard getSystemClipboard(final Toolkit instance) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_SYSTEM_CLIP_BOARD.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Clipboard) {
+				return (Clipboard) obj;
+			}
+			throw new Throwable(obj.getClass() != null ? obj.getClass().toString() : null);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testSetContents() {
+		//
+		Assertions.assertDoesNotThrow(() -> setContents(null, null, null));
+		//
+	}
+
+	private static void setContents(final Clipboard instance, final Transferable contents, final ClipboardOwner owner)
+			throws Throwable {
+		try {
+			METHOD_SET_CONTENTS.invoke(null, instance, contents, owner);
 		} catch (final InvocationTargetException e) {
 			throw e.getTargetException();
 		}
