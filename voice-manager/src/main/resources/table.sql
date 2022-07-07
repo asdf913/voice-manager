9e961a0993dc2c833c928ef01be8dde62394aa1e
asdf913 <asdf913@gmail.com>
2022-07-07 09:46:23 +0900

    Update "table.sql"

--- voice-manager/src/main/resources/table.sql
+++ voice-manager/src/main/resources/table.sql
@@ -1,17 +1,13 @@
 CREATE TABLE IF NOT EXISTS voice(
 	 text                  text
 	,romaji                text
+	,hiragana              text
+	,katakana              text
 	,file_path             text
+	,file_extension        text
 	,file_length           integer
 	,file_digest_algorithm text
 	,file_digest           text
 	,create_ts			   TIMESTAMP WITH TIME ZONE
+	,update_ts			   TIMESTAMP WITH TIME ZONE
 );
-
-ALTER TABLE voice ADD COLUMN IF NOT EXISTS update_ts TIMESTAMP WITH TIME ZONE;
-
-ALTER TABLE voice ADD COLUMN IF NOT EXISTS file_extension text;
-
-ALTER TABLE voice ADD COLUMN IF NOT EXISTS hiragana text;
-
-ALTER TABLE voice ADD COLUMN IF NOT EXISTS katakana text;
