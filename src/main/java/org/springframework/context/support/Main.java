691f32675c3ea016738ec38e5613765bbded575d
asdf913 <asdf913@gmail.com>
2022-07-11 13:00:12 +0900

    Add "org.springframework.context.support.Main.getInstance(org.springframework.beans.factory.ListableBeanFactory,java.lang.Class,java.util.function.Consumer)" method

--- src/main/java/org/springframework/context/support/Main.java
+++ src/main/java/org/springframework/context/support/Main.java
@@ -1,6 +1,17 @@
 package org.springframework.context.support;
 
+import java.awt.Component;
+import java.awt.Window;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import javax.swing.JOptionPane;
+
+import org.apache.commons.collections4.IterableUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.PropertyResolver;
 
 public class Main {
 
@@ -12,18 +23,90 @@ public class Main {
 		try (final ConfigurableApplicationContext beanFactory = new ClassPathXmlApplicationContext(
 				"applicationContext.xml")) {
 			//
-			final VoiceManager instance = beanFactory.getBean(VoiceManager.class);
+			final PropertyResolver environment = beanFactory.getEnvironment();
 			//
-			if (instance != null) {
+			final Class<?> clz = forName(
+					environment != null ? environment.getProperty("org.springframework.context.support.Main.class")
+							: null);
+			//
+			if (clz == null) {
 				//
-				instance.pack();
+				JOptionPane.showMessageDialog(null, "java.lang.Class is null");
 				//
-				instance.setVisible(true);
+				return;
 				//
 			} // if
 				//
-		}
+			final Object instance = getInstance(beanFactory, clz, x -> JOptionPane.showMessageDialog(null, x));
+			//
+			if (instance instanceof Window) {
+				//
+				((Window) instance).pack();
+				//
+			} // if
+				//
+			if (instance instanceof Component) {
+				//
+				((Component) instance).setVisible(true);
+				//
+			} // if
+				//
+		} // try
+			//
+	}
 
+	private static Class<?> forName(final String className) {
+		try {
+			return StringUtils.isNotBlank(className) ? Class.forName(className) : null;
+		} catch (final ClassNotFoundException e) {
+			return null;
+		}
+	}
+
+	private static Object getInstance(final ListableBeanFactory beanFactory, final Class<?> clz,
+			final Consumer<String> consumer) {
+		//
+		if (clz == null) {
+			//
+			accept(consumer, "java.lang.Class is null");
+			//
+			return null;
+			//
+		} // if
+			//
+		final Map<?, ?> beans = beanFactory != null ? beanFactory.getBeansOfType(clz) : null;
+		//
+		if (beans == null) {
+			//
+			accept(consumer, String.format(
+					"org.springframework.beans.factory.ListableBeanFactory.getBeansOfType(%1$s) return null", clz));
+			//
+			return null;
+			//
+		} else if (beans.isEmpty()) {
+			//
+			accept(consumer, String.format(
+					"org.springframework.beans.factory.ListableBeanFactory.getBeansOfType(%1$s) return empty", clz));
+			//
+			return null;
+			//
+		} else if (beans.size() > 1) {
+			//
+			accept(consumer, String.format(
+					"org.springframework.beans.factory.ListableBeanFactory.getBeansOfType(%1$s).size()>1", clz));
+			//
+			return null;
+			//
+		} // if
+			//
+		return IterableUtils.first(beans.values());
+		//
+	}
+
+	private static <T> void accept(final Consumer<T> instance, final T value) {
+		if (instance != null) {
+			instance.accept(value);
+		}
 	}
 
 }
---
+++ src/test/java/org/springframework/context/support/MainTest.java
@@ -0,0 +1,128 @@
+package org.springframework.context.support;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ListableBeanFactory;
+
+import com.google.common.reflect.Reflection;
+
+class MainTest {
+
+	private static Method METHOD_FOR_NAME, METHOD_GET_INSTANCE = null;
+
+	private class IH implements InvocationHandler {
+
+		private Map<Object, Object> beansOfType = null;
+
+		@Override
+		public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+			//
+			final String methodName = method != null ? method.getName() : null;
+			//
+			if (proxy instanceof ListableBeanFactory) {
+				//
+				if (Objects.equals(methodName, "getBeansOfType") && args != null && args.length > 0) {
+					//
+					return beansOfType;
+					//
+				} // if
+					//
+			} // if
+				//
+			throw new Throwable(methodName);
+			//
+		}
+
+	}
+
+	@BeforeAll
+	private static void beforeAll() throws ReflectiveOperationException {
+		//
+		final Class<?> clz = Main.class;
+		//
+		(METHOD_FOR_NAME = clz.getDeclaredMethod("forName", String.class)).setAccessible(true);
+		//
+		(METHOD_GET_INSTANCE = clz.getDeclaredMethod("getInstance", ListableBeanFactory.class, Class.class,
+				Consumer.class)).setAccessible(true);
+		//
+	}
+
+	@Test
+	void testForName() throws Throwable {
+		//
+		Assertions.assertNull(forName(null));
+		//
+		Assertions.assertNull(forName(""));
+		//
+		Assertions.assertNull(forName(" "));
+		//
+		Assertions.assertNull(forName("A"));
+		//
+		Assertions.assertSame(Object.class, forName("java.lang.Object"));
+		//
+	}
+
+	private static Class<?> forName(final String className) throws Throwable {
+		try {
+			final Object obj = METHOD_FOR_NAME.invoke(null, className);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Class) {
+				return (Class<?>) obj;
+			}
+			throw new Throwable(obj.getClass() != null ? obj.getClass().toString() : null);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testGetInstance() throws Throwable {
+		//
+		Assertions.assertNull(getInstance(null, null, null));
+		//
+		Assertions.assertNull(getInstance(null, null, x -> {
+		}));
+		//
+		Assertions.assertNull(getInstance(null, Object.class, null));
+		//
+		final IH ih = new IH();
+		//
+		final ListableBeanFactory beanFactory = Reflection.newProxy(ListableBeanFactory.class, ih);
+		//
+		Assertions.assertNull(getInstance(beanFactory, Object.class, null));
+		//
+		ih.beansOfType = Collections.emptyMap();
+		//
+		Assertions.assertNull(getInstance(beanFactory, Object.class, null));
+		//
+		ih.beansOfType = new LinkedHashMap<>(Collections.singletonMap(null, null));
+		//
+		Assertions.assertNull(getInstance(beanFactory, Object.class, null));
+		//
+		ih.beansOfType.put("", "");
+		//
+		Assertions.assertNull(getInstance(beanFactory, Object.class, null));
+		//
+	}
+
+	private static Object getInstance(final ListableBeanFactory beanFactory, final Class<?> clz,
+			final Consumer<String> consumer) throws Throwable {
+		try {
+			return METHOD_GET_INSTANCE.invoke(null, beanFactory, clz, consumer);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+}
