a9be1ebb12334305e64a463d330da2d8e7b04bc6
asdf913 <asdf913@gmail.com>
2022-07-19 08:13:16 +0900

    Add "org.slf4j.LoggerUtil" class

---
+++ src/main/java/org/slf4j/LoggerUtil.java
@@ -0,0 +1,117 @@
+package org.slf4j;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import org.apache.bcel.classfile.ClassParser;
+import org.apache.bcel.classfile.Code;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.classfile.Method;
+import org.apache.bcel.classfile.Utility;
+import org.apache.bcel.generic.Type;
+import org.apache.commons.lang3.StringUtils;
+
+public class LoggerUtil {
+
+	private static final Pattern PATTERN = Pattern.compile("^<(cl)?init>$");
+
+	private LoggerUtil() {
+	}
+
+	public static boolean isNOPLogger(final Logger instance) {
+		//
+		final Class<?> clz = getClass(instance);
+		//
+		JavaClass javaClass = null;
+		//
+		Method[] ms = null;
+		//
+		try (final InputStream is = clz != null
+				? clz.getResourceAsStream(String.format("/%1$s.class", StringUtils.replace(clz.getName(), ".", "/")))
+				: null) {
+			//
+			ms = (javaClass = parse(testAndApply(Objects::nonNull, is, x -> new ClassParser(x, null), null))) != null
+					? javaClass.getMethods()
+					: null;
+			//
+		} catch (final IOException e) {
+			//
+			e.printStackTrace();
+			//
+		} // try
+			//
+		return isEmpty(toList(filter(testAndApply(Objects::nonNull, ms, Arrays::stream, null), m -> {
+			//
+			if (m == null || !Objects.equals(Type.VOID, m.getReturnType()) || matches(matcher(PATTERN, m.getName()))) {
+				//
+				return false;
+				//
+			} // if
+				//
+			final byte[] bs = getCode(m.getCode());
+			//
+			return !Objects.equals(
+					StringUtils.trim(Utility.codeToString(bs, m.getConstantPool(), 0, bs != null ? bs.length : 0)),
+					"0:    return");
+			//
+		})));
+		//
+	}
+
+	private static byte[] getCode(final Code instance) {
+		return instance != null ? instance.getCode() : null;
+	}
+
+	private static Matcher matcher(final Pattern pattern, final CharSequence input) {
+		return pattern != null && input != null ? pattern.matcher(input) : null;
+	}
+
+	private static boolean matches(final Matcher instance) {
+		return instance != null && instance.matches();
+	}
+
+	private static boolean isEmpty(final Collection<?> instance) {
+		return instance != null && instance.isEmpty();
+	}
+
+	private static <T> List<T> toList(final Stream<T> instance) {
+		return instance != null ? instance.toList() : null;
+
+	}
+
+	private static <T> Stream<T> filter(final Stream<T> instance, final Predicate<? super T> predicate) {
+		//
+		return instance != null && (predicate != null || Proxy.isProxyClass(getClass(instance)))
+				? instance.filter(predicate)
+				: null;
+		//
+	}
+
+	private static Class<?> getClass(final Object instance) {
+		return instance != null ? instance.getClass() : null;
+	}
+
+	private static JavaClass parse(final ClassParser instance) throws IOException {
+		return instance != null ? instance.parse() : null;
+	}
+
+	private static <T, R> R testAndApply(final Predicate<T> predicate, final T value, final Function<T, R> functionTrue,
+			final Function<T, R> functionFalse) {
+		return predicate != null && predicate.test(value) ? apply(functionTrue, value) : apply(functionFalse, value);
+	}
+
+	private static <T, R> R apply(final Function<T, R> instance, final T value) {
+		return instance != null ? instance.apply(value) : null;
+	}
+
+}
---
+++ src/test/java/org/slf4j/LoggerUtilTest.java
@@ -0,0 +1,295 @@
+package org.slf4j;
+
+import java.io.InputStream;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import org.apache.bcel.classfile.ClassParser;
+import org.apache.bcel.classfile.Code;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.google.common.base.Predicates;
+import com.google.common.reflect.Reflection;
+
+class LoggerUtilTest {
+
+	private static Method METHOD_GET_CODE, METHOD_MATCHER, METHOD_MATCHES, METHOD_TO_LIST, METHOD_IS_EMPTY,
+			METHOD_FILTER, METHOD_PARSE, METHOD_TEST_AND_APPLY = null;
+
+	@BeforeAll
+	private static void beforeAll() throws NoSuchMethodException {
+		//
+		final Class<?> clz = LoggerUtil.class;
+		//
+		(METHOD_GET_CODE = clz.getDeclaredMethod("getCode", Code.class)).setAccessible(true);
+		//
+		(METHOD_MATCHER = clz.getDeclaredMethod("matcher", Pattern.class, CharSequence.class)).setAccessible(true);
+		//
+		(METHOD_MATCHES = clz.getDeclaredMethod("matches", Matcher.class)).setAccessible(true);
+		//
+		(METHOD_TO_LIST = clz.getDeclaredMethod("toList", Stream.class)).setAccessible(true);
+		//
+		(METHOD_IS_EMPTY = clz.getDeclaredMethod("isEmpty", Collection.class)).setAccessible(true);
+		//
+		(METHOD_FILTER = clz.getDeclaredMethod("filter", Stream.class, Predicate.class)).setAccessible(true);
+		//
+		(METHOD_PARSE = clz.getDeclaredMethod("parse", ClassParser.class)).setAccessible(true);
+		//
+		(METHOD_TEST_AND_APPLY = clz.getDeclaredMethod("testAndApply", Predicate.class, Object.class, Function.class,
+				Function.class)).setAccessible(true);
+		//
+	}
+
+	private class IH implements InvocationHandler {
+
+		private List<?> toList = null;
+
+		@Override
+		public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+			//
+			final String methodName = method != null ? method.getName() : null;
+			//
+			if (proxy instanceof Stream) {
+				//
+				if (Objects.equals(methodName, "filter")) {
+					//
+					return proxy;
+					//
+				} else if (Objects.equals(methodName, "toList")) {
+					//
+					return toList;
+					//
+				} // if
+					//
+			} // if
+				//
+			throw new Throwable(methodName);
+			//
+		}
+
+	}
+
+	private Stream<?> stream = null;
+
+	@BeforeEach
+	private void beforeEach() {
+		//
+		stream = Reflection.newProxy(Stream.class, new IH());
+		//
+	}
+
+	@Test
+	void testIsNOPLogger() {
+		//
+		Assertions.assertFalse(LoggerUtil.isNOPLogger(null));
+		//
+	}
+
+	@Test
+	void testGetCode() throws Throwable {
+		//
+		Assertions.assertNull(getCode(null));
+		//
+	}
+
+	private static byte[] getCode(final Code instance) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_CODE.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof byte[]) {
+				return (byte[]) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	private static Class<?> getClass(final Object instance) {
+		return instance != null ? instance.getClass() : null;
+	}
+
+	private static String toString(final Object instance) {
+		return instance != null ? instance.toString() : null;
+	}
+
+	@Test
+	void testMatcher() throws Throwable {
+		//
+		Assertions.assertNull(matcher(null, null));
+		//
+	}
+
+	private static Matcher matcher(final Pattern pattern, final CharSequence input) throws Throwable {
+		try {
+			final Object obj = METHOD_MATCHER.invoke(null, pattern, input);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Matcher) {
+				return (Matcher) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testMatches() throws Throwable {
+		//
+		Assertions.assertFalse(matches(null));
+		//
+		final Pattern pattern = Pattern.compile("\\d+");
+		//
+		Assertions.assertFalse(matches(matcher(pattern, null)));
+		//
+		Assertions.assertFalse(matches(matcher(pattern, "")));
+		//
+		Assertions.assertTrue(matches(matcher(pattern, "1")));
+		//
+	}
+
+	private static boolean matches(final Matcher instance) throws Throwable {
+		try {
+			final Object obj = METHOD_MATCHES.invoke(null, instance);
+			if (obj instanceof Boolean) {
+				return ((Boolean) obj).booleanValue();
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testIsEmpty() throws Throwable {
+		//
+		Assertions.assertTrue(isEmpty(Collections.emptySet()));
+		//
+		Assertions.assertFalse(isEmpty(Collections.singleton(null)));
+		//
+	}
+
+	private static boolean isEmpty(final Collection<?> instance) throws Throwable {
+		try {
+			final Object obj = METHOD_IS_EMPTY.invoke(null, instance);
+			if (obj instanceof Boolean) {
+				return ((Boolean) obj).booleanValue();
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testToList() throws Throwable {
+		//
+		Assertions.assertNull(toList(stream));
+		//
+	}
+
+	private static <T> List<T> toList(final Stream<T> instance) throws Throwable {
+		try {
+			final Object obj = METHOD_TO_LIST.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof List) {
+				return (List) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testFilter() throws Throwable {
+		//
+		Assertions.assertNull(filter(null, null));
+		//
+		Assertions.assertNotNull(filter(stream, null));
+		//
+		Assertions.assertNull(filter(Stream.empty(), null));
+		//
+		Assertions.assertNotNull(filter(Stream.empty(), Predicates.alwaysTrue()));
+		//
+	}
+
+	private static <T> Stream<T> filter(final Stream<T> instance, final Predicate<? super T> predicate)
+			throws Throwable {
+		try {
+			final Object obj = METHOD_FILTER.invoke(null, instance, predicate);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Stream) {
+				return (Stream) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testParse() throws Throwable {
+		//
+		final Class<?> clz = String.class;
+		//
+		try (final InputStream is = clz != null
+				? clz.getResourceAsStream(String.format("/%1$s.class", StringUtils.replace(clz.getName(), ".", "/")))
+				: null) {
+			//
+			Assertions.assertNotNull(parse(testAndApply(Objects::nonNull, is, x -> new ClassParser(x, null), null)));
+			//
+		} // try
+			//
+	}
+
+	private static JavaClass parse(final ClassParser instance) throws Throwable {
+		try {
+			final Object obj = METHOD_PARSE.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof JavaClass) {
+				return (JavaClass) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testTestAndApply() throws Throwable {
+		//
+		Assertions.assertNull(testAndApply(null, null, null, null));
+		//
+	}
+
+	private static <T, R> R testAndApply(final Predicate<T> predicate, final T value, final Function<T, R> functionTrue,
+			final Function<T, R> functionFalse) throws Throwable {
+		try {
+			return (R) METHOD_TEST_AND_APPLY.invoke(null, predicate, value, functionTrue, functionFalse);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+}
