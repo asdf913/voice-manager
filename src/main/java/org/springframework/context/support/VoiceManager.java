281417c8dce6519d951957eef9788db591ed600d
asdf913 <asdf913@gmail.com>
2022-11-26 10:03:38 +0900

    Add "Help" tab to show "help.html" by "javax.swing.JEditorPane" class

--- src/main/java/org/springframework/context/support/VoiceManager.java
+++ src/main/java/org/springframework/context/support/VoiceManager.java
@@ -113,6 +113,7 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
+import javax.swing.JEditorPane;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -131,6 +132,7 @@ import javax.swing.MutableComboBoxModel;
 import javax.swing.WindowConstants;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.event.HyperlinkEvent.EventType;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.text.JTextComponent;
 import javax.xml.namespace.QName;
@@ -978,9 +980,7 @@ public class VoiceManager extends JFrame implements ActionListener, ItemListener
 					//
 			});
 			//
-		} else
-
-		{
+		} else {
 			//
 			(jPanelWarning = new JPanel()).setBorder(BorderFactory.createTitledBorder(WARNING));
 			//
@@ -1007,6 +1007,51 @@ public class VoiceManager extends JFrame implements ActionListener, ItemListener
 		jTabbedPane.addTab("Export", createExportPanel(cloneLayoutManager()));
 		//
 		jTabbedPane.addTab("Misc", createMiscellaneousPanel(cloneLayoutManager(), voiceIds));
+		//
+		// help
+		//
+		JEditorPane jep = null;
+		//
+		try {
+			//
+			setEditable(false, jep = testAndApply(Objects::nonNull, VoiceManager.class.getResource("/help.html"),
+					JEditorPane::new, x -> new JEditorPane()));
+			//
+		} catch (final IOException e) {
+			//
+			errorOrPrintStackTraceOrShowMessageDialog(e);
+			//
+		} // try
+			//
+		if (jep != null) {
+			//
+			jep.addHyperlinkListener(x -> {
+				//
+				try {
+					//
+					if (Objects.equals(EventType.ACTIVATED, x != null ? x.getEventType() : null)) {
+						//
+						browse(Desktop.getDesktop(), x != null ? x.getURL().toURI() : null);
+						//
+					} // if
+						//
+				} catch (final IOException | URISyntaxException e) {
+					//
+					errorOrPrintStackTraceOrShowMessageDialog(e);
+					//
+				} // try
+					//
+			});
+			//
+		} // if
+			//
+		final JScrollPane jsp = new JScrollPane(jep);
+		//
+		final JPanel jPanel = new JPanel();
+		//
+		jPanel.add(jsp);
+		//
+		jTabbedPane.addTab("Help", jPanel);
 		//
 		try {
 			//
