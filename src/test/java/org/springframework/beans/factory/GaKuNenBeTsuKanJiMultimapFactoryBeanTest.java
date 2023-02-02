babaf9516124e761e23b3e16cd0be9d7cc9a5902
asdf913 <asdf913@gmail.com>
2023-02-02 13:55:43 +0900

    Add "toString(java.lang.Object)" and "valueOf(java.lang.String)" method(s) in "org.springframework.beans.factory.GaKuNenBeTsuKanJiMultimapFactoryBean" class Change "setTimeout(java.time.Duration)" method to "setTimeout(java.lang.Object)" method  in "org.springframework.beans.factory.GaKuNenBeTsuKanJiMultimapFactoryBean" class

--- src/test/java/org/springframework/beans/factory/GaKuNenBeTsuKanJiMultimapFactoryBeanTest.java
+++ src/test/java/org/springframework/beans/factory/GaKuNenBeTsuKanJiMultimapFactoryBeanTest.java
@@ -1,8 +1,10 @@
 package org.springframework.beans.factory;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
+import java.time.Duration;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -13,13 +15,16 @@ import com.google.common.collect.Multimap;
 
 class GaKuNenBeTsuKanJiMultimapFactoryBeanTest {
 
-	private static Method METHOD_GET_CLASS = null;
+	private static Method METHOD_GET_CLASS, METHOD_TO_STRING = null;
 
 	@BeforeAll
 	static void beforeAll() throws ReflectiveOperationException {
 		//
-		(METHOD_GET_CLASS = GaKuNenBeTsuKanJiMultimapFactoryBean.class.getDeclaredMethod("getClass", Object.class))
-				.setAccessible(true);
+		final Class<?> clz = GaKuNenBeTsuKanJiMultimapFactoryBean.class;
+		//
+		(METHOD_GET_CLASS = clz.getDeclaredMethod("getClass", Object.class)).setAccessible(true);
+		//
+		(METHOD_TO_STRING = clz.getDeclaredMethod("toString", Object.class)).setAccessible(true);
 		//
 	}
 
@@ -54,8 +59,136 @@ class GaKuNenBeTsuKanJiMultimapFactoryBeanTest {
 		//
 	}
 
-	private static String toString(final Object instance) {
-		return instance != null ? instance.toString() : null;
+	@Test
+	void testSetTimeout() throws NoSuchFieldException, IllegalAccessException {
+		//
+		final Field timeout = GaKuNenBeTsuKanJiMultimapFactoryBean.class.getDeclaredField("timeout");
+		//
+		if (timeout != null) {
+			//
+			timeout.setAccessible(true);
+			//
+		} // if
+			//
+			// null
+			//
+		Assertions.assertDoesNotThrow(() -> {
+			//
+			if (instance != null) {
+				//
+				instance.setTimeout(null);
+				//
+			} // if
+				//
+		});
+		//
+		Assertions.assertNull(get(timeout, instance));
+		//
+		// java.time.Duration
+		//
+		final Duration duration = Duration.ZERO;
+		//
+		Assertions.assertDoesNotThrow(() -> {
+			//
+			if (instance != null) {
+				//
+				instance.setTimeout(duration);
+				//
+			} // if
+				//
+		});
+		//
+		Assertions.assertSame(duration, get(timeout, instance));
+		//
+		// java.lang.Number
+		//
+		final long l = 1;
+		//
+		Assertions.assertDoesNotThrow(() -> {
+			//
+			if (instance != null) {
+				//
+				instance.setTimeout(Long.valueOf(l));
+				//
+			} // if
+				//
+		});
+		//
+		Assertions.assertDoesNotThrow(() -> {
+			//
+			if (instance != null) {
+				//
+				instance.setTimeout(Long.toString(l));
+				//
+			} // if
+				//
+		});
+		//
+		Assertions.assertEquals(Duration.ofMillis(l), get(timeout, instance));
+		//
+		// java.lang.String
+		//
+		Assertions.assertDoesNotThrow(() -> {
+			//
+			if (instance != null) {
+				//
+				instance.setTimeout("");
+				//
+			} // if
+				//
+		});
+		//
+		Assertions.assertEquals(Duration.ofMillis(l), get(timeout, instance));
+		//
+		Assertions.assertDoesNotThrow(() -> {
+			//
+			if (instance != null) {
+				//
+				instance.setTimeout(" ");
+				//
+			} // if
+				//
+		});
+		//
+		Assertions.assertEquals(Duration.ofMillis(l), get(timeout, instance));
+		//
+		Assertions.assertDoesNotThrow(() -> {
+			//
+			if (instance != null) {
+				//
+				instance.setTimeout(String.format("PT%1$sS", l));
+				//
+			} // if
+				//
+		});
+		//
+		Assertions.assertEquals(Duration.ofMillis(l * 1000), get(timeout, instance));
+		//
+	}
+
+	private static Object get(final Field field, final Object instance) throws IllegalAccessException {
+		return field != null ? field.get(instance) : null;
+	}
+
+	@Test
+	void testToString() throws Throwable {
+		//
+		Assertions.assertNull(toString(null));
+		//
+	}
+
+	private static String toString(final Object instance) throws Throwable {
+		try {
+			final Object obj = METHOD_TO_STRING.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof String) {
+				return (String) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
 	}
 
 	@Test
