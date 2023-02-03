c1dc5c3484dde95a08fdb248db9e79b773b877e5
asdf913 <asdf913@gmail.com>
2023-02-03 11:16:06 +0900

    Update test case(s)

--- src/test/java/org/apache/poi/ss/usermodel/SheetUtilTest.java
+++ src/test/java/org/apache/poi/ss/usermodel/SheetUtilTest.java
@@ -3,7 +3,7 @@ package org.apache.poi.ss.usermodel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class SheetUtilTest {
+class SheetUtilTest {
 
 	@Test
 	void testCreateRow() {
