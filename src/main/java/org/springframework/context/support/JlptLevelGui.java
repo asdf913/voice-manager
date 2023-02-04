bca20f24875ab468495e829057d714c9c5dc16d7
asdf913 <asdf913@gmail.com>
2023-02-04 12:25:59 +0900

    Add "org.springframework.context.support.JlptLevelGui" class

---
+++ src/main/java/org/springframework/context/support/JlptLevelGui.java
@@ -0,0 +1,70 @@
+package org.springframework.context.support;
+
+import java.lang.reflect.Proxy;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.function.FailableFunctionUtil;
+import org.springframework.beans.factory.InitializingBean;
+
+import net.miginfocom.swing.MigLayout;
+
+public class JlptLevelGui extends JFrame implements InitializingBean {
+
+	private static final long serialVersionUID = 1466869508176258464L;
+
+	private List<String> jlptLevels = null;
+
+	private JlptLevelGui() {
+	}
+
+	public void setJlptLevels(final List<String> jlptLevels) {
+		this.jlptLevels = jlptLevels;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		//
+		if (!(getLayout() instanceof MigLayout)) {
+			//
+			setLayout(new MigLayout());
+			//
+		} // if
+			//
+		add(new JLabel("JLPT Level(s)"));
+		//
+		add(new JList<String>(testAndApply(Objects::nonNull, toArray(jlptLevels, new String[] {}),
+				DefaultComboBoxModel::new, x -> new DefaultComboBoxModel<String>())));
+		//
+	}
+
+	private static <T> T[] toArray(final Collection<T> instance, final T[] array) {
+		//
+		return instance != null && (array != null || Proxy.isProxyClass(getClass(instance))) ? instance.toArray(array)
+				: null;
+		//
+	}
+
+	private static Class<?> getClass(final Object instance) {
+		return instance != null ? instance.getClass() : null;
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse) throws E {
+		return test(predicate, value) ? FailableFunctionUtil.apply(functionTrue, value)
+				: FailableFunctionUtil.apply(functionFalse, value);
+	}
+
+	private static final <T> boolean test(final Predicate<T> instance, final T value) {
+		return instance != null && instance.test(value);
+	}
+
+}
---
+++ src/test/java/org/springframework/context/support/JlptLevelGuiTest.java
@@ -0,0 +1,176 @@
+package org.springframework.context.support;
+
+import java.awt.GraphicsEnvironment;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.InitializingBean;
+
+import com.google.common.reflect.Reflection;
+
+class JlptLevelGuiTest {
+
+	private static Method METHOD_TO_ARRAY, METHOD_GET_CLASS, METHOD_TEST = null;
+
+	@BeforeAll
+	static void beforeAll() throws ReflectiveOperationException {
+		//
+		final Class<?> clz = JlptLevelGui.class;
+		//
+		(METHOD_TO_ARRAY = clz.getDeclaredMethod("toArray", Collection.class, Object[].class)).setAccessible(true);
+		//
+		(METHOD_GET_CLASS = clz.getDeclaredMethod("getClass", Object.class)).setAccessible(true);
+		//
+		(METHOD_TEST = clz.getDeclaredMethod("test", Predicate.class, Object.class)).setAccessible(true);
+		//
+	}
+
+	private class IH implements InvocationHandler {
+
+		private Object[] array = null;
+
+		@Override
+		public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+			//
+			if (Objects.equals(method != null ? method.getReturnType() : null, Void.TYPE)) {
+				//
+				return null;
+				//
+			} // if
+				//
+			final String methodName = method != null ? method.getName() : null;
+			//
+			if (proxy instanceof Collection) {
+				//
+				if (Objects.equals(methodName, "toArray")) {
+					//
+					return array;
+//					/
+				} // if
+					//
+			} // if
+				//
+			throw new Throwable(method != null ? method.getName() : null);
+			//
+		}
+
+	}
+
+	private JlptLevelGui instance = null;
+
+	@BeforeEach
+	void beforeEach() throws ReflectiveOperationException {
+		//
+		if (!GraphicsEnvironment.isHeadless()) {
+			//
+			final Constructor<JlptLevelGui> constructor = JlptLevelGui.class.getDeclaredConstructor();
+			//
+			if (constructor != null) {
+				//
+				constructor.setAccessible(true);
+				//
+			} // if
+				//
+			instance = constructor != null ? constructor.newInstance() : null;
+			//
+		} // if
+			//
+	}
+
+	@Test
+	void testAfterPropertiesSet() {
+		//
+		Assertions.assertDoesNotThrow(() -> afterPropertiesSet(instance));
+		//
+		if (instance != null) {
+			//
+			instance.setJlptLevels(Collections.emptyList());
+			//
+		} // if
+			//
+		Assertions.assertDoesNotThrow(() -> afterPropertiesSet(instance));
+		//
+	}
+
+	private static void afterPropertiesSet(final InitializingBean instance) throws Exception {
+		if (instance != null) {
+			instance.afterPropertiesSet();
+		}
+	}
+
+	@Test
+	void testToArray() throws Throwable {
+		//
+		Assertions.assertNull(toArray(Reflection.newProxy(Collection.class, new IH()), null));
+		//
+		Assertions.assertNull(toArray(Collections.emptyList(), null));
+		//
+	}
+
+	private static <T> T[] toArray(final Collection<T> instance, final T[] array) throws Throwable {
+		try {
+			final Object obj = METHOD_TO_ARRAY.invoke(null, instance, array);
+			if (obj == null) {
+				return null;
+			}
+			return (T[]) obj;
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testGetClass() throws Throwable {
+		//
+		Assertions.assertNull(getClass(null));
+		//
+	}
+
+	private static Class<?> getClass(final Object instance) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_CLASS.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Class<?>) {
+				return (Class<?>) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	private static String toString(final Object instance) {
+		return instance != null ? instance.toString() : null;
+	}
+
+	@Test
+	void testTest() throws Throwable {
+		//
+		Assertions.assertFalse(test(null, null));
+		//
+	}
+
+	private static final <T> boolean test(final Predicate<T> instance, final T value) throws Throwable {
+		try {
+			final Object obj = METHOD_TEST.invoke(null, instance, value);
+			if (obj instanceof Boolean) {
+				return ((Boolean) obj).booleanValue();
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+}
