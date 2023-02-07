6252697270cc03f977d771d0ff79623696fda763
asdf913 <asdf913@gmail.com>
2023-02-07 23:06:29 +0900

    Remove unused import statement(s) in test case(s)

--- src/test/java/org/springframework/beans/factory/JlptVocabularyListFactoryBeanTest.java
+++ src/test/java/org/springframework/beans/factory/JlptVocabularyListFactoryBeanTest.java
@@ -7,7 +7,6 @@ import java.io.InputStream;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
