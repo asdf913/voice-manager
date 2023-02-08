cf554af0361f016a1ea7f7a22f8f4a2254015deb
asdf913 <asdf913@gmail.com>
2023-02-08 13:34:43 +0900

    Apply rule "Multiple variables should not be declared on the same line"

--- src/main/java/domain/JlptVocabulary.java
+++ src/main/java/domain/JlptVocabulary.java
@@ -1,8 +1,22 @@
 package domain;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 public class JlptVocabulary {
 
-	private String level, kanji, kana = null;
+	@Target(ElementType.FIELD)
+	@Retention(RetentionPolicy.RUNTIME)
+	private @interface Note {
+		String value();
+	}
+
+	@Note("level")
+	private String level = null;
+
+	private String kanji, kana = null;
 
 	public String getLevel() {
 		return level;
