0972607b1e561787b837f94d2c8d63851ea93203
asdf913 <asdf913@gmail.com>
2023-02-14 20:03:08 +0900

    Add "org.springframework.beans.config.CustomBeanPostProcessor.defaultCloseOperation" property in "configuration.properties" (Default value is "EXIT_ON_CLOSE") Update "org.springframework.beans.config.CustomBeanPostProcessor.setDefaultCloseOperation(java.lang.Object)" method

--- src/main/java/org/springframework/beans/config/CustomBeanPostProcessor.java
+++ src/main/java/org/springframework/beans/config/CustomBeanPostProcessor.java
@@ -1,20 +1,151 @@
 package org.springframework.beans.config;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 import javax.swing.JFrame;
 
+import org.apache.commons.collections4.IterableUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.javatuples.Unit;
+import org.javatuples.valueintf.IValue0;
+import org.javatuples.valueintf.IValue0Util;
+import org.oxbow.swingbits.dialog.task.TaskDialogsUtil;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapperUtil;
+
 public class CustomBeanPostProcessor implements BeanPostProcessor {
 
-	private Integer defaultCloseOperation = null;
+	/**
+	 * @see javax.swing.JFrame#setDefaultCloseOperation(int)
+	 */
+	private Number defaultCloseOperation = null;
 
-	public void setDefaultCloseOperation(final Integer defaultCloseOperation) {
+	public void setDefaultCloseOperation(final Object object) {
 		//
-		// TODO
+		IValue0<Number> value = null;
 		//
-		this.defaultCloseOperation = defaultCloseOperation;
+		IValue0<Exception> exception = null;
 		//
+		if (object == null) {
+			//
+			value = Unit.with(null);
+			//
+		} else if (object instanceof Number) {
+			//
+			value = Unit.with(Integer.valueOf(((Number) object).intValue()));
+			//
+		} else if (object instanceof CharSequence) {
+			//
+			final String string = object != null ? object.toString() : null;
+			//
+			try {
+				//
+				value = Unit.with(this.defaultCloseOperation = Integer.valueOf(string));
+				//
+			} catch (final NumberFormatException e) {
+				//
+				exception = Unit.with(e);
+				//
+			} // try
+				//
+			if (value == null) {
+				//
+				List<Field> fs = FieldUtils.getAllFieldsList(JFrame.class).stream()
+						.filter(f -> f != null && StringUtils.equalsIgnoreCase(f.getName(), string)).toList();
+				//
+				int size = IterableUtils.size(fs);
+				//
+				if (size != 1) {
+					//
+					size = IterableUtils.size(fs = Arrays.stream(JFrame.class.getInterfaces())
+							.map(FieldUtils::getAllFieldsList).flatMap(Collection::stream)
+							.filter(f -> f != null && StringUtils.equalsIgnoreCase(f.getName(), string)).toList());
+					//
+				} // if
+					//
+				if (size > 1) {
+					//
+					throw new IllegalArgumentException("size=" + size);
+					//
+				} // if
+					//
+				final Field f = size == 1 ? IterableUtils.get(fs, 0) : null;
+				//
+				if (f != null && Modifier.isStatic(f.getModifiers())) {
+					//
+					try {
+						//
+						final Object obj = f.get(null);
+						//
+						if (obj instanceof Number) {
+							//
+							value = Unit.with(Integer.valueOf(((Number) obj).intValue()));
+							//
+						} // if
+							//
+					} catch (final IllegalAccessException e) {
+						//
+						exception = Unit.with(e);
+						//
+					} // try
+						//
+				} // if
+					//
+			} // if
+				//
+			if (value == null) {
+				//
+				try {
+					//
+					final Object obj = ObjectMapperUtil.readValue(new ObjectMapper(), string, Object.class);
+					//
+					if (obj instanceof Number) {
+						//
+						value = Unit.with(Integer.valueOf(((Number) object).intValue()));
+						//
+					} else if (obj != null) {
+						//
+						throw new IllegalArgumentException(obj.getClass() != null ? obj.getClass().toString() : null);
+						//
+					} // if
+						//
+				} catch (final JsonProcessingException e) {
+					//
+					exception = Unit.with(e);
+					//
+				} // try
+					//
+			} // if
+				//
+		} // if
+			//
+		if (value != null) {
+			//
+			this.defaultCloseOperation = IValue0Util.getValue0(value);
+			//
+		} else if (exception != null) {
+			//
+			TaskDialogsUtil.errorOrPrintStackTraceOrAssertOrShowException(IValue0Util.getValue0(exception));
+			//
+		} else {
+			//
+			final Object obj = IValue0Util.getValue0(value);
+			//
+			final Class<?> clz = obj != null ? obj.getClass() : null;
+			//
+			throw new IllegalArgumentException(clz != null ? clz.toString() : null);
+			//
+		} // if
+			//
 	}
 
 	@Override
--- src/test/java/org/springframework/beans/config/CustomBeanPostProcessorTest.java
+++ src/test/java/org/springframework/beans/config/CustomBeanPostProcessorTest.java
@@ -1,10 +1,12 @@
 package org.springframework.beans.config;
 
 import java.awt.GraphicsEnvironment;
+import java.lang.reflect.Field;
 
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +15,7 @@ import io.github.toolfactory.narcissus.Narcissus;
 class CustomBeanPostProcessorTest {
 
 	@Test
-	void testPostProcessBeforeInitialization() throws Exception {
+	void testPostProcessBeforeInitialization() throws NoSuchFieldException, IllegalAccessException {
 		//
 		final CustomBeanPostProcessor instance = new CustomBeanPostProcessor();
 		//
@@ -39,10 +41,55 @@ class CustomBeanPostProcessorTest {
 			//
 		Assertions.assertSame(jFrame, instance.postProcessBeforeInitialization(jFrame, null));
 		//
+		// javax.swing.WindowConstants.EXIT_ON_CLOSE
+		//
 		instance.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		//
+		final Field defaultCloseOperation = CustomBeanPostProcessor.class.getDeclaredField("defaultCloseOperation");
+		//
+		if (defaultCloseOperation != null) {
+			//
+			defaultCloseOperation.setAccessible(true);
+			//
+		} // if
+			//
+		Assertions.assertEquals(WindowConstants.EXIT_ON_CLOSE, get(defaultCloseOperation, instance));
 		//
 		Assertions.assertSame(jFrame, instance.postProcessBeforeInitialization(jFrame, null));
 		//
+		// null
+		//
+		instance.setDefaultCloseOperation(null);
+		//
+		Assertions.assertNull(get(defaultCloseOperation, instance));
+		//
+		// java.lang.String
+		//
+		instance.setDefaultCloseOperation(Integer.toString(0));
+		//
+		Assertions.assertEquals(WindowConstants.DO_NOTHING_ON_CLOSE, get(defaultCloseOperation, instance));
+		//
+		Assertions.assertSame(jFrame, instance.postProcessBeforeInitialization(jFrame, null));
+		//
+		Assertions.assertThrows(IllegalArgumentException.class,
+				() -> instance.setDefaultCloseOperation(StringUtils.wrap(Integer.toString(0), "\"")));
+		//
+		// EXIT_ON_CLOSE
+		//
+		instance.setDefaultCloseOperation("EXIT_ON_CLOSE");
+		//
+		Assertions.assertEquals(WindowConstants.EXIT_ON_CLOSE, get(defaultCloseOperation, instance));
+		//
+		Assertions.assertSame(jFrame, instance.postProcessBeforeInitialization(jFrame, null));
+		//
+		// java.lang.Boolean
+		//
+		Assertions.assertThrows(IllegalArgumentException.class, () -> instance.setDefaultCloseOperation(Boolean.TRUE));
+		//
+	}
+
+	private static Object get(final Field field, final Object instance) throws IllegalAccessException {
+		return field != null ? field.get(instance) : null;
 	}
 
 }
