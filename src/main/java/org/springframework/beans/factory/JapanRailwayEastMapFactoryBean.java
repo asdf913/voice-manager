585298e4d57a8c55414e0ad62b9866483ea18a54
asdf913 <asdf913@gmail.com>
2023-03-14 19:54:23 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/springframework/beans/factory/JapanRailwayEastMapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/JapanRailwayEastMapFactoryBean.java
@@ -133,12 +133,13 @@ public class JapanRailwayEastMapFactoryBean implements FactoryBean<Map<String, S
 		//
 	}
 
-	private static InputStream openStream(final URL instance) throws IOException {
+	private static InputStream openStream(@Nullable final URL instance) throws IOException {
 		return instance != null ? instance.openStream() : null;
 	}
 
 	private static <T, R, E extends Throwable> R testAndApply(@Nullable final Predicate<T> predicate, final T value,
-			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse) throws E {
+			final FailableFunction<T, R, E> functionTrue, @Nullable final FailableFunction<T, R, E> functionFalse)
+			throws E {
 		return predicate != null && predicate.test(value) ? FailableFunctionUtil.apply(functionTrue, value)
 				: FailableFunctionUtil.apply(functionFalse, value);
 	}
