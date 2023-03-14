01196d1fbe7e3524a4d0b9ba3f9069222767d4f5
asdf913 <asdf913@gmail.com>
2023-03-14 19:51:09 +0900

    Apply rule "Boolean expressions should not be gratuitous"

--- src/main/java/org/springframework/beans/factory/JapanRailwayEastMapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/JapanRailwayEastMapFactoryBean.java
@@ -135,7 +135,7 @@ public class JapanRailwayEastMapFactoryBean implements FactoryBean<Map<String, S
 		return instance != null ? instance.openStream() : null;
 	}
 
-	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+	private static <T, R, E extends Throwable> R testAndApply(@Nullable final Predicate<T> predicate, final T value,
 			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse) throws E {
 		return predicate != null && predicate.test(value) ? FailableFunctionUtil.apply(functionTrue, value)
 				: FailableFunctionUtil.apply(functionFalse, value);
