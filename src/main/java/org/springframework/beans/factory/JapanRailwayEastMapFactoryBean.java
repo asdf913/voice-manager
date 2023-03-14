2351d52d6ee48d9168cffb7e3803b1cca4e45dc4
asdf913 <asdf913@gmail.com>
2023-03-14 19:45:38 +0900

    Add "org.springframework.beans.factory.JapanRailwayEastMapFactoryBean" class

---
+++ src/main/java/org/springframework/beans/factory/JapanRailwayEastMapFactoryBean.java
@@ -0,0 +1,187 @@
+package org.springframework.beans.factory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+import org.apache.commons.collections4.IterableUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.function.FailableFunctionUtil;
+import org.apache.commons.validator.routines.UrlValidator;
+import org.javatuples.Pair;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+public class JapanRailwayEastMapFactoryBean implements FactoryBean<Map<String, String>> {
+
+	private String[] urls = null;
+
+	public void setUrls(final String[] urls) {
+		this.urls = urls;
+	}
+
+	@Override
+	public Map<String, String> getObject() throws Exception {
+		//
+		UrlValidator urlValidator = null;
+		//
+		Map<String, String> result = null, temp = null;
+		//
+		String key = null;
+		//
+		for (int i = 0; urls != null && i < urls.length; i++) {
+			//
+			if ((result = ObjectUtils.getIfNull(result, LinkedHashMap::new)) == null || (temp = createMap(urls[i],
+					urlValidator = ObjectUtils.getIfNull(urlValidator, UrlValidator::getInstance))) == null) {
+				//
+				continue;
+				//
+			} // if
+				//
+			for (final Entry<String, String> entry : temp.entrySet()) {
+				//
+				if (entry == null) {
+					//
+					continue;
+					//
+				} // if
+					//
+				if (result.containsKey(key = entry.getKey())) {
+					//
+					throw new IllegalStateException();
+					//
+				} // if
+					//
+				result.put(key, entry.getValue());
+				//
+			} // for
+				//
+		} // for
+			//
+		return result;
+		//
+	}
+
+	private static Map<String, String> createMap(final String url, final UrlValidator urlValidator)
+			throws IOException, CsvValidationException {
+		//
+		Map<String, String> map = null;
+		//
+		try (final InputStream is = openStream(StringUtils.isNotBlank(url) ? new URL(url) : null)) {
+			//
+			map = createMap(is, urlValidator);
+			//
+		} // try
+			//
+		return map;
+		//
+	}
+
+	private static Map<String, String> createMap(final InputStream is, final UrlValidator urlValidator)
+			throws IOException, CsvValidationException {
+		//
+		Map<String, String> map = null;
+		//
+		try (final Reader reader = testAndApply(Objects::nonNull, is, InputStreamReader::new, null);
+				final CSVReader csvReader = testAndApply(Objects::nonNull, reader, CSVReader::new, null)) {
+			//
+			String[] line = null;
+			//
+			String u, key = null;
+			//
+			Pair<String, String> pair = null;
+			//
+			while ((line = csvReader != null ? csvReader.readNext() : null) != null) {
+				//
+				if (line == null || line.length < 6 || urlValidator == null || !urlValidator.isValid(u = line[5])
+						|| (pair = createPair(u)) == null
+						|| (map = ObjectUtils.getIfNull(map, LinkedHashMap::new)) == null) {
+					//
+					continue;
+					//
+				} // if
+					//
+				if (map.containsKey(key = pair.getValue0())) {
+					//
+					throw new IllegalStateException();
+					//
+				} // if
+					//
+				map.put(key, pair.getValue1());
+				//
+			} // while
+				//
+		} // try
+			//
+		return map;
+		//
+	}
+
+	private static InputStream openStream(final URL instance) throws IOException {
+		return instance != null ? instance.openStream() : null;
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse) throws E {
+		return predicate != null && predicate.test(value) ? FailableFunctionUtil.apply(functionTrue, value)
+				: FailableFunctionUtil.apply(functionFalse, value);
+	}
+
+	private static Pair<String, String> createPair(final String urlString) throws IOException, CsvValidationException {
+		//
+		final Element element = testAndApply(Objects::nonNull,
+				testAndApply(StringUtils::isNotBlank, urlString, URL::new, null), x -> Jsoup.parse(x, 0), null);
+		//
+		return createPair(element);
+		//
+	}
+
+	private static Pair<String, String> createPair(final Element document) throws IOException, CsvValidationException {
+		//
+		Pair<String, String> pair = null;
+		//
+		// text
+		//
+		List<Element> elements = document != null ? document.select(".station_name01") : null;
+		//
+		Element element = IterableUtils.size(elements) == 1 ? IterableUtils.get(elements, 0) : null;
+		//
+		if (element != null) {
+			//
+			pair = new Pair<>(element.text(), null);
+			//
+		} // if
+			//
+			// hiragana
+			//
+		if ((element = IterableUtils.size(elements = document != null ? document.select(".station_name02") : null) == 1
+				? IterableUtils.get(elements, 0)
+				: null) != null && (pair = ObjectUtils.getIfNull(pair, () -> new Pair<>(null, null))) != null) {
+			//
+			pair = pair.setAt1(element.text());
+			//
+		} // if
+			//
+		return pair;
+		//
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return Map.class;
+	}
+
+}
---
+++ src/test/java/org/springframework/beans/factory/JapanRailwayEastMapFactoryBeanTest.java
@@ -0,0 +1,189 @@
+package org.springframework.beans.factory;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.ConnectException;
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.validator.routines.UrlValidator;
+import org.javatuples.Pair;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class JapanRailwayEastMapFactoryBeanTest {
+
+	private static Method METHOD_CREATE_MAP, METHOD_CREATE_PAIR_STRING, METHOD_CREATE_PAIR_ELEMENT = null;
+
+	@BeforeAll
+	static void beforeAll() throws ReflectiveOperationException {
+		//
+		final Class<?> clz = JapanRailwayEastMapFactoryBean.class;
+		//
+		(METHOD_CREATE_MAP = clz.getDeclaredMethod("createMap", InputStream.class, UrlValidator.class))
+				.setAccessible(true);
+		//
+		(METHOD_CREATE_PAIR_STRING = clz.getDeclaredMethod("createPair", String.class)).setAccessible(true);
+		//
+		(METHOD_CREATE_PAIR_ELEMENT = clz.getDeclaredMethod("createPair", Element.class)).setAccessible(true);
+		//
+	}
+
+	private JapanRailwayEastMapFactoryBean instance = null;
+
+	@BeforeEach
+	void beforeEach() {
+		//
+		instance = new JapanRailwayEastMapFactoryBean();
+		//
+	}
+
+	@Test
+	void testGetObject() throws Exception {
+		//
+		Assertions.assertNull(getObject(instance));
+		//
+		if (instance != null) {
+			//
+			instance.setUrls(new String[] { null, "", " " });
+			//
+		} // if
+			//
+		Assertions.assertEquals(Collections.emptyMap(), getObject(instance));
+		//
+		final File file = File.createTempFile(RandomStringUtils.randomAlphanumeric(3), null);
+		//
+		if (file != null) {
+			//
+			file.deleteOnExit();
+			//
+		} // if
+			//
+		final String url = file != null ? file.toURI().toURL().toString() : null;
+		//
+		if (instance != null) {
+			//
+			instance.setUrls(new String[] { url });
+			//
+		} // if
+			//
+		Assertions.assertEquals(Collections.emptyMap(), getObject(instance));
+		//
+	}
+
+	private static <T> T getObject(final FactoryBean<T> instnace) throws Exception {
+		return instnace != null ? instnace.getObject() : null;
+	}
+
+	@Test
+	void testGetObjectType() {
+		//
+		Assertions.assertEquals(Map.class, instance != null ? instance.getObjectType() : null);
+		//
+	}
+
+	@Test
+	void testCreateMap() throws Throwable {
+		//
+		try (final InputStream is = new ByteArrayInputStream("1,2,3,4,5".getBytes())) {
+			//
+			Assertions.assertNull(createMap(is, null));
+			//
+		} // try
+			//
+		try (final InputStream is = new ByteArrayInputStream("1,2,3,4,5,6".getBytes())) {
+			//
+			Assertions.assertNull(createMap(is, null));
+			//
+		} // try
+			//
+		final UrlValidator urlValidator = UrlValidator.getInstance();
+		//
+		try (final InputStream is = new ByteArrayInputStream("1,2,3,4,5,6".getBytes())) {
+			//
+			Assertions.assertNull(createMap(is, urlValidator));
+			//
+		} // try
+			//
+		try (final InputStream is = new ByteArrayInputStream("1,2,3,4,5,http://127.0.0.1".getBytes())) {
+			//
+			Assertions.assertThrows(ConnectException.class, () -> createMap(is, urlValidator));
+			//
+		} // try
+			//
+	}
+
+	private static Map<String, String> createMap(final InputStream is, final UrlValidator urlValidator)
+			throws Throwable {
+		try {
+			final Object obj = METHOD_CREATE_MAP.invoke(null, is, urlValidator);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Map) {
+				return (Map) obj;
+			}
+			throw new Throwable(obj.getClass() != null ? obj.getClass().toString() : null);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testCreatePair() throws Throwable {
+		//
+		Assertions.assertNull(createPair((String) null));
+		//
+		Assertions.assertNull(createPair(""));
+		//
+		Assertions.assertNull(createPair(" "));
+		//
+		Assertions.assertNull(createPair((Element) null));
+		//
+		final String key = "key";
+		//
+		final String value = "value";
+		//
+		final Element element = Jsoup.parse(String.format(
+				"<span class=\"station_name01\">%1$s</span><span class=\"station_name02\">%2$s</span>", key, value));
+		//
+		Assertions.assertEquals(Pair.with(key, value), createPair(element));
+		//
+	}
+
+	private static Pair<String, String> createPair(final String url) throws Throwable {
+		try {
+			final Object obj = METHOD_CREATE_PAIR_STRING.invoke(null, url);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Pair) {
+				return (Pair) obj;
+			}
+			throw new Throwable(obj.getClass() != null ? obj.getClass().toString() : null);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	private static Pair<String, String> createPair(final Element document) throws Throwable {
+		try {
+			final Object obj = METHOD_CREATE_PAIR_ELEMENT.invoke(null, document);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Pair) {
+				return (Pair) obj;
+			}
+			throw new Throwable(obj.getClass() != null ? obj.getClass().toString() : null);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+}
