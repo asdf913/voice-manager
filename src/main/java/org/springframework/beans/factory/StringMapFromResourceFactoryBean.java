b63aa6fc55b96201d4175498648311b788d1e877
asdf913 <asdf913@gmail.com>
2023-03-17 23:20:39 +0900

    Apply rule "Conditionally executed code should be reachable"

--- src/main/java/org/springframework/beans/factory/StringMapFromResourceFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/StringMapFromResourceFactoryBean.java
@@ -322,7 +322,7 @@ public class StringMapFromResourceFactoryBean implements MapFromResourceFactoryB
 
 	@Nullable
 	private static Cell getValueCell(final Row row, final ObjectIntMap<String> objectIntMap,
-			final Pair<String, Integer> valueColumnNameAndIndex) {
+			@Nullable final Pair<String, Integer> valueColumnNameAndIndex) {
 		//
 		Cell cell = null;
 		//
