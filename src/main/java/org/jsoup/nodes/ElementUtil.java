4236eb4a9aefabe4281dd207c74e91278b1cac6d
asdf913 <asdf913@gmail.com>
2023-03-22 22:06:36 +0900

    Apply rule "String literals should not be duplicated"

--- src/main/java/org/jsoup/nodes/ElementUtil.java
+++ src/main/java/org/jsoup/nodes/ElementUtil.java
@@ -4,13 +4,15 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.jsoup.select.Elements;
 
-public interface ElementUtil {
+public final class ElementUtil {
 
-	static String text(final Element instance) {
+	private static final String CHILD_NODES = "childNodes";
+
+	public static String text(final Element instance) {
 		//
 		try {
 			//
-			return instance != null && FieldUtils.readField(instance, "childNodes", true) != null ? instance.text()
+			return instance != null && FieldUtils.readField(instance, CHILD_NODES, true) != null ? instance.text()
 					: null;
 			//
 		} catch (final IllegalAccessException e) {
@@ -21,7 +23,7 @@ public interface ElementUtil {
 			//
 	}
 
-	static Elements getElementsByTag(final Element instance, final String tagName) {
+	public static Elements getElementsByTag(final Element instance, final String tagName) {
 		//
 		try {
 			//
@@ -36,15 +38,15 @@ public interface ElementUtil {
 			//
 	}
 
-	static Elements selectXpath(final Element instance, final String xpath) {
+	public static Elements selectXpath(final Element instance, final String xpath) {
 		return instance != null && xpath != null ? instance.selectXpath(xpath) : null;
 	}
 
-	static Elements select(final Element instance, final String cssQuery) {
+	public static Elements select(final Element instance, final String cssQuery) {
 		//
 		try {
 			//
-			return instance != null && FieldUtils.readField(instance, "childNodes", true) != null
+			return instance != null && FieldUtils.readField(instance, CHILD_NODES, true) != null
 					? instance.select(cssQuery)
 					: null;
 			//
@@ -56,11 +58,11 @@ public interface ElementUtil {
 			//
 	}
 
-	static Elements children(final Element instance) {
+	public static Elements children(final Element instance) {
 		//
 		try {
 			//
-			return instance != null && FieldUtils.readField(instance, "childNodes", true) != null ? instance.children()
+			return instance != null && FieldUtils.readField(instance, CHILD_NODES, true) != null ? instance.children()
 					: null;
 			//
 		} catch (final IllegalAccessException e) {
@@ -71,15 +73,15 @@ public interface ElementUtil {
 			//
 	}
 
-	static String attr(final Element instance, final String attributeKey) {
+	public static String attr(final Element instance, final String attributeKey) {
 		return instance != null && attributeKey != null ? instance.attr(attributeKey) : null;
 	}
 
-	static Element nextElementSibling(final Element instance) {
+	public static Element nextElementSibling(final Element instance) {
 		return instance != null ? instance.nextElementSibling() : null;
 	}
 
-	static String tagName(final Element instance) {
+	public static String tagName(final Element instance) {
 		//
 		try {
 			//
