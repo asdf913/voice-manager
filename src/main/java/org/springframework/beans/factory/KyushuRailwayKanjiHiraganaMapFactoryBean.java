49b0e949f4a6c6babd3266668ab96a6b9f4a3c44
asdf913 <asdf913@gmail.com>
2023-03-26 12:16:08 +0900

    Apply rule "Boolean expressions should not be gratuitous"

--- src/main/java/org/springframework/beans/factory/KyushuRailwayKanjiHiraganaMapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/KyushuRailwayKanjiHiraganaMapFactoryBean.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.function.Predicate;
 
+import javax.annotation.Nullable;
+
 import org.apache.commons.collections4.IterableUtils;
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.function.FailableFunction;
@@ -78,7 +80,7 @@ public class KyushuRailwayKanjiHiraganaMapFactoryBean extends StringMapFromResou
 		//
 	}
 
-	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+	private static <T, R, E extends Throwable> R testAndApply(@Nullable final Predicate<T> predicate, final T value,
 			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse) throws E {
 		return predicate != null && predicate.test(value) ? FailableFunctionUtil.apply(functionTrue, value)
 				: FailableFunctionUtil.apply(functionFalse, value);
