a255f159905c1f7a9d832efcbe9ed552af9776a4
asdf913 <asdf913@gmail.com>
2023-03-29 22:35:39 +0900

    Add "org.springframework.beans.factory.TsukubaExpressKanjiMapFactoryBean" class

---
+++ src/main/java/org/springframework/beans/factory/TsukubaExpressKanjiMapFactoryBean.java
@@ -0,0 +1,186 @@
+package org.springframework.beans.factory;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+import org.apache.commons.collections4.IterableUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.function.FailableFunctionUtil;
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.ElementUtil;
+import org.jsoup.nodes.NodeUtil;
+
+public class TsukubaExpressKanjiMapFactoryBean implements FactoryBean<Map<String, String>> {
+
+	private static enum RomajiOrHiragana {
+
+		ROMAJI, HIRAGANA
+
+	}
+
+	private String url = null;
+
+	private RomajiOrHiragana romajiOrHiragana = null;
+
+	public void setUrl(final String url) {
+		this.url = url;
+	}
+
+	public void setRomajiOrHiragana(final Object instance) {
+		//
+		if (instance == null) {
+			//
+			this.romajiOrHiragana = null;
+			//
+		} else if (instance instanceof RomajiOrHiragana roh) {
+			//
+			this.romajiOrHiragana = roh;
+			//
+		} else if (instance instanceof String string) {
+			//
+			if (StringUtils.isBlank(string)) {
+				//
+				setRomajiOrHiragana(null);
+				//
+			} else {
+				//
+				final List<RomajiOrHiragana> rohs = Arrays.stream(RomajiOrHiragana.values())
+						.filter(x -> StringUtils.startsWithIgnoreCase(x != null ? x.name() : null, string)).toList();
+				//
+				if (IterableUtils.size(rohs) > 1) {
+					//
+					throw new IllegalStateException();
+					//
+				} // if
+					//
+				if (!IterableUtils.isEmpty(rohs)) {
+					//
+					this.romajiOrHiragana = IterableUtils.get(rohs, 0);
+					//
+				} // if
+					//
+			} // if
+				//
+		} else if (instance instanceof char[] cs) {
+			//
+			setRomajiOrHiragana(new String(cs));
+			//
+		} else {
+			//
+			throw new IllegalArgumentException(instance.toString());
+			//
+		} // if
+			//
+	}
+
+	@Override
+	public Map<String, String> getObject() throws Exception {
+		//
+		final List<Element> es = ElementUtil.select(testAndApply(Objects::nonNull,
+				testAndApply(Objects::nonNull, url, URL::new, null), x -> Jsoup.parse(x, 0), null), ".station_list a");
+		//
+		Map<String, String> map = null;
+		//
+		Entry<String, String> entry = null;
+		//
+		for (int i = 0; es != null && i < es.size(); i++) {
+			//
+			if ((entry = createEntry(NodeUtil.absUrl(es.get(i), "href"), romajiOrHiragana)) == null
+					|| (map = ObjectUtils.getIfNull(map, LinkedHashMap::new)) == null) {
+				//
+				continue;
+				//
+			} // if
+				//
+			map.put(entry.getKey(), entry.getValue());
+			//
+		} // for
+			//
+		return map;
+		//
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return Map.class;
+	}
+
+	private static Entry<String, String> createEntry(final String url, final RomajiOrHiragana roh) throws IOException {
+		//
+		final Element document = testAndApply(Objects::nonNull, testAndApply(Objects::nonNull, url, URL::new, null),
+				x -> Jsoup.parse(x, 0), null);
+		///
+		final MutablePair<String, String> pair = new MutablePair<>(
+				getString(ElementUtil.select(document, ".hero_title_label")), null);
+		//
+		if (Objects.equals(roh, RomajiOrHiragana.HIRAGANA)) {
+			//
+			pair.setValue(getString(ElementUtil.select(document, ".hero_ruby")));
+			//
+		} else if (Objects.equals(roh, RomajiOrHiragana.ROMAJI)) {
+			//
+			pair.setValue(getString(ElementUtil.select(document, ".hero_romanization")));
+			//
+		} else {
+			//
+			throw new IllegalArgumentException();
+			//
+		} // if
+			//
+		return pair;
+		//
+	}
+
+	private static String getString(final Iterable<Element> es) {
+		//
+		List<String> ss = null;
+		//
+		String s = null;
+		//
+		for (int i = 0; i < IterableUtils.size(es); i++) {
+			//
+			if (contains(ss = ObjectUtils.getIfNull(ss, ArrayList::new),
+					s = ElementUtil.text(IterableUtils.get(es, i)))) {
+				//
+				continue;
+				//
+			} // if
+				//
+			ss.add(s);
+			//
+		} // for
+			//
+		if (IterableUtils.size(ss) > 1) {
+			//
+			throw new IllegalStateException();
+			//
+		} // if
+			//
+		return !IterableUtils.isEmpty(ss) ? IterableUtils.get(ss, 0) : null;
+		//
+	}
+
+	private static boolean contains(final Collection<?> items, final Object item) {
+		return items != null && items.contains(item);
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse) throws E {
+		return predicate != null && predicate.test(value) ? FailableFunctionUtil.apply(functionTrue, value)
+				: FailableFunctionUtil.apply(functionFalse, value);
+	}
+
+}
---
+++ src/test/java/org/springframework/beans/factory/TsukubaExpressKanjiMapFactoryBeanTest.java
@@ -0,0 +1,223 @@
+package org.springframework.beans.factory;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Predicate;
+
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.jsoup.nodes.Element;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.meeuw.functional.Predicates;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+class TsukubaExpressKanjiMapFactoryBeanTest {
+
+	private static Class<?> CLASS_ROMAJI_OR_HIRAGANA = null;
+
+	private static Method METHOD_CREATE_ENTRY, METHOD_GET_STRING, METHOD_CONTAINS, METHOD_TEST_AND_APPLY = null;
+
+	@BeforeAll
+	static void beforeAll() throws ReflectiveOperationException {
+		//
+		final Class<?> clz = TsukubaExpressKanjiMapFactoryBean.class;
+		//
+		(METHOD_CREATE_ENTRY = clz.getDeclaredMethod("createEntry", String.class,
+				CLASS_ROMAJI_OR_HIRAGANA = Class.forName(
+						"org.springframework.beans.factory.TsukubaExpressKanjiMapFactoryBean$RomajiOrHiragana")))
+				.setAccessible(true);
+		//
+		(METHOD_GET_STRING = clz.getDeclaredMethod("getString", Iterable.class)).setAccessible(true);
+		//
+		(METHOD_CONTAINS = clz.getDeclaredMethod("contains", Collection.class, Object.class)).setAccessible(true);
+		//
+		(METHOD_TEST_AND_APPLY = clz.getDeclaredMethod("testAndApply", Predicate.class, Object.class,
+				FailableFunction.class, FailableFunction.class)).setAccessible(true);
+		//
+	}
+
+	private TsukubaExpressKanjiMapFactoryBean instance = null;
+
+	@BeforeEach
+	void beforeEach() {
+		//
+		instance = new TsukubaExpressKanjiMapFactoryBean();
+		//
+	}
+
+	@Test
+	void testSetRomajiOrHiragana() throws NoSuchFieldException, IllegalAccessException {
+		//
+		final Field romajiOrHiragana = TsukubaExpressKanjiMapFactoryBean.class.getDeclaredField("romajiOrHiragana");
+		//
+		if (romajiOrHiragana != null) {
+			//
+			romajiOrHiragana.setAccessible(true);
+			//
+		} // if
+			//
+		Assertions.assertDoesNotThrow(() -> setRomajiOrHiragana(instance, null));
+		//
+		Assertions.assertNull(get(romajiOrHiragana, instance));
+		//
+		Assertions.assertDoesNotThrow(() -> setRomajiOrHiragana(instance, ""));
+		//
+		Assertions.assertNull(get(romajiOrHiragana, instance));
+		//
+		Assertions.assertDoesNotThrow(() -> setRomajiOrHiragana(instance, " "));
+		//
+		Assertions.assertNull(get(romajiOrHiragana, instance));
+		//
+		Assertions.assertDoesNotThrow(() -> setRomajiOrHiragana(instance, "b"));
+		//
+		Assertions.assertNull(get(romajiOrHiragana, instance));
+		//
+		Assertions.assertDoesNotThrow(() -> setRomajiOrHiragana(instance, "".toCharArray()));
+		//
+		Assertions.assertNull(get(romajiOrHiragana, instance));
+		//
+		Assertions.assertDoesNotThrow(() -> setRomajiOrHiragana(instance, "h"));
+		//
+		final Object roh = get(romajiOrHiragana, instance);
+		//
+		Assertions.assertNotNull(roh);
+		//
+		Assertions.assertDoesNotThrow(() -> setRomajiOrHiragana(instance, roh));
+		//
+		Assertions.assertSame(roh, get(romajiOrHiragana, instance));
+		//
+	}
+
+	private static Object get(final Field field, final Object instance) throws IllegalAccessException {
+		return field != null ? field.get(instance) : null;
+	}
+
+	private static void setRomajiOrHiragana(final TsukubaExpressKanjiMapFactoryBean instance, final Object object) {
+		if (instance != null) {
+			instance.setRomajiOrHiragana(object);
+		}
+	}
+
+	@Test
+	void tsetGetObject() throws Exception {
+		//
+		Assertions.assertNull(instance != null ? instance.getObject() : null);
+		//
+	}
+
+	@Test
+	void tsetGetObjectType() {
+		//
+		Assertions.assertEquals(Map.class, instance != null ? instance.getObjectType() : null);
+		//
+	}
+
+	@Test
+	void testCreateEntry() throws Throwable {
+		//
+		Assertions.assertThrows(IllegalArgumentException.class, () -> createEntry(null, null));
+		//
+		if (CLASS_ROMAJI_OR_HIRAGANA != null && CLASS_ROMAJI_OR_HIRAGANA.isEnum()) {
+			//
+			for (final Object enumConstant : CLASS_ROMAJI_OR_HIRAGANA.getEnumConstants()) {
+				//
+				Assertions.assertEquals(ImmutablePair.of(null, null), createEntry(null, enumConstant));
+				//
+			} // for
+				//
+		} // for
+			//
+	}
+
+	private static Entry<String, String> createEntry(final String url, final Object roh) throws Throwable {
+		try {
+			final Object obj = METHOD_CREATE_ENTRY.invoke(null, url, roh);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Entry) {
+				return (Entry) obj;
+			}
+			throw new Throwable(obj.getClass() != null ? obj.getClass().toString() : null);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testGetString() throws Throwable {
+		//
+		Assertions.assertNull(getString(Collections.singleton(null)));
+		//
+		Assertions.assertNull(
+				getString(Collections.nCopies(2, cast(Element.class, Narcissus.allocateInstance(Element.class)))));
+		//
+	}
+
+	private static <T> T cast(final Class<T> clz, final Object instance) {
+		return clz != null && clz.isInstance(instance) ? clz.cast(instance) : null;
+	}
+
+	private static String getString(final Iterable<Element> es) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_STRING.invoke(null, es);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof String) {
+				return (String) obj;
+			}
+			throw new Throwable(obj.getClass() != null ? obj.getClass().toString() : null);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testContains() throws Throwable {
+		//
+		Assertions.assertFalse(contains(null, null));
+		//
+		Assertions.assertFalse(contains(Collections.singleton(""), null));
+		//
+	}
+
+	private static boolean contains(final Collection<?> items, final Object item) throws Throwable {
+		try {
+			final Object obj = METHOD_CONTAINS.invoke(null, items, item);
+			if (obj instanceof Boolean) {
+				return ((Boolean) obj).booleanValue();
+			}
+			throw new Throwable(obj != null && obj.getClass() != null ? obj.getClass().toString() : null);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testTestAndApply() throws Throwable {
+		//
+		Assertions.assertNull(testAndApply(null, null, null, null));
+		//
+		Assertions.assertNull(testAndApply(Predicates.alwaysTrue(), null, null, null));
+		//
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse)
+			throws Throwable {
+		try {
+			return (R) METHOD_TEST_AND_APPLY.invoke(null, predicate, value, functionTrue, functionFalse);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+}
