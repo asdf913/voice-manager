4c65e5385f683196831b583b49794b15a18854fa
asdf913 <asdf913@gmail.com>
2023-03-29 22:43:07 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/springframework/beans/factory/TsukubaExpressKanjiMapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/TsukubaExpressKanjiMapFactoryBean.java
@@ -181,7 +181,8 @@ public class TsukubaExpressKanjiMapFactoryBean implements FactoryBean<Map<String
 	}
 
 	private static <T, R, E extends Throwable> R testAndApply(@Nullable final Predicate<T> predicate, final T value,
-			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse) throws E {
+			final FailableFunction<T, R, E> functionTrue, @Nullable final FailableFunction<T, R, E> functionFalse)
+			throws E {
 		return predicate != null && predicate.test(value) ? FailableFunctionUtil.apply(functionTrue, value)
 				: FailableFunctionUtil.apply(functionFalse, value);
 	}
