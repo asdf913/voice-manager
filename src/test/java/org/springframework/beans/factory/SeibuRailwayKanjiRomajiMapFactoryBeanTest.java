8fbdedf52f8700baf8af32a089f764c1b03dfc09
asdf913 <asdf913@gmail.com>
2023-03-30 08:09:49 +0900

    Add "org.springframework.beans.factory.SeibuRailwayKanjiRomajiMapFactoryBean" class

---
+++ src/test/java/org/springframework/beans/factory/SeibuRailwayKanjiRomajiMapFactoryBeanTest.java
@@ -0,0 +1,70 @@
+package org.springframework.beans.factory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.function.Predicate;
+
+import org.apache.commons.lang3.function.FailableFunction;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.google.common.base.Predicates;
+
+class SeibuRailwayKanjiRomajiMapFactoryBeanTest {
+
+	private static Method METHOD_TEST_AND_APPLY = null;
+
+	@BeforeAll
+	static void beforeAll() throws ReflectiveOperationException {
+		//
+		(METHOD_TEST_AND_APPLY = SeibuRailwayKanjiRomajiMapFactoryBean.class.getDeclaredMethod("testAndApply",
+				Predicate.class, Object.class, FailableFunction.class, FailableFunction.class)).setAccessible(true);
+		//
+	}
+
+	private SeibuRailwayKanjiRomajiMapFactoryBean instance = null;
+
+	@BeforeEach
+	void beforeEach() {
+		//
+		instance = new SeibuRailwayKanjiRomajiMapFactoryBean();
+		//
+	}
+
+	@Test
+	void testGetObject() throws Exception {
+		//
+		Assertions.assertNull(instance != null ? instance.getObject() : null);
+		//
+	}
+
+	@Test
+	void testGetObjectType() {
+		//
+		Assertions.assertEquals(Map.class, instance != null ? instance.getObjectType() : null);
+		//
+	}
+
+	@Test
+	void testTestAndApply() throws Throwable {
+		//
+		Assertions.assertNull(testAndApply(null, null, null, null));
+		//
+		Assertions.assertNull(testAndApply(Predicates.alwaysTrue(), null, null, null));
+		//
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse)
+			throws Throwable {
+		try {
+			return (R) METHOD_TEST_AND_APPLY.invoke(null, predicate, value, functionTrue, functionFalse);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+}
