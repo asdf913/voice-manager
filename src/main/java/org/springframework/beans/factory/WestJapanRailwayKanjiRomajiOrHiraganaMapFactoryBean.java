4ae0d05466d33777e5cb8b6e04d84fe113b7e3f2
asdf913 <asdf913@gmail.com>
2023-06-05 08:12:14 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/springframework/beans/factory/WestJapanRailwayKanjiRomajiOrHiraganaMapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/WestJapanRailwayKanjiRomajiOrHiraganaMapFactoryBean.java
@@ -426,7 +426,7 @@ public class WestJapanRailwayKanjiRomajiOrHiraganaMapFactoryBean implements Fact
 				: FailableFunctionUtil.apply(functionFalse, value);
 	}
 
-	private static final <T> boolean test(@Nullable final Predicate<T> instance, final T value) {
+	private static final <T> boolean test(@Nullable final Predicate<T> instance, @Nullable final T value) {
 		return instance != null && instance.test(value);
 	}
 
