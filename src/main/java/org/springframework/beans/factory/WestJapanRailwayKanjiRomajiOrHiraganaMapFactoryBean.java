f87ed00ce93085a202c4bdc5bf76add81cad3166
asdf913 <asdf913@gmail.com>
2023-06-05 08:05:50 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/springframework/beans/factory/WestJapanRailwayKanjiRomajiOrHiraganaMapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/WestJapanRailwayKanjiRomajiOrHiraganaMapFactoryBean.java
@@ -414,7 +414,7 @@ public class WestJapanRailwayKanjiRomajiOrHiraganaMapFactoryBean implements Fact
 	}
 
 	@Nullable
-	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, @Nullable final T value,
 			final FailableFunction<T, R, E> functionTrue, @Nullable final FailableFunction<T, R, E> functionFalse)
 			throws E {
 		return test(predicate, value) ? FailableFunctionUtil.apply(functionTrue, value)
