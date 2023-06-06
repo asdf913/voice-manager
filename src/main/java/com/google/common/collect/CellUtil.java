4289c65b49864652439a578c301bdfe1ee9b753c
asdf913 <asdf913@gmail.com>
2023-06-06 12:56:13 +0900

    Apply rule "Conditionally executed code should be reachable"

--- src/main/java/com/google/common/collect/CellUtil.java
+++ src/main/java/com/google/common/collect/CellUtil.java
@@ -1,18 +1,20 @@
 package com.google.common.collect;
 
+import javax.annotation.Nullable;
+
 import com.google.common.collect.Table.Cell;
 
 public interface CellUtil {
 
-	static <R> R getRowKey(final Cell<R, ?, ?> instance) {
+	static <R> R getRowKey(@Nullable final Cell<R, ?, ?> instance) {
 		return instance != null ? instance.getRowKey() : null;
 	}
 
-	static <C> C getColumnKey(final Cell<?, C, ?> instance) {
+	static <C> C getColumnKey(@Nullable final Cell<?, C, ?> instance) {
 		return instance != null ? instance.getColumnKey() : null;
 	}
 
-	static <V> V getValue(final Cell<?, ?, V> instance) {
+	static <V> V getValue(@Nullable final Cell<?, ?, V> instance) {
 		return instance != null ? instance.getValue() : null;
 	}
 
