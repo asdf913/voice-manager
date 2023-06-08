6d57aa517f9df6db506db92855017ecca9097253
asdf913 <asdf913@gmail.com>
2023-06-08 09:25:18 +0900

    Add "org.springframework.beans.factory.TokyoToeiTodenKanjiRomajiOrHiraganaMapFactoryBean" class

---
+++ src/main/java/org/springframework/beans/factory/TokyoToeiTodenKanjiRomajiOrHiraganaMapFactoryBean.java
@@ -0,0 +1,326 @@
+package org.springframework.beans.factory;
+
+import java.io.IOException;
+import java.lang.Character.UnicodeBlock;
+import java.lang.reflect.Field;
+import java.lang.reflect.Member;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Proxy;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.collections4.IterableUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.function.FailableFunctionUtil;
+import org.javatuples.Unit;
+import org.javatuples.valueintf.IValue0;
+import org.javatuples.valueintf.IValue0Util;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.ElementUtil;
+import org.jsoup.nodes.NodeUtil;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.CellUtil;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapUtil;
+import com.google.common.collect.Table;
+import com.google.common.collect.Table.Cell;
+import com.google.common.collect.TableUtil;
+
+public class TokyoToeiTodenKanjiRomajiOrHiraganaMapFactoryBean implements FactoryBean<Map<String, String>> {
+
+	private String url = null;
+
+	private UnicodeBlock unicodeBlock = null;
+
+	public void setUrl(final String url) {
+		this.url = url;
+	}
+
+	public void setUnicodeBlock(final Object instance) throws IllegalAccessException {
+		//
+		if (instance == null) {
+			//
+			this.unicodeBlock = null;
+			//
+		} else if (instance instanceof UnicodeBlock ub) {
+			//
+			this.unicodeBlock = ub;
+			//
+		} else if (instance instanceof String string) {
+			//
+			final IValue0<UnicodeBlock> iValue0 = getUnicodeBlock(string);
+			//
+			if (iValue0 != null) {
+				//
+				setUnicodeBlock(IValue0Util.getValue0(iValue0));
+				//
+			} // if
+				//
+		} else if (instance instanceof char[] cs) {
+			//
+			setUnicodeBlock(new String(cs));
+			//
+		} else if (instance instanceof byte[] bs) {
+			//
+			setUnicodeBlock(new String(bs));
+			//
+		} else {
+			//
+			throw new IllegalArgumentException(instance.toString());
+			//
+		} // if
+			//
+	}
+
+	private static IValue0<UnicodeBlock> getUnicodeBlock(final String string) throws IllegalAccessException {
+		//
+		if (StringUtils.isBlank(string)) {
+			//
+			return Unit.with(null);
+			//
+		} else {
+			//
+			final List<Field> fs = Arrays.stream(UnicodeBlock.class.getDeclaredFields())
+					.filter(f -> StringUtils.startsWithIgnoreCase(getName(f), string)).toList();
+			//
+			final int size = IterableUtils.size(fs);
+			//
+			if (size > 1) {
+				//
+				throw new IllegalStateException();
+				//
+			} else if (size == 0) {
+				//
+				return null;
+				//
+			} // if
+				//
+			final Field f = IterableUtils.get(fs, 0);
+			//
+			if (f == null || !Modifier.isStatic(f.getModifiers())) {
+				//
+				return null;
+				//
+			} else if (!isAssignableFrom(f.getType(), UnicodeBlock.class)) {
+				//
+				throw new IllegalStateException();
+				//
+			} // if
+				//
+			return Unit.with(cast(UnicodeBlock.class, f.get(0)));
+			//
+		} // if
+			//
+	}
+
+	private static String getName(final Member instance) {
+		return instance != null ? instance.getName() : null;
+	}
+
+	private static boolean isAssignableFrom(final Class<?> a, final Class<?> b) {
+		return a != null && b != null && a.isAssignableFrom(b);
+	}
+
+	private static <T> T cast(final Class<T> clz, final Object instance) {
+		return clz != null && clz.isInstance(instance) ? clz.cast(instance) : null;
+	}
+
+	@Override
+	public Map<String, String> getObject() throws Exception {
+		//
+		final Set<Cell<String, UnicodeBlock, String>> cells = TableUtil.cellSet(createTable(url));
+		//
+		return cells != null
+				? collect(stream(cells).filter(c -> Objects.equals(CellUtil.getColumnKey(c), unicodeBlock)),
+						Collectors.toMap(CellUtil::getRowKey, CellUtil::getValue))
+				: null;
+		//
+	}
+
+	private static Table<String, UnicodeBlock, String> createTable(final String url) throws IOException {
+		//
+		final List<Element> es = ElementUtil.select(testAndApply(Objects::nonNull,
+				testAndApply(StringUtils::isNotBlank, url, URL::new, null), x -> Jsoup.parse(x, 0), null),
+				"ul.routeList li a");
+		//
+		Element e = null;
+		//
+		Table<String, UnicodeBlock, String> table = null;
+		//
+		Map<UnicodeBlock, String> map = null;
+		//
+		for (int i = 0; es != null && i < es.size(); i++) {
+			//
+			if ((map = createMap(NodeUtil.absUrl(e = es.get(i), "href"))) == null) {
+				//
+				continue;
+				//
+			} // if
+			for (final Entry<UnicodeBlock, String> entry : map.entrySet()) {
+				//
+				if (entry == null) {
+					//
+					continue;
+					//
+				} // if
+					//
+				TableUtil.put(table = ObjectUtils.getIfNull(table, HashBasedTable::create),
+						getKanji(ElementUtil.text(e)), entry.getKey(), entry.getValue());
+				//
+			} // for
+				//
+		} // for
+			//
+		return table;
+		//
+	}
+
+	private static String getKanji(final String string) {
+		//
+		return collect(
+				stream(MultimapUtil.get(createUnicodeBlockCharacterMultimap(string),
+						UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS)),
+				Collector.of(StringBuilder::new, StringBuilder::append, StringBuilder::append,
+						StringBuilder::toString));
+		//
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse) throws E {
+		return test(predicate, value) ? FailableFunctionUtil.apply(functionTrue, value)
+				: FailableFunctionUtil.apply(functionFalse, value);
+	}
+
+	private static final <T> boolean test(final Predicate<T> instance, final T value) {
+		return instance != null && instance.test(value);
+	}
+
+	private static Map<UnicodeBlock, String> createMap(final String url) throws IOException {
+		//
+		Map<UnicodeBlock, String> map = null;
+		//
+		final Document document = testAndApply(Objects::nonNull,
+				testAndApply(StringUtils::isNotBlank, url, URL::new, null), x -> Jsoup.parse(x, 0), null);
+		//
+		// hiragana
+		//
+		List<Element> es = ElementUtil.select(document, ".name-kana");
+		//
+		if (es != null) {
+			//
+			if (es != null && es.size() > 1) {
+				//
+				throw new IllegalStateException();
+				//
+			} else if (es.size() == 1) {
+				//
+				put(map = ObjectUtils.getIfNull(map, LinkedHashMap::new), UnicodeBlock.HIRAGANA,
+						getHiragana(!es.isEmpty() ? IterableUtils.get(es, 0) : null));
+				//
+			} // if
+				//
+		} // if
+			//
+			// romaji
+			//
+		if (es != null) {
+			//
+			if ((es = ElementUtil.select(document, ".name-eng")) != null && es.size() > 1) {
+				//
+				throw new IllegalStateException();
+				//
+			} else if (es.size() == 1) {
+				//
+				put(map = ObjectUtils.getIfNull(map, LinkedHashMap::new), UnicodeBlock.BASIC_LATIN,
+						getRomaji(!es.isEmpty() ? IterableUtils.get(es, 0) : null));
+				//
+			} // if
+				//
+		} // if
+			//
+		return map;
+		//
+	}
+
+	private static <K, V> void put(final Map<K, V> instance, final K key, final V value) {
+		if (instance != null) {
+			instance.put(key, value);
+		}
+	}
+
+	private static String getHiragana(final Element element) {
+		//
+		return collect(
+				stream(MultimapUtil.get(createUnicodeBlockCharacterMultimap(ElementUtil.text(element)),
+						UnicodeBlock.HIRAGANA)),
+				Collector.of(StringBuilder::new, StringBuilder::append, StringBuilder::append,
+						StringBuilder::toString));
+		//
+	}
+
+	private static <E> Stream<E> stream(final Collection<E> instance) {
+		return instance != null ? instance.stream() : null;
+	}
+
+	private static <T, R, A> R collect(final Stream<T> instance, final Collector<? super T, A, R> collector) {
+		//
+		return instance != null && (collector != null || Proxy.isProxyClass(getClass(instance)))
+				? instance.collect(collector)
+				: null;
+		//
+	}
+
+	private static Class<?> getClass(final Object instance) {
+		return instance != null ? instance.getClass() : null;
+	}
+
+	private static String getRomaji(final Element element) {
+		//
+		return collect(
+				stream(MultimapUtil.get(createUnicodeBlockCharacterMultimap(ElementUtil.text(element)),
+						UnicodeBlock.BASIC_LATIN)),
+				Collector.of(StringBuilder::new, StringBuilder::append, StringBuilder::append,
+						StringBuilder::toString));
+		//
+	}
+
+	private static Multimap<UnicodeBlock, Character> createUnicodeBlockCharacterMultimap(final String string) {
+		//
+		char c = ' ';
+		//
+		Multimap<UnicodeBlock, Character> chars = null;
+		//
+		for (int i = 0; string != null && i < string.length(); i++) {
+			//
+			MultimapUtil.put(chars = ObjectUtils.getIfNull(chars, ArrayListMultimap::create),
+					UnicodeBlock.of(c = string.charAt(i)), c);
+			//
+		} // for
+			//
+		return chars;
+		//
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return Map.class;
+	}
+
+}
---
+++ src/test/java/org/springframework/beans/factory/TokyoToeiTodenKanjiRomajiOrHiraganaMapFactoryBeanTest.java
@@ -0,0 +1,418 @@
+package org.springframework.beans.factory;
+
+import java.lang.Character.UnicodeBlock;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.function.FailableFunction;
+import org.jsoup.nodes.Element;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.meeuw.functional.Predicates;
+
+import com.google.common.collect.Multimap;
+
+class TokyoToeiTodenKanjiRomajiOrHiraganaMapFactoryBeanTest {
+
+	private static Method METHOD_CREATE_MAP, METHOD_GET_CLASS, METHOD_GET_NAME, METHOD_IS_ASSIGNABLE_FROM, METHOD_CAST,
+			METHOD_TEST_AND_APPLY, METHOD_PUT, METHOD_GET_HIRAGANA, METHOD_GET_ROMAJI,
+			METHOD_CREATE_UNICODE_BLOCK_CHARACTER_MULTI_MAP, METHOD_STREAM, METHOD_COLLECT = null;
+
+	@BeforeAll
+	static void beforeAll() throws ReflectiveOperationException {
+		//
+		final Class<?> clz = TokyoToeiTodenKanjiRomajiOrHiraganaMapFactoryBean.class;
+		//
+		(METHOD_CREATE_MAP = clz.getDeclaredMethod("createMap", String.class)).setAccessible(true);
+		//
+		(METHOD_GET_CLASS = clz.getDeclaredMethod("getClass", Object.class)).setAccessible(true);
+		//
+		(METHOD_GET_NAME = clz.getDeclaredMethod("getName", Member.class)).setAccessible(true);
+		//
+		(METHOD_IS_ASSIGNABLE_FROM = clz.getDeclaredMethod("isAssignableFrom", Class.class, Class.class))
+				.setAccessible(true);
+		//
+		(METHOD_CAST = clz.getDeclaredMethod("cast", Class.class, Object.class)).setAccessible(true);
+		//
+		(METHOD_TEST_AND_APPLY = clz.getDeclaredMethod("testAndApply", Predicate.class, Object.class,
+				FailableFunction.class, FailableFunction.class)).setAccessible(true);
+		//
+		(METHOD_PUT = clz.getDeclaredMethod("put", Map.class, Object.class, Object.class)).setAccessible(true);
+		//
+		(METHOD_GET_HIRAGANA = clz.getDeclaredMethod("getHiragana", Element.class)).setAccessible(true);
+		//
+		(METHOD_GET_ROMAJI = clz.getDeclaredMethod("getRomaji", Element.class)).setAccessible(true);
+		//
+		(METHOD_CREATE_UNICODE_BLOCK_CHARACTER_MULTI_MAP = clz.getDeclaredMethod("createUnicodeBlockCharacterMultimap",
+				String.class)).setAccessible(true);
+		//
+		(METHOD_STREAM = clz.getDeclaredMethod("stream", Collection.class)).setAccessible(true);
+		//
+		(METHOD_COLLECT = clz.getDeclaredMethod("collect", Stream.class, Collector.class)).setAccessible(true);
+		//
+	}
+
+	private TokyoToeiTodenKanjiRomajiOrHiraganaMapFactoryBean instance = null;
+
+	@BeforeEach
+	void beforeEach() {
+		//
+		instance = new TokyoToeiTodenKanjiRomajiOrHiraganaMapFactoryBean();
+		//
+	}
+
+	@Test
+	void testGetObjectType() {
+		//
+		Assertions.assertEquals(Map.class, instance != null ? instance.getObjectType() : null);
+		//
+	}
+
+	@Test
+	void testGetObject() throws Exception {
+		//
+		Assertions.assertNull(getObject(instance));
+		//
+		if (instance != null) {
+			//
+			instance.setUrl("");
+			//
+		} // if
+			//
+		Assertions.assertNull(getObject(instance));
+		//
+		if (instance != null) {
+			//
+			instance.setUrl(" ");
+			//
+		} // if
+			//
+		Assertions.assertNull(getObject(instance));
+		//
+	}
+
+	private static <T> T getObject(final FactoryBean<T> instance) throws Exception {
+		return instance != null ? instance.getObject() : null;
+	}
+
+	@Test
+	void testSetUnicodeBlock() throws Throwable {
+		//
+		final Field unicodeBlock = TokyoToeiTodenKanjiRomajiOrHiraganaMapFactoryBean.class
+				.getDeclaredField("unicodeBlock");
+		//
+		if (unicodeBlock != null) {
+			//
+			unicodeBlock.setAccessible(true);
+			//
+		} // if
+			//
+		Assertions.assertDoesNotThrow(() -> instance.setUnicodeBlock(null));
+		//
+		Assertions.assertNull(get(unicodeBlock, instance));
+		//
+		Assertions.assertDoesNotThrow(() -> instance.setUnicodeBlock(""));
+		//
+		Assertions.assertNull(get(unicodeBlock, instance));
+		//
+		Assertions.assertDoesNotThrow(() -> instance.setUnicodeBlock("".toCharArray()));
+		//
+		Assertions.assertNull(get(unicodeBlock, instance));
+		//
+		Assertions.assertDoesNotThrow(() -> instance.setUnicodeBlock("".getBytes()));
+		//
+		Assertions.assertNull(get(unicodeBlock, instance));
+		//
+		Assertions.assertDoesNotThrow(() -> instance.setUnicodeBlock("123"));
+		//
+		Assertions.assertDoesNotThrow(() -> instance.setUnicodeBlock("HIRAGANA".toCharArray()));
+		//
+		final Object hiragana = get(unicodeBlock, instance);
+		//
+		Assertions.assertEquals("HIRAGANA", toString(hiragana));
+		//
+		Assertions.assertDoesNotThrow(() -> instance.setUnicodeBlock(hiragana));
+		//
+		Assertions.assertSame(hiragana, get(unicodeBlock, instance));
+		//
+		Assertions.assertThrows(IllegalArgumentException.class, () -> instance.setUnicodeBlock(Collections.emptyMap()));
+		//
+		Assertions.assertThrows(IllegalStateException.class, () -> instance.setUnicodeBlock("h"));
+		//
+		Assertions.assertThrows(IllegalStateException.class, () -> instance.setUnicodeBlock("map"));
+		//
+	}
+
+	private static Object get(final Field field, final Object instance)
+			throws IllegalArgumentException, IllegalAccessException {
+		return field != null ? field.get(instance) : null;
+	}
+
+	private static String toString(final Object instance) {
+		return instance != null ? instance.toString() : null;
+	}
+
+	@Test
+	void testCreateMap() throws Throwable {
+		//
+		Assertions.assertNull(createMap(null));
+		//
+		Assertions.assertNull(createMap(""));
+		//
+		Assertions.assertNull(createMap(" "));
+		//
+	}
+
+	private static Map<UnicodeBlock, String> createMap(final String url) throws Throwable {
+		try {
+			final Object obj = METHOD_CREATE_MAP.invoke(null, url);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Map) {
+				return (Map) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testGetClass() throws Throwable {
+		//
+		Assertions.assertNull(getClass(null));
+		//
+		Assertions.assertNotNull(getClass(""));
+		//
+	}
+
+	private static Class<?> getClass(final Object instance) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_CLASS.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Class) {
+				return (Class<?>) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testGetName() throws Throwable {
+		//
+		Assertions.assertNull(getName(null));
+		//
+	}
+
+	private static String getName(final Member instance) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_NAME.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof String) {
+				return (String) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testIsAssignableFrom() throws Throwable {
+		//
+		Assertions.assertFalse(isAssignableFrom(null, null));
+		//
+		Assertions.assertFalse(isAssignableFrom(String.class, null));
+		//
+	}
+
+	private static boolean isAssignableFrom(final Class<?> a, final Class<?> b) throws Throwable {
+		try {
+			final Object obj = METHOD_IS_ASSIGNABLE_FROM.invoke(null, a, b);
+			if (obj instanceof Boolean) {
+				return ((Boolean) obj).booleanValue();
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testCast() throws Throwable {
+		//
+		Assertions.assertNull(cast(null, null));
+		//
+		Assertions.assertNull(cast(Object.class, null));
+		//
+	}
+
+	private static <T> T cast(final Class<T> clz, final Object instance) throws Throwable {
+		try {
+			return (T) METHOD_CAST.invoke(null, clz, instance);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testTestAndApply() throws Throwable {
+		//
+		Assertions.assertNull(testAndApply(null, null, null, null));
+		//
+		Assertions.assertNull(testAndApply(Predicates.alwaysTrue(), null, null, null));
+		//
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse)
+			throws Throwable {
+		try {
+			return (R) METHOD_TEST_AND_APPLY.invoke(null, predicate, value, functionTrue, functionFalse);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testPut() {
+		//
+		Assertions.assertDoesNotThrow(() -> put(null, null, null));
+		//
+		Assertions.assertDoesNotThrow(() -> put(new LinkedHashMap<>(), null, null));
+		//
+	}
+
+	private static <K, V> void put(final Map<K, V> instance, final K key, final V value) throws Throwable {
+		try {
+			METHOD_PUT.invoke(null, instance, key, value);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testGetHiragana() throws Throwable {
+		//
+		Assertions.assertNull(getHiragana(null));
+		//
+	}
+
+	private static String getHiragana(final Element element) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_HIRAGANA.invoke(null, element);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof String) {
+				return (String) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testGetRomaja() throws Throwable {
+		//
+		Assertions.assertNull(getRomaja(null));
+		//
+	}
+
+	private static String getRomaja(final Element element) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_ROMAJI.invoke(null, element);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof String) {
+				return (String) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testCreateUnicodeBlockCharacterMultimap() throws Throwable {
+		//
+		final char c = ' ';
+		//
+		Assertions.assertEquals(String.format("{BASIC_LATIN=[%1$s]}", c),
+				toString(createUnicodeBlockCharacterMultimap(new String(new char[] { c }))));
+		//
+	}
+
+	private static Multimap<UnicodeBlock, Character> createUnicodeBlockCharacterMultimap(final String string)
+			throws Throwable {
+		try {
+			final Object obj = METHOD_CREATE_UNICODE_BLOCK_CHARACTER_MULTI_MAP.invoke(null, string);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Multimap) {
+				return (Multimap) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testStream() throws Throwable {
+		//
+		Assertions.assertNotNull(stream(Collections.emptySet()));
+		//
+	}
+
+	private static <T> Stream<T> stream(final Collection<T> instance) throws Throwable {
+		try {
+			final Object obj = METHOD_STREAM.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Stream) {
+				return (Stream) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testCollect() throws Throwable {
+		//
+		final Stream<?> stream1 = Stream.empty();
+		//
+		Assertions.assertNull(collect(stream1, null));
+		//
+		Assertions.assertEquals(Collections.emptyList(), collect(stream1, Collectors.toList()));
+		//
+	}
+
+	private static <T, R, A> R collect(final Stream<T> instance, final Collector<? super T, A, R> collector)
+			throws Throwable {
+		try {
+			return (R) METHOD_COLLECT.invoke(null, instance, collector);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+}
