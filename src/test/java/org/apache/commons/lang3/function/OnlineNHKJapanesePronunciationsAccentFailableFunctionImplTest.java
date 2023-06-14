cb672b7dd2ba2708b8cceaa740061cf466d6f2f9
asdf913 <asdf913@gmail.com>
2023-06-15 08:46:48 +0900

    Remove unused import statement(s) in test case

--- src/test/java/org/apache/commons/lang3/function/OnlineNHKJapanesePronunciationsAccentFailableFunctionImplTest.java
+++ src/test/java/org/apache/commons/lang3/function/OnlineNHKJapanesePronunciationsAccentFailableFunctionImplTest.java
@@ -15,7 +15,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
