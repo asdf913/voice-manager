101ae92400da8ea9a3525259ddf026be37ceae69
asdf913 <asdf913@gmail.com>
2023-06-16 08:41:16 +0900

    Apply rule "Boolean expressions should not be gratuitous"

--- src/main/java/org/springframework/context/support/JlptLevelGui.java
+++ src/main/java/org/springframework/context/support/JlptLevelGui.java
@@ -708,8 +708,8 @@ public class JlptLevelGui extends JFrame implements InitializingBean, ActionList
 		}
 	}
 
-	private static <T, U> void testAndAccept(final BiPredicate<T, U> predicate, final T t, final U u,
-			final BiConsumer<T, U> consumer) {
+	private static <T, U> void testAndAccept(@Nullable final BiPredicate<T, U> predicate, final T t, final U u,
+			@Nullable final BiConsumer<T, U> consumer) {
 		if (predicate != null && predicate.test(t, u) && consumer != null) {
 			consumer.accept(t, u);
 		}
