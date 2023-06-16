0da7d8d3e93e58588c274445a44f52f8ec66b8b0
asdf913 <asdf913@gmail.com>
2023-06-16 09:30:47 +0900

    Apply rule "Boolean expressions should not be gratuitous"

--- src/main/java/org/springframework/context/support/JouYouKanjiGui.java
+++ src/main/java/org/springframework/context/support/JouYouKanjiGui.java
@@ -776,8 +776,8 @@ public class JouYouKanjiGui extends JFrame implements EnvironmentAware, Initiali
 		}
 	}
 
-	private static <T, U> void testAndAccept(final BiPredicate<T, U> predicate, final T t, final U u,
-			final BiConsumer<T, U> consumer) {
+	private static <T, U> void testAndAccept(@Nullable final BiPredicate<T, U> predicate, final T t, final U u,
+			@Nullable final BiConsumer<T, U> consumer) {
 		if (predicate != null && predicate.test(t, u) && consumer != null) {
 			consumer.accept(t, u);
 		}
