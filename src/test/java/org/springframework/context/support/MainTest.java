32c99477d4aa44c6fd40ed64ebc135ee9a7e3cda
asdf913 <asdf913@gmail.com>
2023-06-23 09:26:32 +0900

    Update test case(s)

--- src/test/java/org/springframework/context/support/MainTest.java
+++ src/test/java/org/springframework/context/support/MainTest.java
@@ -7,6 +7,7 @@ import java.awt.Window;
 import java.io.PrintStream;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Member;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Collections;
@@ -45,8 +46,9 @@ class MainTest {
 
 	private static Method METHOD_TO_STRING, METHOD_GET_INSTANCE, METHOD_SHOW_MESSAGE_DIALOG_OR_PRINT_LN, METHOD_CAST,
 			METHOD_GET_BEAN_NAMES_FOR_TYPE, METHOD_GET_BEAN_CLASS_NAME, METHOD_PACK, METHOD_SET_VISIBLE,
-			METHOD_TEST_AND_APPLY, METHOD_GET_SELECTED_VALUE, METHOD_GET_CLASS1, METHOD_GET_CLASS3, METHOD_GET_NAME,
-			METHOD_IS_RAISE_THROWABLE_ONLY, METHOD_MAP, METHOD_ERROR_OR_PRINT_STACK_TRACE, METHOD_GET_CLASS_NAME = null;
+			METHOD_TEST_AND_APPLY, METHOD_GET_SELECTED_VALUE, METHOD_GET_CLASS1, METHOD_GET_CLASS3,
+			METHOD_GET_NAME_CLASS, METHOD_GET_NAME_MEMBER, METHOD_IS_RAISE_THROWABLE_ONLY, METHOD_MAP,
+			METHOD_ERROR_OR_PRINT_STACK_TRACE, METHOD_GET_CLASS_NAME = null;
 
 	@BeforeAll
 	static void beforeAll() throws ReflectiveOperationException {
@@ -83,7 +85,9 @@ class MainTest {
 		(METHOD_GET_CLASS3 = clz.getDeclaredMethod("getClass", ConfigurableListableBeanFactory.class,
 				PropertyResolver.class, String.class)).setAccessible(true);
 		//
-		(METHOD_GET_NAME = clz.getDeclaredMethod("getName", Class.class)).setAccessible(true);
+		(METHOD_GET_NAME_CLASS = clz.getDeclaredMethod("getName", Class.class)).setAccessible(true);
+		//
+		(METHOD_GET_NAME_MEMBER = clz.getDeclaredMethod("getName", Member.class)).setAccessible(true);
 		//
 		(METHOD_IS_RAISE_THROWABLE_ONLY = clz.getDeclaredMethod("isRaiseThrowableOnly", Class.class, Method.class))
 				.setAccessible(true);
@@ -494,13 +498,29 @@ class MainTest {
 	@Test
 	void testGetName() throws Throwable {
 		//
-		Assertions.assertNull(getName(null));
+		Assertions.assertNull(getName((Class<?>) null));
+		//
+		Assertions.assertNull(getName((Member) null));
 		//
 	}
 
 	private static String getName(final Class<?> instance) throws Throwable {
 		try {
-			final Object obj = METHOD_GET_NAME.invoke(null, instance);
+			final Object obj = METHOD_GET_NAME_CLASS.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof String) {
+				return (String) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	private static String getName(final Member instance) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_NAME_MEMBER.invoke(null, instance);
 			if (obj == null) {
 				return null;
 			} else if (obj instanceof String) {
