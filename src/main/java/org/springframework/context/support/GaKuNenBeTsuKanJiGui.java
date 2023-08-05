d24b784d07b0144fb9660428acc43e514f50a9b6
asdf913 <asdf913@gmail.com>
2023-08-05 10:43:14 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/springframework/context/support/GaKuNenBeTsuKanJiGui.java
+++ src/main/java/org/springframework/context/support/GaKuNenBeTsuKanJiGui.java
@@ -224,7 +224,7 @@ public class GaKuNenBeTsuKanJiGui extends JFrame
 			//
 	}
 
-	private static <T> T orElse(@Nullable final Optional<T> instance, final T other) {
+	private static <T> T orElse(@Nullable final Optional<T> instance, @Nullable final T other) {
 		return instance != null ? instance.orElse(other) : null;
 	}
 
