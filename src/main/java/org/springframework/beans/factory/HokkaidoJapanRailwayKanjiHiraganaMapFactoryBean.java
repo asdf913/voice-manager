d29f3026aead856f80fafbbf82da7bc4350474b9
asdf913 <asdf913@gmail.com>
2023-08-12 13:53:16 +0900

    Add "org.springframework.beans.factory.HokkaidoJapanRailwayKanjiHiraganaMapFactoryBean" class

---
+++ src/main/java/org/springframework/beans/factory/HokkaidoJapanRailwayKanjiHiraganaMapFactoryBean.java
@@ -0,0 +1,188 @@
+package org.springframework.beans.factory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Field;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+import org.apache.commons.lang3.function.FailableBiFunction;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerUtil;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+public class HokkaidoJapanRailwayKanjiHiraganaMapFactoryBean implements FactoryBean<Map<String, String>> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(HokkaidoJapanRailwayKanjiHiraganaMapFactoryBean.class);
+
+	private String url, encoding = null;
+
+	public void setUrl(final String url) {
+		this.url = url;
+	}
+
+	public void setEncoding(final String encoding) {
+		this.encoding = encoding;
+	}
+
+	@Override
+	public Map<String, String> getObject() throws Exception {
+		//
+		try (final InputStream is = openStream(testAndApply(Objects::nonNull, url, x -> new URL(x), null))) {
+			//
+			return createMap(is, encoding);
+			//
+		} // try
+			//
+	}
+
+	private static Map<String, String> createMap(final InputStream is, final String encoding)
+			throws UnsupportedEncodingException, MalformedURLException, IOException, CsvValidationException {
+		//
+		Map<String, String> map = null;
+		//
+		try (final Reader reader = testAndApply((a, b) -> a != null && b != null, is, encoding,
+				(a, b) -> new InputStreamReader(a, b),
+				(a, b) -> testAndApply(Objects::nonNull, a, x -> new InputStreamReader(x), null));
+				final CSVReader csvReader = testAndApply(Objects::nonNull, reader, CSVReader::new, null)) {
+			//
+			String[] ss = null;
+			//
+			String s = null;
+			//
+			MutablePair<String, String> pair = null;
+			//
+			while ((ss = csvReader != null ? csvReader.readNext() : null) != null) {
+				//
+				if (map == null) {
+					//
+					map = new LinkedHashMap<>();
+					//
+					continue;
+					//
+				} // if
+					//
+				pair = null;
+				//
+				for (int i = 0; i < ss.length; i++) {
+					//
+					s = ss[i];
+					//
+					if (i == 0) {
+						//
+						if (pair == null) {
+							//
+							pair = MutablePair.of(s, null);
+							//
+						} // if
+							//
+						pair.setLeft(s);
+						//
+					} else if (i == 1) {
+						//
+						if (pair == null) {
+							//
+							pair = MutablePair.of(null, s);
+							//
+						} // if
+							//
+						pair.setRight(s);
+						//
+					} // if
+						//
+				} // for
+					//
+				if (pair != null) {
+					//
+					map.put(pair.getKey(), pair.getValue());
+					//
+				} // for
+					//
+			} // while
+				//
+		} // try
+			//
+		return map;
+		//
+	}
+
+	private static final InputStream openStream(final URL instance) throws IOException {
+		//
+		if (instance == null) {
+			//
+			return null;
+			//
+		} // if
+			//
+		try {
+			//
+			if (Narcissus.getField(instance, getDeclaredField(getClass(instance), "handler")) == null) {
+				//
+				return null;
+				//
+			} // if
+				//
+		} catch (final NoSuchFieldException e) {
+			//
+			LoggerUtil.error(LOG, e.getMessage(), e);
+			//
+		} // try
+			//
+		return instance.openStream();
+		//
+	}
+
+	private static Class<?> getClass(final Object instance) {
+		return instance != null ? instance.getClass() : null;
+	}
+
+	private static Field getDeclaredField(final Class<?> instance, final String name) throws NoSuchFieldException {
+		return instance != null && name != null ? instance.getDeclaredField(name) : null;
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse) throws E {
+		return test(predicate, value) ? apply(functionTrue, value) : apply(functionFalse, value);
+	}
+
+	private static <T, R, E extends Throwable> R apply(final FailableFunction<T, R, E> instance, final T value)
+			throws E {
+		return instance != null ? instance.apply(value) : null;
+	}
+
+	private static final <T> boolean test(final Predicate<T> instance, final T value) {
+		return instance != null && instance.test(value);
+	}
+
+	private static <T, U, R, E extends Throwable> R testAndApply(final BiPredicate<T, U> predicate, final T t,
+			final U u, final FailableBiFunction<T, U, R, E> functionTrue,
+			final FailableBiFunction<T, U, R, E> functionFalse) throws E {
+		return predicate != null && predicate.test(t, u) ? apply(functionTrue, t, u) : apply(functionFalse, t, u);
+	}
+
+	private static <T, R, U, E extends Throwable> R apply(final FailableBiFunction<T, U, R, E> instance, final T t,
+			final U u) throws E {
+		return instance != null ? instance.apply(t, u) : null;
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return Map.class;
+	}
+
+}
---
+++ src/test/java/org/springframework/beans/factory/HokkaidoJapanRailwayKanjiHiraganaMapFactoryBeanTest.java
@@ -0,0 +1,252 @@
+package org.springframework.beans.factory;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+import org.apache.commons.lang3.function.FailableBiFunction;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+class HokkaidoJapanRailwayKanjiHiraganaMapFactoryBeanTest {
+
+	private static Method METHOD_CREATE_MAP, METHOD_GET_CLASS, METHOD_OPEN_STREAM, METHOD_GET_DECLARED_FIELD,
+			METHOD_TEST, METHOD_TEST_AND_APPLY = null;
+
+	@BeforeAll
+	static void beforeAll() throws ReflectiveOperationException {
+		//
+		final Class<?> clz = HokkaidoJapanRailwayKanjiHiraganaMapFactoryBean.class;
+		//
+		(METHOD_CREATE_MAP = clz.getDeclaredMethod("createMap", InputStream.class, String.class)).setAccessible(true);
+		//
+		(METHOD_GET_CLASS = clz.getDeclaredMethod("getClass", Object.class)).setAccessible(true);
+		//
+		(METHOD_OPEN_STREAM = clz.getDeclaredMethod("openStream", URL.class)).setAccessible(true);
+		//
+		(METHOD_GET_DECLARED_FIELD = clz.getDeclaredMethod("getDeclaredField", Class.class, String.class))
+				.setAccessible(true);
+		//
+		(METHOD_TEST = clz.getDeclaredMethod("test", Predicate.class, Object.class)).setAccessible(true);
+		//
+		(METHOD_TEST_AND_APPLY = clz.getDeclaredMethod("testAndApply", BiPredicate.class, Object.class, Object.class,
+				FailableBiFunction.class, FailableBiFunction.class)).setAccessible(true);
+		//
+	}
+
+	private HokkaidoJapanRailwayKanjiHiraganaMapFactoryBean instance = null;
+
+	@BeforeEach
+	void beforeEach() {
+		//
+		instance = new HokkaidoJapanRailwayKanjiHiraganaMapFactoryBean();
+		//
+	}
+
+	@Test
+	void testGetObjectType() {
+		//
+		Assertions.assertEquals(Map.class, instance != null ? instance.getObjectType() : null);
+		//
+	}
+
+	@Test
+	void testGetObject() throws Exception {
+		//
+		Assertions.assertNull(instance != null ? instance.getObject() : null);
+		//
+	}
+
+	@Test
+	void testSetUrl() {
+		//
+		Assertions.assertDoesNotThrow(() -> {
+			//
+			if (instance != null) {
+				//
+				instance.setUrl(null);
+				//
+			} // if
+				//
+		});
+		//
+	}
+
+	@Test
+	void testSetEncoding() {
+		//
+		Assertions.assertDoesNotThrow(() -> {
+			//
+			if (instance != null) {
+				//
+				instance.setEncoding(null);
+				//
+			} // if
+				//
+		});
+		//
+	}
+
+	@Test
+	void testCreateMap() throws Throwable {
+		//
+		try (final InputStream is = new ByteArrayInputStream(getBytes(""))) {
+			//
+			Assertions.assertNull(createMap(is, null));
+			//
+		} // try
+			//
+		try (final InputStream is = new ByteArrayInputStream(getBytes("1,2"))) {
+			//
+			Assertions.assertEquals(Collections.emptyMap(), createMap(is, null));
+			//
+		} // try
+			//
+		try (final InputStream is = new ByteArrayInputStream(
+				getBytes(String.join(System.lineSeparator(), "1,2", "1,2")))) {
+			//
+			Assertions.assertEquals(Collections.singletonMap("1", "2"), createMap(is, "utf-8"));
+			//
+		} // try
+			//
+	}
+
+	private static byte[] getBytes(final String instance) {
+		return instance != null ? instance.getBytes() : null;
+	}
+
+	private static Map<String, String> createMap(final InputStream is, final String encoding) throws Throwable {
+		try {
+			final Object obj = METHOD_CREATE_MAP.invoke(null, is, encoding);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Map) {
+				return (Map) obj;
+			}
+			throw new Throwable(Util.toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testGetClass() throws Throwable {
+		//
+		Assert.assertNull(getClass(null));
+		//
+	}
+
+	private static Class<?> getClass(final Object instance) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_CLASS.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Class) {
+				return (Class<?>) obj;
+			}
+			throw new Throwable(Util.toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testOpenStream() throws Throwable {
+		//
+		Assertions.assertNotNull(openStream(new File("pom.xml").toURI().toURL()));
+		//
+		Assertions.assertNull(openStream(cast(URL.class, Narcissus.allocateInstance(URL.class))));
+		//
+	}
+
+	private static <T> T cast(final Class<T> clz, final Object instance) {
+		return clz != null && clz.isInstance(instance) ? clz.cast(instance) : null;
+	}
+
+	private static InputStream openStream(final URL instance) throws Throwable {
+		try {
+			final Object obj = METHOD_OPEN_STREAM.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof InputStream) {
+				return (InputStream) obj;
+			}
+			throw new Throwable(Util.toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testGetDeclaredField() throws Throwable {
+		//
+		Assert.assertNull(getDeclaredField(null, null));
+		//
+		Assert.assertNull(getDeclaredField(Object.class, null));
+		//
+	}
+
+	private static Field getDeclaredField(final Class<?> instance, final String name) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_DECLARED_FIELD.invoke(null, instance, name);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Field) {
+				return (Field) obj;
+			}
+			throw new Throwable(Util.toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testTest() throws Throwable {
+		//
+		Assert.assertFalse(test(null, null));
+		//
+	}
+
+	private static final <T> boolean test(final Predicate<T> instance, final T value) throws Throwable {
+		try {
+			final Object obj = METHOD_TEST.invoke(null, instance, value);
+			if (obj instanceof Boolean) {
+				return ((Boolean) obj).booleanValue();
+			}
+			throw new Throwable(Util.toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testTestAndApply() throws Throwable {
+		//
+		Assert.assertNull(testAndApply(null, null, null, null, null));
+		//
+	}
+
+	private static <T, U, R, E extends Throwable> R testAndApply(final BiPredicate<T, U> predicate, final T t,
+			final U u, final FailableBiFunction<T, U, R, E> functionTrue,
+			final FailableBiFunction<T, U, R, E> functionFalse) throws Throwable {
+		try {
+			return (R) METHOD_TEST_AND_APPLY.invoke(null, predicate, t, u, functionTrue, functionFalse);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+}
