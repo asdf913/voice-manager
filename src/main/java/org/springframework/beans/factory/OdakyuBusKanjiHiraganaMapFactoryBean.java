356374a3a1b7c10d7803e79bdf6345637c3b5111
asdf913 <asdf913@gmail.com>
2023-08-19 19:55:19 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/beans/factory/OdakyuBusKanjiHiraganaMapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OdakyuBusKanjiHiraganaMapFactoryBean.java
@@ -15,7 +15,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
