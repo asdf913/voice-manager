dcd04f3f8031bbc4e3efb3cae1042ed26f39f1cb
asdf913 <asdf913@gmail.com>
2023-08-20 06:43:16 +0900

    Update "freemarker.cache.StringTemplateLoaderUtil.putTemplate(freemarker.cache.StringTemplateLoader,java.lang.String,java.lang.String)" method

--- src/main/java/freemarker/cache/StringTemplateLoaderUtil.java
+++ src/main/java/freemarker/cache/StringTemplateLoaderUtil.java
@@ -1,11 +1,59 @@
 package freemarker.cache;
 
-public interface StringTemplateLoaderUtil {
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Field;
 
-	static void putTemplate(final StringTemplateLoader instance, final String name, final String templateContent) {
-		if (instance != null && name != null && templateContent != null) {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerUtil;
+
+public class StringTemplateLoaderUtil {
+
+	private static final Logger LOG = LoggerFactory.getLogger(StringTemplateLoaderUtil.class);
+
+	public static void putTemplate(final StringTemplateLoader instance, final String name,
+			final String templateContent) {
+		//
+		if (instance == null) {
+			//
+			return;
+			//
+		} // if
+			//
+		try {
+			//
+			final Field field = StringTemplateLoader.class.getDeclaredField("templates");
+			//
+			setAccessible(field, true);
+			//
+			if (get(field, instance) == null) {
+				//
+				return;
+				//
+			} // if
+				//
+		} catch (final NoSuchFieldException | IllegalAccessException e) {
+			//
+			LoggerUtil.error(LOG, e.getMessage(), e);
+			//
+		} // try
+			//
+		if (name != null && templateContent != null) {
+			//
 			instance.putTemplate(name, templateContent);
+			//
+		} // if
+			//
+	}
+
+	private static void setAccessible(final AccessibleObject instance, final boolean flag) {
+		if (instance != null) {
+			instance.setAccessible(flag);
 		}
+	}
+
+	private static Object get(final Field field, final Object instance) throws IllegalAccessException {
+		return field != null ? field.get(instance) : null;
 	}
 
 }
--- src/test/java/freemarker/cache/StringTemplateLoaderUtilTest.java
+++ src/test/java/freemarker/cache/StringTemplateLoaderUtilTest.java
@@ -1,15 +1,74 @@
 package freemarker.cache;
 
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import io.github.toolfactory.narcissus.Narcissus;
+
 class StringTemplateLoaderUtilTest {
+
+	private static Method METHOD_SET_ACCESSIBLE, METHOD_GET = null;
+
+	@BeforeAll
+	static void beforeAll() throws ReflectiveOperationException {
+		//
+		final Class<?> clz = StringTemplateLoaderUtil.class;
+		//
+		(METHOD_SET_ACCESSIBLE = clz.getDeclaredMethod("setAccessible", AccessibleObject.class, Boolean.TYPE))
+				.setAccessible(true);
+		//
+		(METHOD_GET = clz.getDeclaredMethod("get", Field.class, Object.class)).setAccessible(true);
+		//
+	}
 
 	@Test
 	void testPutTemplate() {
 		//
 		Assertions.assertDoesNotThrow(() -> StringTemplateLoaderUtil.putTemplate(new StringTemplateLoader(), null, ""));
 		//
+		Assertions.assertDoesNotThrow(() -> StringTemplateLoaderUtil.putTemplate(
+				cast(StringTemplateLoader.class, Narcissus.allocateInstance(StringTemplateLoader.class)), "", ""));
+		//
+	}
+
+	private static <T> T cast(final Class<T> clz, final Object instance) {
+		return clz != null && clz.isInstance(instance) ? clz.cast(instance) : null;
+	}
+
+	@Test
+	void testSetAccessible() {
+		//
+		Assertions.assertDoesNotThrow(() -> setAccessible(null, true));
+		//
+	}
+
+	private static void setAccessible(final AccessibleObject instance, final boolean flag) throws Throwable {
+		try {
+			METHOD_SET_ACCESSIBLE.invoke(null, instance, flag);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testGet() throws Throwable {
+		//
+		Assertions.assertNull(get(null, null));
+		//
+	}
+
+	private static Object get(final Field field, final Object instance) throws Throwable {
+		try {
+			return METHOD_GET.invoke(null, field, instance);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
 	}
 
 }
