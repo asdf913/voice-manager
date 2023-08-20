98d0c884a9936ed87784a7a33bf76b97aa1f11de
asdf913 <asdf913@gmail.com>
2023-08-20 09:41:39 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/beans/factory/TokyuKanjiRomajiOrHiraganaMapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/TokyuKanjiRomajiOrHiraganaMapFactoryBean.java
@@ -5,7 +5,6 @@ import java.lang.Character.UnicodeBlock;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
