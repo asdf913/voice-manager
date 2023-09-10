de1013a06519d5ee2c67740af78b6fb88f36f2e4
asdf913 <asdf913@gmail.com>
2023-09-10 12:52:18 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/context/support/VoiceManager.java
+++ src/main/java/org/springframework/context/support/VoiceManager.java
@@ -100,7 +100,6 @@ import java.util.Set;
 import java.util.Spliterator;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiConsumer;
