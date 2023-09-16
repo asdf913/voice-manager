e5ee91b0108252142c293f020cb0400b53be7b40
asdf913 <asdf913@gmail.com>
2023-09-16 19:02:08 +0900

    Add "org.springframework.beans.factory.KantetsuKanjiRomajiOrHiraganaMapFactoryBean" class

---
+++ src/main/java/org/springframework/beans/factory/KantetsuKanjiRomajiOrHiraganaMapFactoryBean.java
@@ -0,0 +1,241 @@
+package org.springframework.beans.factory;
+
+import java.io.IOException;
+import java.lang.Character.UnicodeBlock;
+import java.lang.reflect.Field;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.function.FailableFunctionUtil;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.commons.lang3.stream.Streams.FailableStream;
+import org.javatuples.Unit;
+import org.javatuples.valueintf.IValue0;
+import org.javatuples.valueintf.IValue0Util;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.ElementUtil;
+import org.jsoup.select.Elements;
+
+public class KantetsuKanjiRomajiOrHiraganaMapFactoryBean implements FactoryBean<Map<String, String>> {
+
+	private String url = null;
+
+	private UnicodeBlock keyUnicodeBlock, valueUnicodeBlock = null;
+
+	public void setUrl(final String url) {
+		this.url = url;
+	}
+
+	public void setKeyUnicodeBlock(final UnicodeBlock keyUnicodeBlock) {
+		this.keyUnicodeBlock = keyUnicodeBlock;
+	}
+
+	public void setValueUnicodeBlock(final UnicodeBlock valueUnicodeBlock) {
+		this.valueUnicodeBlock = valueUnicodeBlock;
+	}
+
+	@Override
+	public Map<String, String> getObject() throws Exception {
+		//
+		return getObject(
+				Util.filter(
+						stream(map(
+								new FailableStream<>(
+										Util.stream(
+												ElementUtil.select(
+														testAndApply(Objects::nonNull,
+																testAndApply(StringUtils::isNotBlank, url, URL::new,
+																		null),
+																x -> Jsoup.parse(x, 0), null),
+														".list-stationguide-vt li a"))),
+								KantetsuKanjiRomajiOrHiraganaMapFactoryBean::getKanjiHiraganaRomaji)),
+						Objects::nonNull));
+		//
+	}
+
+	private Map<String, String> getObject(final Stream<KanjiHiraganaRomaji> stream) {
+		//
+		return collect(stream, LinkedHashMap::new, (m, v) -> {
+			//
+			final Field[] fs = getDeclaredFields(Util.getClass(v));
+			//
+			Field f = null;
+			//
+			String s = null;
+			//
+			List<UnicodeBlock> unicodeBlocks = null;
+			//
+			IValue0<String> key = null, value = null;
+			//
+			for (int i = 0; fs != null && i < fs.length; i++) {
+				//
+				if ((f = fs[i]) == null) {
+					//
+					continue;
+					//
+				} // if
+					//
+				try {
+					//
+					s = Util.toString(FieldUtils.readField(f, v, true));
+					//
+				} catch (final IllegalAccessException e) {
+					//
+					throw new RuntimeException(e);
+					//
+				} // try
+					//
+				if ((unicodeBlocks = getUnicodeBlocks(s)) != null && unicodeBlocks.size() == 1
+						&& keyUnicodeBlock == unicodeBlocks.get(0)) {
+					//
+					key = Unit.with(s);
+					//
+				} // if
+					//
+				if ((unicodeBlocks = getUnicodeBlocks(s)) != null && unicodeBlocks.size() == 1
+						&& valueUnicodeBlock == unicodeBlocks.get(0)) {
+					//
+					value = Unit.with(s);
+					//
+				} // if
+					//
+			} // for
+				//
+			if (key != null && value != null) {
+				//
+				Util.put(m, IValue0Util.getValue0(key), IValue0Util.getValue0(value));
+				//
+			} // if
+				//
+		}, Map::putAll);
+		//
+	}
+
+	private static Field[] getDeclaredFields(final Class<?> instance) {
+		return instance != null ? instance.getDeclaredFields() : null;
+	}
+
+	private static <T, R> R collect(final Stream<T> instance, final Supplier<R> supplier,
+			final BiConsumer<R, ? super T> accumulator, final BiConsumer<R, R> combiner) {
+		return instance != null ? instance.collect(supplier, accumulator, combiner) : null;
+	}
+
+	private static <T, R> FailableStream<R> map(final FailableStream<T> instance,
+			final FailableFunction<T, R, ?> mapper) {
+		return instance != null && stream(instance) != null ? instance.map(mapper) : null;
+	}
+
+	private static <T> Stream<T> stream(final FailableStream<T> instance) {
+		return instance != null ? instance.stream() : null;
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse) throws E {
+		return predicate != null && predicate.test(value) ? FailableFunctionUtil.apply(functionTrue, value)
+				: FailableFunctionUtil.apply(functionFalse, value);
+	}
+
+	private static class KanjiHiraganaRomaji {
+
+		private String kanji = null, hiragana = null, romaji = null;
+
+	}
+
+	private static KanjiHiraganaRomaji getKanjiHiraganaRomaji(final Element instance)
+			throws MalformedURLException, IOException {
+		//
+		final String attr = ElementUtil.attr(instance, "href");
+		//
+		if (StringUtils.endsWith(attr, ".pdf")) {
+			//
+			return null;
+			//
+		} // if
+			//
+		return getKanjiHiraganaRomaji(attr);
+		//
+	}
+
+	private static KanjiHiraganaRomaji getKanjiHiraganaRomaji(final String urlString)
+			throws MalformedURLException, IOException {
+		//
+		final URL url = testAndApply(StringUtils::isNotBlank, urlString, URL::new, null);
+		//
+		final KanjiHiraganaRomaji kanjiHiraganaRomaji = new KanjiHiraganaRomaji();
+		//
+		kanjiHiraganaRomaji.romaji = StringUtils.substringAfterLast(getPath(url), '/');
+		//
+		final Elements elements = ElementUtil.select(testAndApply(Objects::nonNull, url, x -> Jsoup.parse(x, 0), null),
+				"h2.current");
+		//
+		if (elements != null && elements.size() > 1) {
+			//
+			throw new IllegalStateException();
+			//
+		} // if
+			//
+		final Element element = elements != null ? elements.get(0) : null;
+		//
+		kanjiHiraganaRomaji.kanji = ElementUtil.text(element);
+		//
+		kanjiHiraganaRomaji.hiragana = ElementUtil.text(ElementUtil.nextElementSibling(element));
+		//
+		return kanjiHiraganaRomaji;
+		//
+	}
+
+	private static String getPath(final URL instance) {
+		return instance != null ? instance.getPath() : null;
+	}
+
+	private static List<UnicodeBlock> getUnicodeBlocks(final String string) {
+		//
+		final char[] cs = string != null ? string.toCharArray() : null;
+		//
+		if (cs != null) {
+			//
+			List<UnicodeBlock> unicodeBlocks = null;
+			//
+			for (final char c : cs) {
+				//
+				testAndAccept((a, b) -> b != null && !Util.contains(a, b),
+						unicodeBlocks = ObjectUtils.getIfNull(unicodeBlocks, ArrayList::new), UnicodeBlock.of(c),
+						Util::add);
+				//
+			} // for
+				//
+			return unicodeBlocks;
+			//
+		} // if
+			//
+		return null;
+		//
+	}
+
+	private static <T, U> void testAndAccept(final BiPredicate<T, U> instance, final T t, final U u,
+			final BiConsumer<T, U> consumer) {
+		if (instance != null && instance.test(t, u) && consumer != null) {
+			consumer.accept(t, u);
+		} // if
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return Map.class;
+	}
+
+}
---
+++ src/test/java/org/springframework/beans/factory/KantetsuKanjiRomajiOrHiraganaMapFactoryBeanTest.java
@@ -0,0 +1,285 @@
+package org.springframework.beans.factory;
+
+import java.lang.Character.UnicodeBlock;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.commons.lang3.stream.Streams.FailableStream;
+import org.jsoup.nodes.Element;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.meeuw.functional.Predicates;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapperUtil;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+class KantetsuKanjiRomajiOrHiraganaMapFactoryBeanTest {
+
+	private static Method METHOD_GET_OBJECT, METHOD_GET_KANJI_HIRAGANA_ROMAJI, METHOD_MAP, METHOD_TEST_AND_APPLY,
+			METHOD_GET_PATH, METHOD_TEST_AND_ACCEPT = null;
+
+	@BeforeAll
+	static void beforeAll() throws ReflectiveOperationException {
+		//
+		final Class<?> clz = KantetsuKanjiRomajiOrHiraganaMapFactoryBean.class;
+		//
+		(METHOD_GET_OBJECT = clz.getDeclaredMethod("getObject", Stream.class)).setAccessible(true);
+		//
+		(METHOD_GET_KANJI_HIRAGANA_ROMAJI = clz.getDeclaredMethod("getKanjiHiraganaRomaji", Element.class))
+				.setAccessible(true);
+		//
+		(METHOD_MAP = clz.getDeclaredMethod("map", FailableStream.class, FailableFunction.class)).setAccessible(true);
+		//
+		(METHOD_TEST_AND_APPLY = clz.getDeclaredMethod("testAndApply", Predicate.class, Object.class,
+				FailableFunction.class, FailableFunction.class)).setAccessible(true);
+		//
+		(METHOD_GET_PATH = clz.getDeclaredMethod("getPath", URL.class)).setAccessible(true);
+		//
+		(METHOD_TEST_AND_ACCEPT = clz.getDeclaredMethod("testAndAccept", BiPredicate.class, Object.class, Object.class,
+				BiConsumer.class)).setAccessible(true);
+		//
+	}
+
+	private KantetsuKanjiRomajiOrHiraganaMapFactoryBean instance = null;
+
+	@BeforeEach
+	void beforeEach() {
+		//
+		instance = new KantetsuKanjiRomajiOrHiraganaMapFactoryBean();
+		//
+	}
+
+	@Test
+	void testGetObjectType() {
+		//
+		Assertions.assertSame(Map.class, instance != null ? instance.getObjectType() : null);
+		//
+	}
+
+	@Test
+	void testGetObject() throws Throwable {
+		//
+		Assertions.assertNull(instance != null ? instance.getObject() : null);
+		//
+		if (instance != null) {
+			//
+			instance.setUrl("");
+			//
+		} // if
+			//
+		Assertions.assertNull(instance != null ? instance.getObject() : null);
+		//
+		if (instance != null) {
+			//
+			instance.setUrl(" ");
+			//
+		} // if
+			//
+		Assertions.assertNull(instance != null ? instance.getObject() : null);
+		//
+		Assertions.assertNull(getObject(null));
+		//
+		Assertions.assertEquals(Collections.emptyMap(), getObject(Stream.of((Object) null)));
+		//
+		final Class<?> clz = Class.forName(
+				"org.springframework.beans.factory.KantetsuKanjiRomajiOrHiraganaMapFactoryBean$KanjiHiraganaRomaji");
+		//
+		final Constructor<?> constructor = clz != null ? clz.getDeclaredConstructor() : null;
+		//
+		if (constructor != null) {
+			//
+			constructor.setAccessible(true);
+			//
+		} // if
+			//
+		final Object kanjiHiraganaRomaji = constructor != null ? constructor.newInstance() : null;
+		//
+		Assertions.assertEquals(Collections.emptyMap(), getObject(Stream.of(kanjiHiraganaRomaji)));
+		//
+		if (instance != null) {
+			//
+			instance.setKeyUnicodeBlock(UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS);
+			//
+		} // if
+			//
+		FieldUtils.writeDeclaredField(kanjiHiraganaRomaji, "kanji", "1", true);
+		//
+		Assertions.assertEquals(Collections.emptyMap(), getObject(Stream.of(kanjiHiraganaRomaji)));
+		//
+		final String kanji = "一";
+		//
+		FieldUtils.writeDeclaredField(kanjiHiraganaRomaji, "kanji", "一", true);
+		//
+		Assertions.assertEquals(Collections.emptyMap(), getObject(Stream.of(kanjiHiraganaRomaji)));
+		//
+		if (instance != null) {
+			//
+			instance.setValueUnicodeBlock(UnicodeBlock.HIRAGANA);
+			//
+		} // if
+			//
+		FieldUtils.writeDeclaredField(kanjiHiraganaRomaji, "hiragana", "1", true);
+		//
+		Assertions.assertEquals(Collections.emptyMap(), getObject(Stream.of(kanjiHiraganaRomaji)));
+		//
+		final String hiragana = "いち";
+		//
+		FieldUtils.writeDeclaredField(kanjiHiraganaRomaji, "hiragana", "いち", true);
+		//
+		Assertions.assertEquals(Collections.singletonMap(kanji, hiragana), getObject(Stream.of(kanjiHiraganaRomaji)));
+		//
+	}
+
+	private Map<String, String> getObject(final Stream<?> stream) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_OBJECT.invoke(instance, stream);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Map) {
+				return (Map) obj;
+			}
+			throw new Throwable(Util.toString(obj.getClass()));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testGetKanjiHiraganaRomaji() throws Throwable {
+		//
+		final ObjectMapper objectMapper = new ObjectMapper().setVisibility(PropertyAccessor.ALL, Visibility.ANY);
+		//
+		Assertions.assertEquals("{\"kanji\":null,\"hiragana\":null,\"romaji\":null}",
+				ObjectMapperUtil.writeValueAsString(objectMapper, getKanjiHiraganaRomaji(null)));
+		//
+		final Element element = Util.cast(Element.class, Narcissus.allocateInstance(Element.class));
+		//
+		Assertions.assertEquals("{\"kanji\":null,\"hiragana\":null,\"romaji\":null}",
+				ObjectMapperUtil.writeValueAsString(objectMapper, getKanjiHiraganaRomaji(null)));
+		//
+		if (element != null) {
+			//
+			element.attr("href", ".pdf");
+			//
+		} // if
+			//
+		Assertions.assertNull(getKanjiHiraganaRomaji(element));
+		//
+		if (element != null) {
+			//
+			element.attr("href", "");
+			//
+		} // if
+			//
+		Assertions.assertEquals("{\"kanji\":null,\"hiragana\":null,\"romaji\":null}",
+				ObjectMapperUtil.writeValueAsString(objectMapper, getKanjiHiraganaRomaji(null)));
+		//
+	}
+
+	private static Object getKanjiHiraganaRomaji(final Element instance) throws Throwable {
+		try {
+			return METHOD_GET_KANJI_HIRAGANA_ROMAJI.invoke(null, instance);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testMap() throws Throwable {
+		//
+		Assertions.assertNull(map(null, null));
+		//
+		Assertions.assertNotNull(map(new FailableStream<>(Stream.empty()), null));
+		//
+	}
+
+	private static <T, R> FailableStream<R> map(final FailableStream<T> instance,
+			final FailableFunction<T, R, ?> mapper) throws Throwable {
+		try {
+			final Object obj = METHOD_MAP.invoke(null, instance, mapper);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof FailableStream) {
+				return (FailableStream) obj;
+			}
+			throw new Throwable(Util.toString(Util.getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testTestAndApply() throws Throwable {
+		//
+		Assertions.assertNull(testAndApply(null, null, null, null));
+		//
+		Assertions.assertNull(testAndApply(Predicates.alwaysTrue(), null, null, null));
+		//
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse)
+			throws Throwable {
+		try {
+			return (R) METHOD_TEST_AND_APPLY.invoke(null, predicate, value, functionTrue, functionFalse);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void tesGetPath() throws Throwable {
+		//
+		Assertions.assertNull(getPath(Util.cast(URL.class, Narcissus.allocateInstance(URL.class))));
+		//
+	}
+
+	private static String getPath(final URL instance) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_PATH.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof String) {
+				return (String) obj;
+			}
+			throw new Throwable(Util.toString(Util.getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testTestAndAccept() {
+		//
+		Assertions.assertDoesNotThrow(() -> testAndAccept(null, null, null, null));
+		//
+		Assertions.assertDoesNotThrow(() -> testAndAccept(Predicates.biAlwaysTrue(), null, null, null));
+		//
+	}
+
+	private static <T, U> void testAndAccept(final BiPredicate<T, U> instance, final T t, final U u,
+			final BiConsumer<T, U> consumer) throws Throwable {
+		try {
+			METHOD_TEST_AND_ACCEPT.invoke(null, instance, t, u, consumer);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+}
