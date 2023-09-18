990f7f2e55ce108a693b7f0a7f89206cef325938
asdf913 <asdf913@gmail.com>
2023-09-18 09:27:50 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/springframework/beans/factory/KantetsuKanjiRomajiOrHiraganaMapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/KantetsuKanjiRomajiOrHiraganaMapFactoryBean.java
@@ -151,8 +151,8 @@ public class KantetsuKanjiRomajiOrHiraganaMapFactoryBean implements FactoryBean<
 	}
 
 	private static <T, U, R, E extends Throwable> R testAndApply(@Nullable final BiPredicate<T, U> predicate,
-			@Nullable final T t, final U u, @Nullable final FailableBiFunction<T, U, R, E> functionTrue,
-			final FailableBiFunction<T, U, R, E> functionFalse) throws E {
+			@Nullable final T t, final U u, final FailableBiFunction<T, U, R, E> functionTrue,
+			@Nullable final FailableBiFunction<T, U, R, E> functionFalse) throws E {
 		return predicate != null && predicate.test(t, u) ? apply(functionTrue, t, u) : apply(functionFalse, t, u);
 	}
 
