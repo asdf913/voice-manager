aefd61c53cac990513d1897dc0242e24f5088bae
asdf913 <asdf913@gmail.com>
2023-09-24 13:29:44 +0900

    Add "org.springframework.beans.factory.KominatoKanjiRomajiOrHiraganaMapFactoryBean" class

---
+++ src/main/java/org/springframework/beans/factory/KominatoKanjiRomajiOrHiraganaMapFactoryBean.java
@@ -0,0 +1,292 @@
+package org.springframework.beans.factory;
+
+import java.io.IOException;
+import java.lang.Character.UnicodeBlock;
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.function.FailableBiFunction;
+import org.apache.commons.lang3.function.FailableBiFunctionUtil;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.function.FailableFunctionUtil;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.commons.lang3.stream.FailableStreamUtil;
+import org.apache.commons.lang3.stream.Streams.FailableStream;
+import org.javatuples.Unit;
+import org.javatuples.valueintf.IValue0;
+import org.javatuples.valueintf.IValue0Util;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.ElementUtil;
+import org.jsoup.nodes.NodeUtil;
+import org.jsoup.select.Elements;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+public class KominatoKanjiRomajiOrHiraganaMapFactoryBean implements FactoryBean<Map<String, String>> {
+
+	private String url = null;
+
+	private UnicodeBlock keyUnicodeBlock, valueUnicodeBlock = null;
+
+	public void setUrl(final String url) {
+		this.url = url;
+	}
+
+	public void setKeyUnicodeBlock(final UnicodeBlock keyUnicodeBlock) {
+		this.keyUnicodeBlock = keyUnicodeBlock;
+	}
+
+	public void setValueUnicodeBlock(final UnicodeBlock valueUnicodeBlock) {
+		this.valueUnicodeBlock = valueUnicodeBlock;
+	}
+
+	@Override
+	public Map<String, String> getObject() throws Exception {
+		//
+		return getObject(
+				Util.filter(
+						FailableStreamUtil
+								.stream(FailableStreamUtil.map(
+										new FailableStream<>(
+												Util.stream(ElementUtil.select(
+														testAndApply(Objects::nonNull,
+																testAndApply(StringUtils::isNotBlank, url, URL::new,
+																		null),
+																x -> Jsoup.parse(x, 0), null),
+														"div.box-img a"))),
+										x -> getKanjiHiraganaRomaji(NodeUtil.absUrl(x, "href")))),
+						Objects::nonNull));
+		//
+	}
+
+	private Map<String, String> getObject(final Stream<KanjiHiraganaRomaji> stream) {
+		//
+		return Util.collect(stream, LinkedHashMap::new, this::accumulate, Map::putAll);
+		//
+	}
+
+	private void accumulate(final Map<String, String> m, final KanjiHiraganaRomaji v) {
+		//
+		accumulate(m, v, Util.getDeclaredFields(Util.getClass(v)));
+		//
+	}
+
+	private void accumulate(final Map<String, String> m, final KanjiHiraganaRomaji v, final Field[] fs) {
+		//
+		Field f = null;
+		//
+		String s = null;
+		//
+		List<UnicodeBlock> unicodeBlocks = null;
+		//
+		UnicodeBlock unicodeBlock = null;
+		//
+		IValue0<String> key = null, value = null;
+		//
+		for (int i = 0; fs != null && i < fs.length; i++) {
+			//
+			if (!Util.isAssignableFrom(Util.getDeclaringClass(f = fs[i]), Util.getClass(v))) {
+				//
+				continue;
+				//
+			} // if
+				//
+			try {
+				//
+				s = Util.toString(
+						testAndApply((a, b) -> b != null, f, v, (a, b) -> FieldUtils.readField(a, b, true), null));
+				//
+			} catch (final IllegalAccessException e) {
+				//
+				throw new RuntimeException(e);
+				//
+			} // try
+				//
+			if ((unicodeBlocks = getUnicodeBlocks(s)) != null && unicodeBlocks.size() == 1) {
+				//
+				if (keyUnicodeBlock == (unicodeBlock = unicodeBlocks.get(0))) {
+					//
+					key = Unit.with(s);
+					//
+				} else if (valueUnicodeBlock == unicodeBlock) {
+					//
+					value = Unit.with(s);
+					//
+				} // if
+					//
+			} // if
+				//
+		} // for
+			//
+		if (Boolean.logicalAnd(key != null, value != null)) {
+			//
+			Util.put(m, IValue0Util.getValue0(key), IValue0Util.getValue0(value));
+			//
+		} // if
+			//
+	}
+
+	private static <T, U, R, E extends Throwable> R testAndApply(final BiPredicate<T, U> predicate, final T t,
+			final U u, final FailableBiFunction<T, U, R, E> functionTrue,
+			final FailableBiFunction<T, U, R, E> functionFalse) throws E {
+		return predicate != null && predicate.test(t, u) ? FailableBiFunctionUtil.apply(functionTrue, t, u)
+				: FailableBiFunctionUtil.apply(functionFalse, t, u);
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse) throws E {
+		return predicate != null && predicate.test(value) ? FailableFunctionUtil.apply(functionTrue, value)
+				: FailableFunctionUtil.apply(functionFalse, value);
+	}
+
+	private static class KanjiHiraganaRomaji {
+
+		private String kanji, hiragana, romaji = null;
+
+	}
+
+	private static KanjiHiraganaRomaji getKanjiHiraganaRomaji(final String urlString) throws IOException {
+		//
+		final Elements elements = ElementUtil.select(testAndApply(Objects::nonNull,
+				testAndApply(StringUtils::isNotBlank, urlString, URL::new, null), x -> Jsoup.parse(x, 0), null),
+				".tx--title");
+		//
+		if (elements != null && elements.size() > 1) {
+			//
+			throw new IllegalStateException();
+			//
+		} // if
+			//
+		return getKanjiHiraganaRomaji(ElementUtil.children(elements != null ? elements.get(0) : null));
+		//
+	}
+
+	private static KanjiHiraganaRomaji getKanjiHiraganaRomaji(final List<Element> elements) {
+		//
+		Element element = null;
+		//
+		KanjiHiraganaRomaji kanjiHiraganaRomaji = null;
+		//
+		Field[] fs = null;
+		//
+		String string = null;
+		//
+		List<UnicodeBlock> unicodeBlocks = null;
+		//
+		for (int i = 0; elements != null && i < elements.size(); i++) {
+			//
+			if ((element = elements.get(i)) == null || isAllFieldNonBlank(
+					fs = ObjectUtils.getIfNull(fs, () -> Util.getDeclaredFields(KanjiHiraganaRomaji.class)),
+					kanjiHiraganaRomaji = ObjectUtils.getIfNull(kanjiHiraganaRomaji, KanjiHiraganaRomaji::new))) {
+				//
+				continue;
+				//
+			} // if
+				//
+			if ((unicodeBlocks = getUnicodeBlocks(string = ElementUtil.text(element))) == null
+					|| unicodeBlocks.size() != 1) {
+				//
+				continue;
+				//
+			} // if
+				//
+			if (Objects.equals(unicodeBlocks, Collections.singletonList(UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS))
+					&& kanjiHiraganaRomaji != null) {
+				//
+				kanjiHiraganaRomaji.kanji = string;
+				//
+			} else if (Objects.equals(unicodeBlocks, Collections.singletonList(UnicodeBlock.HIRAGANA))
+					&& kanjiHiraganaRomaji != null) {
+				//
+				kanjiHiraganaRomaji.hiragana = string;
+				//
+			} else if (Objects.equals(unicodeBlocks, Collections.singletonList(UnicodeBlock.BASIC_LATIN))
+					&& kanjiHiraganaRomaji != null) {
+				//
+				kanjiHiraganaRomaji.romaji = string;
+				//
+			} // if
+				//
+		} // for
+			//
+		return kanjiHiraganaRomaji;
+		//
+	}
+
+	private static boolean isAllFieldNonBlank(final Field[] fs, final KanjiHiraganaRomaji kanjiHiraganaRomaji) {
+		//
+		Boolean nonBlank = null;
+		//
+		Field f = null;
+		//
+		for (int j = 0; fs != null && j < fs.length; j++) {
+			//
+			if ((f = fs[j]) == null) {
+				//
+				continue;
+				//
+			} // if
+				//
+			nonBlank = BooleanUtils.toBooleanDefaultIfNull(nonBlank, true)
+					&& StringUtils.isNotBlank(Util.toString(Util.isStatic(f) ? Narcissus.getStaticField(f)
+							: testAndApply(
+									(a, b) -> a != null && Objects.equals(Util.getDeclaringClass(b), Util.getClass(a)),
+									kanjiHiraganaRomaji, f, Narcissus::getField, null)));
+			//
+		} // for
+			//
+		return BooleanUtils.toBooleanDefaultIfNull(nonBlank, false);
+		//
+	}
+
+	private static List<UnicodeBlock> getUnicodeBlocks(final String string) {
+		//
+		final char[] cs = string != null ? string.toCharArray() : null;
+		//
+		if (cs != null) {
+			//
+			List<UnicodeBlock> unicodeBlocks = null;
+			//
+			for (final char c : cs) {
+				//
+				testAndAccept((a, b) -> b != null && !Util.contains(a, b),
+						unicodeBlocks = ObjectUtils.getIfNull(unicodeBlocks, ArrayList::new), UnicodeBlock.of(c),
+						Util::add);
+				//
+			} // for
+				//
+			return unicodeBlocks;
+			//
+		} // if
+			//
+		return null;
+		//
+	}
+
+	private static <T, U> void testAndAccept(final BiPredicate<T, U> instance, final T t, final U u,
+			final BiConsumer<T, U> consumer) {
+		if (instance != null && instance.test(t, u) && consumer != null) {
+			consumer.accept(t, u);
+		}
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return Map.class;
+	}
+
+}
---
+++ src/test/java/org/springframework/beans/factory/KominatoKanjiRomajiOrHiraganaMapFactoryBeanTest.java
@@ -0,0 +1,351 @@
+package org.springframework.beans.factory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.function.FailableBiFunction;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.stream.Streams.FailableStream;
+import org.jsoup.nodes.Element;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.meeuw.functional.Predicates;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapperUtil;
+
+import io.github.toolfactory.narcissus.Narcissus;
+import javassist.util.proxy.MethodHandler;
+import javassist.util.proxy.ProxyFactory;
+import javassist.util.proxy.ProxyObject;
+
+class KominatoKanjiRomajiOrHiraganaMapFactoryBeanTest {
+
+	private static Class<?> CLASS_KANJI_HIRAGANA_ROMAJI = null;
+
+	private static Method METHOD_GET_KANJI_HIRAGANA_ROMAJI_STRING, METHOD_GET_KANJI_HIRAGANA_ROMAJI_LIST,
+			METHOD_ACCUMULATE2, METHOD_ACCUMULATE3, METHOD_TEST_AND_ACCEPT, METHOD_TEST_AND_APPLY4,
+			METHOD_TEST_AND_APPLY5, METHOD_IS_ALL_FIELD_NON_BLANK = null;
+
+	@BeforeAll
+	static void beforeAll() throws ReflectiveOperationException {
+		//
+		final Class<?> clz = KominatoKanjiRomajiOrHiraganaMapFactoryBean.class;
+		//
+		(METHOD_GET_KANJI_HIRAGANA_ROMAJI_STRING = clz.getDeclaredMethod("getKanjiHiraganaRomaji", String.class))
+				.setAccessible(true);
+		//
+		(METHOD_GET_KANJI_HIRAGANA_ROMAJI_LIST = clz.getDeclaredMethod("getKanjiHiraganaRomaji", List.class))
+				.setAccessible(true);
+		//
+		(METHOD_ACCUMULATE2 = clz.getDeclaredMethod("accumulate", Map.class,
+				CLASS_KANJI_HIRAGANA_ROMAJI = Class.forName(
+						"org.springframework.beans.factory.KominatoKanjiRomajiOrHiraganaMapFactoryBean$KanjiHiraganaRomaji")))
+				.setAccessible(true);
+		//
+		(METHOD_ACCUMULATE3 = clz.getDeclaredMethod("accumulate", Map.class, CLASS_KANJI_HIRAGANA_ROMAJI,
+				Field[].class)).setAccessible(true);
+		//
+		(METHOD_TEST_AND_ACCEPT = clz.getDeclaredMethod("testAndAccept", BiPredicate.class, Object.class, Object.class,
+				BiConsumer.class)).setAccessible(true);
+		//
+		(METHOD_TEST_AND_APPLY4 = clz.getDeclaredMethod("testAndApply", Predicate.class, Object.class,
+				FailableFunction.class, FailableFunction.class)).setAccessible(true);
+		//
+		(METHOD_TEST_AND_APPLY5 = clz.getDeclaredMethod("testAndApply", BiPredicate.class, Object.class, Object.class,
+				FailableBiFunction.class, FailableBiFunction.class)).setAccessible(true);
+		//
+		(METHOD_IS_ALL_FIELD_NON_BLANK = clz.getDeclaredMethod("isAllFieldNonBlank", Field[].class,
+				CLASS_KANJI_HIRAGANA_ROMAJI)).setAccessible(true);
+		//
+	}
+
+	private static class MH implements MethodHandler {
+
+		private String text = null;
+
+		@Override
+		public Object invoke(final Object self, final Method thisMethod, final Method proceed, final Object[] args)
+				throws Throwable {
+			//
+			final String methodName = thisMethod != null ? thisMethod.getName() : null;
+			//
+			if (self instanceof Element) {
+				//
+				if (Objects.equals(methodName, "text")) {
+					//
+					return text;
+					//
+				} // if
+					//
+			} // if
+				//
+			throw new Throwable(methodName);
+			//
+		}
+
+	}
+
+	private KominatoKanjiRomajiOrHiraganaMapFactoryBean instance = null;
+
+	@BeforeEach
+	void before() {
+		//
+		instance = new KominatoKanjiRomajiOrHiraganaMapFactoryBean();
+		//
+	}
+
+	@Test
+	void testGetObjectType() {
+		//
+		Assertions.assertSame(Map.class, instance != null ? instance.getObjectType() : null);
+		//
+	}
+
+	@Test
+	void testGetObject() throws Exception {
+		//
+		Assertions.assertNull(instance != null ? instance.getObject() : null);
+		//
+	}
+
+	@Test
+	void testGetKanjiHiraganaRomaji() throws Throwable {
+		//
+		Assertions.assertNull(getKanjiHiraganaRomaji((String) null));
+		//
+		Assertions.assertNull(getKanjiHiraganaRomaji(Collections.singletonList(null)));
+		//
+		final ObjectMapper objectMapper = new ObjectMapper().setVisibility(PropertyAccessor.ALL, Visibility.ANY);
+		//
+		Assertions.assertEquals("{\"kanji\":null,\"hiragana\":null,\"romaji\":null}",
+				ObjectMapperUtil.writeValueAsString(objectMapper, getKanjiHiraganaRomaji(
+						Collections.nCopies(2, Util.cast(Element.class, Narcissus.allocateInstance(Element.class))))));
+		//
+		final MH mh1 = new MH();
+		//
+		mh1.text = "中";
+		//
+		final MH mh2 = new MH();
+		//
+		mh2.text = "あ";
+		//
+		final MH mh3 = new MH();
+		//
+		mh3.text = "a";
+		//
+		Assertions
+				.assertEquals("{\"kanji\":\"中\",\"hiragana\":\"あ\",\"romaji\":\"a\"}",
+						ObjectMapperUtil
+								.writeValueAsString(objectMapper,
+										getKanjiHiraganaRomaji(
+												new FailableStream<>(Stream.of(mh1, mh2, mh3))
+														.map(x -> createProxy(Element.class, x,
+																y -> newInstance(
+																		getDeclaredConstructor(y, String.class), "a")))
+														.collect(Collectors.toList()))));
+		//
+	}
+
+	private static <T, E extends ReflectiveOperationException> T createProxy(final Class<T> clz, final MethodHandler mh,
+			final FailableFunction<Class<?>, Object, E> function) throws ReflectiveOperationException {
+		//
+		final ProxyFactory proxyFactory = new ProxyFactory();
+		//
+		proxyFactory.setSuperclass(Element.class);
+		//
+		final Class<?> c = proxyFactory.createClass();
+		//
+		Object instance = function != null ? function.apply(c) : null;
+		//
+		if (instance == null) {
+			//
+			instance = newInstance(c != null ? c.getDeclaredConstructor() : null);
+			//
+		} // if
+			//
+		if (instance instanceof ProxyObject) {
+			//
+			((ProxyObject) instance).setHandler(mh);
+			//
+		} // if
+			//
+		return Util.cast(clz, instance);
+		//
+	}
+
+	private static Object getKanjiHiraganaRomaji(final String urlString) throws Throwable {
+		try {
+			return METHOD_GET_KANJI_HIRAGANA_ROMAJI_STRING.invoke(null, urlString);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	private static Object getKanjiHiraganaRomaji(final List<Element> elements) throws Throwable {
+		try {
+			return METHOD_GET_KANJI_HIRAGANA_ROMAJI_LIST.invoke(null, elements);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	private static <T> Constructor<T> getDeclaredConstructor(final Class<T> instance, final Class<?>... parameterTypes)
+			throws NoSuchMethodException {
+		return instance != null ? instance.getDeclaredConstructor(parameterTypes) : null;
+	}
+
+	private static <T> T newInstance(final Constructor<T> instance, final Object... initargs)
+			throws InstantiationException, IllegalAccessException, InvocationTargetException {
+		return instance != null ? instance.newInstance(initargs) : null;
+	}
+
+	@Test
+	void testAccumulate() throws NoSuchMethodException, NoSuchFieldException, InstantiationException,
+			IllegalAccessException, InvocationTargetException {
+		//
+		Assertions.assertDoesNotThrow(() -> accumulate(null, null));
+		//
+		Assertions.assertDoesNotThrow(() -> accumulate(null, null, new Field[] { null }));
+		//
+		final Field[] fs = new Field[] { Boolean.class.getDeclaredField("value") };
+		//
+		Assertions.assertDoesNotThrow(() -> accumulate(null, null, fs));
+		//
+		final Constructor<?> constructor = getDeclaredConstructor(CLASS_KANJI_HIRAGANA_ROMAJI);
+		//
+		if (constructor != null) {
+			//
+			constructor.setAccessible(true);
+			//
+		} // if
+			//
+		Assertions.assertDoesNotThrow(() -> accumulate(null, newInstance(constructor), fs));
+		//
+	}
+
+	private void accumulate(final Map<String, String> m, final Object v) throws Throwable {
+		try {
+			METHOD_ACCUMULATE2.invoke(instance, m, v);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	private void accumulate(final Map<String, String> m, final Object v, final Field[] fs) throws Throwable {
+		try {
+			METHOD_ACCUMULATE3.invoke(instance, m, v, fs);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testTestAndAccept() {
+		//
+		Assertions.assertDoesNotThrow(() -> testAndAccept(null, null, null, null));
+		//
+		Assertions.assertDoesNotThrow(() -> testAndAccept(Predicates.biAlwaysTrue(), null, null, null));
+		//
+		Assertions.assertDoesNotThrow(() -> testAndAccept(Predicates.biAlwaysFalse(), null, null, null));
+		//
+	}
+
+	private static <T, U> void testAndAccept(final BiPredicate<T, U> instance, final T t, final U u,
+			final BiConsumer<T, U> consumer) throws Throwable {
+		try {
+			METHOD_TEST_AND_ACCEPT.invoke(null, instance, t, u, consumer);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testTestAndApply() throws Throwable {
+		//
+		Assertions.assertNull(testAndApply(null, null, null, null));
+		//
+		Assertions.assertNull(testAndApply(Predicates.alwaysTrue(), null, null, null));
+		//
+		Assertions.assertNull(testAndApply(null, null, null, null, null));
+		//
+		Assertions.assertNull(testAndApply(Predicates.biAlwaysTrue(), null, null, null, null));
+		//
+		Assertions.assertNull(testAndApply(Predicates.biAlwaysFalse(), null, null, null, null));
+		//
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse)
+			throws Throwable {
+		try {
+			return (R) METHOD_TEST_AND_APPLY4.invoke(null, predicate, value, functionTrue, functionFalse);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	private static <T, U, R, E extends Throwable> R testAndApply(final BiPredicate<T, U> predicate, final T t,
+			final U u, final FailableBiFunction<T, U, R, E> functionTrue,
+			final FailableBiFunction<T, U, R, E> functionFalse) throws Throwable {
+		try {
+			return (R) METHOD_TEST_AND_APPLY5.invoke(null, predicate, t, u, functionTrue, functionFalse);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testIsAllFieldNonBlank() throws Throwable {
+		//
+		Assertions.assertFalse(isAllFieldNonBlank(null, null));
+		//
+		Assertions.assertFalse(isAllFieldNonBlank(new Field[] { null }, null));
+		//
+		final Constructor<?> constructor = getDeclaredConstructor(CLASS_KANJI_HIRAGANA_ROMAJI);
+		//
+		if (constructor != null) {
+			//
+			constructor.setAccessible(true);
+			//
+		} // if
+			//
+		Assertions.assertTrue(
+				isAllFieldNonBlank(new Field[] { Boolean.class.getDeclaredField("TRUE") }, newInstance(constructor)));
+		//
+		Assertions.assertFalse(
+				isAllFieldNonBlank(new Field[] { Boolean.class.getDeclaredField("value") }, newInstance(constructor)));
+		//
+		Assertions.assertFalse(
+				isAllFieldNonBlank(new Field[] { Long.class.getDeclaredField("value") }, newInstance(constructor)));
+		//
+	}
+
+	private static boolean isAllFieldNonBlank(final Field[] fs, final Object kanjiHiraganaRomaji) throws Throwable {
+		try {
+			final Object obj = METHOD_IS_ALL_FIELD_NON_BLANK.invoke(null, fs, kanjiHiraganaRomaji);
+			if (obj instanceof Boolean) {
+				return ((Boolean) obj).booleanValue();
+			}
+			throw new Throwable(Util.toString(Util.getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+}
