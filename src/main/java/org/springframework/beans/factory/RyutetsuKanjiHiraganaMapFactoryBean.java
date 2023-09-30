78d64230e91081fb0c5defe8728920ad7fbd592f
asdf913 <asdf913@gmail.com>
2023-10-01 08:13:23 +0900

    Add "org.springframework.beans.factory.RyutetsuKanjiHiraganaMapFactoryBean" class

---
+++ src/main/java/org/springframework/beans/factory/RyutetsuKanjiHiraganaMapFactoryBean.java
@@ -0,0 +1,228 @@
+package org.springframework.beans.factory;
+
+import java.lang.Character.UnicodeBlock;
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.function.FailableBiFunction;
+import org.apache.commons.lang3.function.FailableBiFunctionUtil;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.function.FailableFunctionUtil;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.javatuples.Unit;
+import org.javatuples.valueintf.IValue0;
+import org.javatuples.valueintf.IValue0Util;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.ElementUtil;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.NodeUtil;
+import org.jsoup.nodes.TextNode;
+
+public class RyutetsuKanjiHiraganaMapFactoryBean implements FactoryBean<Map<String, String>> {
+
+	private String url = null;
+
+	private UnicodeBlock keyUnicodeBlock, valueUnicodeBlock = null;
+
+	public void setUrl(final String url) {
+		this.url = url;
+	}
+
+	public void setKeyUnicodeBlock(final UnicodeBlock keyUnicodeBlock) {
+		this.keyUnicodeBlock = keyUnicodeBlock;
+	}
+
+	public void setValueUnicodeBlock(final UnicodeBlock valueUnicodeBlock) {
+		this.valueUnicodeBlock = valueUnicodeBlock;
+	}
+
+	@Override
+	public Map<String, String> getObject() throws Exception {
+		//
+		final List<Element> es = ElementUtil.select(testAndApply(Objects::nonNull,
+				testAndApply(Objects::nonNull, url, URL::new, null), x -> Jsoup.parse(x, 0), null), ".station_t");
+		//
+		List<Node> childNodes = null;
+		//
+		KanjiHiraganaRomaji khr = null;
+		//
+		TextNode textNode = null;
+		//
+		Element e = null;
+		//
+		List<KanjiHiraganaRomaji> khrs = null;
+		//
+		for (int i = 0; es != null && i < es.size(); i++) {
+			//
+			if ((childNodes = NodeUtil.childNodes(e = es.get(i))) == null) {
+				//
+				continue;
+				//
+			} // if
+				//
+			khr = new KanjiHiraganaRomaji();
+			//
+			for (final Node node : childNodes) {
+				//
+				if (node == null) {
+					//
+					continue;
+					//
+				} // if
+					//
+				if (StringUtils.equalsIgnoreCase(node.nodeName(), "a") && khr.romaji == null) {
+					//
+					khr.romaji = NodeUtil.attr(node, "name");
+					//
+				} else if ((textNode = Util.cast(TextNode.class, node)) != null && khr.kanji == null) {
+					//
+					khr.kanji = textNode.text();
+					//
+				} else if (StringUtils.equalsIgnoreCase(node.nodeName(), "span")
+						&& (e = Util.cast(Element.class, node)) != null && khr.hiragana == null) {
+					//
+					khr.hiragana = StringUtils.replaceChars(ElementUtil.text(e), "()", "");
+					//
+				} // if
+					//
+			} // for
+				//
+			Util.add(khrs = ObjectUtils.getIfNull(khrs, ArrayList::new), khr);
+			//
+		} // for
+			//
+		final Field[] fs = Util.getDeclaredFields(KanjiHiraganaRomaji.class);
+		//
+		Field f = null;
+		//
+		String s = null;
+		//
+		List<UnicodeBlock> unicodeBlocks = null;
+		//
+		UnicodeBlock unicodeBlock = null;
+		//
+		IValue0<String> key = null, value = null;
+		//
+		Map<String, String> map = null;
+		//
+		for (int i = 0; khrs != null && i < khrs.size(); i++) {
+			//
+			khr = khrs.get(i);
+			//
+			key = value = null;
+			//
+			for (int j = 0; fs != null && j < fs.length; j++) {
+				//
+				if (!Util.isAssignableFrom(Util.getDeclaringClass(f = fs[j]), Util.getClass(khr))) {
+					//
+					continue;
+					//
+				} // if
+					//
+				try {
+					//
+					s = Util.toString(testAndApply((a, b) -> b != null, f, khr,
+							(a, b) -> FieldUtils.readField(a, b, true), null));
+					//
+				} catch (final IllegalAccessException iae) {
+					//
+					throw new RuntimeException(iae);
+					//
+				} // try
+					//
+				if ((unicodeBlocks = getUnicodeBlocks(Util.toCharArray(s))) != null && unicodeBlocks.size() == 1) {
+					//
+					if (keyUnicodeBlock == (unicodeBlock = unicodeBlocks.get(0))) {
+						//
+						key = Unit.with(s);
+						//
+					} else if (valueUnicodeBlock == unicodeBlock) {
+						//
+						value = Unit.with(s);
+						//
+					} // if
+						//
+				} // if
+					//
+			} // for
+				//
+			if (key == null || value == null) {
+				//
+				continue;
+				//
+			} // if
+				//
+			Util.put(map = ObjectUtils.getIfNull(map, LinkedHashMap::new), IValue0Util.getValue0(key),
+					IValue0Util.getValue0(value));
+			//
+		} // for
+			//
+		return map;
+		//
+	}
+
+	private static <T, U, R, E extends Throwable> R testAndApply(final BiPredicate<T, U> predicate, final T t,
+			final U u, final FailableBiFunction<T, U, R, E> functionTrue,
+			final FailableBiFunction<T, U, R, E> functionFalse) throws E {
+		return predicate != null && predicate.test(t, u) ? FailableBiFunctionUtil.apply(functionTrue, t, u)
+				: FailableBiFunctionUtil.apply(functionFalse, t, u);
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse) throws E {
+		return predicate != null && predicate.test(value) ? FailableFunctionUtil.apply(functionTrue, value)
+				: FailableFunctionUtil.apply(functionFalse, value);
+	}
+
+	private static class KanjiHiraganaRomaji {
+
+		private String kanji, hiragana, romaji = null;
+
+	}
+
+	private static List<UnicodeBlock> getUnicodeBlocks(final char[] cs) {
+		//
+		if (cs != null) {
+			//
+			List<UnicodeBlock> unicodeBlocks = null;
+			//
+			for (final char c : cs) {
+				//
+				testAndAccept((a, b) -> b != null && !Util.contains(a, b),
+						unicodeBlocks = ObjectUtils.getIfNull(unicodeBlocks, ArrayList::new), UnicodeBlock.of(c),
+						Util::add);
+				//
+			} // for
+				//
+			return unicodeBlocks;
+			//
+		} // if
+			//
+		return null;
+		//
+	}
+
+	private static <T, U> void testAndAccept(final BiPredicate<T, U> instance, final T t, final U u,
+			final BiConsumer<T, U> consumer) {
+		if (instance != null && instance.test(t, u) && consumer != null) {
+			consumer.accept(t, u);
+		} // if
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return Map.class;
+	}
+
+}
---
+++ src/test/java/org/springframework/beans/factory/RyutetsuKanjiHiraganaMapFactoryBeanTest.java
@@ -0,0 +1,146 @@
+package org.springframework.beans.factory;
+
+import java.lang.Character.UnicodeBlock;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+import org.apache.commons.lang3.function.FailableBiFunction;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.meeuw.functional.Predicates;
+
+class RyutetsuKanjiHiraganaMapFactoryBeanTest {
+
+	private static Method METHOD_TEST_AND_APPLY4, METHOD_TEST_AND_APPLY5, METHOD_GET_UNICODE_BLOCKS,
+			METHOD_TEST_AND_ACCEPT = null;
+
+	@BeforeAll
+	static void beforeAll() throws ReflectiveOperationException {
+		//
+		final Class<?> clz = RyutetsuKanjiHiraganaMapFactoryBean.class;
+		//
+		(METHOD_TEST_AND_APPLY4 = clz.getDeclaredMethod("testAndApply", Predicate.class, Object.class,
+				FailableFunction.class, FailableFunction.class)).setAccessible(true);
+		//
+		(METHOD_TEST_AND_APPLY5 = clz.getDeclaredMethod("testAndApply", BiPredicate.class, Object.class, Object.class,
+				FailableBiFunction.class, FailableBiFunction.class)).setAccessible(true);
+		//
+		(METHOD_GET_UNICODE_BLOCKS = clz.getDeclaredMethod("getUnicodeBlocks", char[].class)).setAccessible(true);
+		//
+		(METHOD_TEST_AND_ACCEPT = clz.getDeclaredMethod("testAndAccept", BiPredicate.class, Object.class, Object.class,
+				BiConsumer.class)).setAccessible(true);
+		//
+	}
+
+	private RyutetsuKanjiHiraganaMapFactoryBean instance = null;
+
+	@BeforeEach
+	private void beforeEach() {
+		//
+		instance = new RyutetsuKanjiHiraganaMapFactoryBean();
+		//
+	}
+
+	@Test
+	void testGetObjectType() {
+		//
+		Assertions.assertEquals(Map.class, instance != null ? instance.getObjectType() : null);
+		//
+	}
+
+	@Test
+	void testGetObject() throws Exception {
+		//
+		Assertions.assertNull(instance != null ? instance.getObject() : null);
+		//
+	}
+
+	@Test
+	void testTestAndApply() throws Throwable {
+		//
+		Assertions.assertNull(testAndApply(null, null, null, null));
+		//
+		Assertions.assertNull(testAndApply(Predicates.alwaysTrue(), null, null, null));
+		//
+		Assertions.assertNull(testAndApply(null, null, null, null, null));
+		//
+		Assertions.assertNull(testAndApply(Predicates.biAlwaysTrue(), null, null, null, null));
+		//
+		Assertions.assertNull(testAndApply(Predicates.biAlwaysFalse(), null, null, null, null));
+		//
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse)
+			throws Throwable {
+		try {
+			return (R) METHOD_TEST_AND_APPLY4.invoke(null, predicate, value, functionTrue, functionFalse);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	private static <T, U, R, E extends Throwable> R testAndApply(final BiPredicate<T, U> predicate, final T t,
+			final U u, final FailableBiFunction<T, U, R, E> functionTrue,
+			final FailableBiFunction<T, U, R, E> functionFalse) throws Throwable {
+		try {
+			return (R) METHOD_TEST_AND_APPLY5.invoke(null, predicate, t, u, functionTrue, functionFalse);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testGetUnicodeBlocks() throws Throwable {
+		//
+		Assertions.assertNull(getUnicodeBlocks(null));
+		//
+		Assertions.assertEquals(Collections.singletonList(UnicodeBlock.BASIC_LATIN),
+				getUnicodeBlocks(new char[] { ' ' }));
+		//
+	}
+
+	private static List<UnicodeBlock> getUnicodeBlocks(final char[] cs) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_UNICODE_BLOCKS.invoke(null, cs);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof List) {
+				return (List) obj;
+			}
+			throw new Throwable(Util.toString(Util.getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testTestAndAccept() {
+		//
+		Assertions.assertDoesNotThrow(() -> testAndAccept(null, null, null, null));
+		//
+		Assertions.assertDoesNotThrow(() -> testAndAccept(Predicates.biAlwaysTrue(), null, null, null));
+		//
+		Assertions.assertDoesNotThrow(() -> testAndAccept(Predicates.biAlwaysFalse(), null, null, null));
+		//
+	}
+
+	private static <T, U> void testAndAccept(final BiPredicate<T, U> instance, final T t, final U u,
+			final BiConsumer<T, U> consumer) throws Throwable {
+		try {
+			METHOD_TEST_AND_ACCEPT.invoke(null, instance, t, u, consumer);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+}
