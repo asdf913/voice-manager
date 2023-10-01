32f596494cdeb6c54455998b680cdd936ed70839
asdf913 <asdf913@gmail.com>
2023-10-01 14:34:04 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/context/support/Main.java
+++ src/main/java/org/springframework/context/support/Main.java
@@ -26,7 +26,6 @@ import org.apache.bcel.classfile.ClassParser;
 import org.apache.bcel.classfile.ClassParserUtil;
 import org.apache.bcel.classfile.ConstantPool;
 import org.apache.bcel.classfile.FieldOrMethodUtil;
-import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.classfile.JavaClassUtil;
 import org.apache.bcel.generic.ATHROW;
 import org.apache.bcel.generic.ConstantPoolGen;
