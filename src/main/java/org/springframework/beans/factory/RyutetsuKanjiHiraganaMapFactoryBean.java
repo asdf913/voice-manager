c4209ec6b609bbcefddb2a809a0e8dc54b588fb6
asdf913 <asdf913@gmail.com>
2023-10-01 10:47:09 +0900

    Apply rule "Multiple variables should not be declared on the same line"

--- src/main/java/org/springframework/beans/factory/RyutetsuKanjiHiraganaMapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/RyutetsuKanjiHiraganaMapFactoryBean.java
@@ -1,6 +1,10 @@
 package org.springframework.beans.factory;
 
 import java.lang.Character.UnicodeBlock;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.lang.reflect.Field;
 import java.net.URL;
 import java.util.ArrayList;
@@ -39,7 +43,16 @@ public class RyutetsuKanjiHiraganaMapFactoryBean implements FactoryBean<Map<Stri
 
 	private String url = null;
 
-	private UnicodeBlock keyUnicodeBlock, valueUnicodeBlock = null;
+	@Target(ElementType.FIELD)
+	@Retention(RetentionPolicy.RUNTIME)
+	private @interface Note {
+		String value();
+	}
+
+	@Note("Key Unicode Block")
+	private UnicodeBlock keyUnicodeBlock = null;
+
+	private UnicodeBlock valueUnicodeBlock = null;
 
 	public void setUrl(final String url) {
 		this.url = url;
@@ -206,7 +219,10 @@ public class RyutetsuKanjiHiraganaMapFactoryBean implements FactoryBean<Map<Stri
 
 	private static class KanjiHiraganaRomaji {
 
-		private String kanji, hiragana, romaji = null;
+		@Note("Kanji")
+		private String kanji = null;
+
+		private String hiragana, romaji = null;
 
 	}
 
