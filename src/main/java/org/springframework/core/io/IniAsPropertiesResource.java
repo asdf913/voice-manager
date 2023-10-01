9b4805ed7df892b4cf64ce7a1ba3aee1fb624cee
asdf913 <asdf913@gmail.com>
2023-10-01 14:08:53 +0900

    Apply rule ""@NonNull" values should not be set to null"

--- src/main/java/org/springframework/core/io/IniAsPropertiesResource.java
+++ src/main/java/org/springframework/core/io/IniAsPropertiesResource.java
@@ -309,6 +309,7 @@ public class IniAsPropertiesResource implements Resource, ApplicationEventPublis
 		return instance != null && instance.test(t, u);
 	}
 
+	@Nullable
 	private static <T, U, R> R apply(@Nullable final BiFunction<T, U, R> instance, final T t, final U u) {
 		return instance != null ? instance.apply(t, u) : null;
 	}
