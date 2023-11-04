fa6a8007322e35fae23f1e2ffab669786babe38c
asdf913 <asdf913@gmail.com>
2023-11-04 09:50:51 +0900

    Apply rule "Boolean expressions should not be gratuitous"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenFrameMultiMapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenFrameMultiMapFactoryBean.java
@@ -115,6 +115,7 @@ public class OtoYakuNoHeyaYomikataJitenFrameMultiMapFactoryBean implements Facto
 		//
 	}
 
+	@Nullable
 	private static <T, R, E extends Throwable> R testAndApply(@Nullable final Predicate<T> predicate,
 			@Nullable final T value, final FailableFunction<T, R, E> functionTrue,
 			@Nullable final FailableFunction<T, R, E> functionFalse) throws E {
