6c822152cd6db275bc09c8e24cb520a8554976dc
asdf913 <asdf913@gmail.com>
2023-11-04 09:39:40 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenFrameMultiMapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenFrameMultiMapFactoryBean.java
@@ -8,6 +8,8 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.function.Predicate;
 
+import javax.annotation.Nullable;
+
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.function.FailableFunction;
@@ -114,7 +116,8 @@ public class OtoYakuNoHeyaYomikataJitenFrameMultiMapFactoryBean implements Facto
 	}
 
 	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
-			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse) throws E {
+			final FailableFunction<T, R, E> functionTrue, @Nullable final FailableFunction<T, R, E> functionFalse)
+			throws E {
 		return predicate != null && predicate.test(value) ? FailableFunctionUtil.apply(functionTrue, value)
 				: FailableFunctionUtil.apply(functionFalse, value);
 	}
