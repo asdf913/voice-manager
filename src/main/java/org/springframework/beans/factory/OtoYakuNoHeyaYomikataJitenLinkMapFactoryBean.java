ff27fec2317c09421c4d6ec9af6b03fc1ca6b532
asdf913 <asdf913@gmail.com>
2023-12-31 10:33:12 +0900

    Add "org.springframework.beans.factory.OtoYakuNoHeyaYomikataJitenLinkMapFactoryBean" class

---
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenLinkMapFactoryBean.java
@@ -0,0 +1,375 @@
+package org.springframework.beans.factory;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.collections4.IterableUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.function.FailableFunctionUtil;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.ElementUtil;
+import org.jsoup.nodes.NodeUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerUtil;
+
+import com.google.common.reflect.Reflection;
+
+/*
+ * https://hiramatu-hifuka.com/onyak/onyindx.html
+ */
+public class OtoYakuNoHeyaYomikataJitenLinkMapFactoryBean implements FactoryBean<Object> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(OtoYakuNoHeyaYomikataJitenLinkMapFactoryBean.class);
+
+	private String url, title = null;
+
+	public void setUrl(final String url) {
+		this.url = url;
+	}
+
+	public void setTitle(final String title) {
+		this.title = title;
+	}
+
+	static interface Link {
+
+		String getCategory();
+
+		String getUrl();
+
+		String getText();
+
+		String getDescription();
+
+		Integer getNumber();
+
+	}
+
+	private static class IH implements InvocationHandler {
+
+		private String text, description, url, category = null;
+
+		private Integer number = null;
+
+		@Override
+		public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+			//
+			final String methodName = Util.getName(method);
+			//
+			if (proxy instanceof Link) {
+				//
+				if (Objects.equals(methodName, "getText")) {
+					//
+					return text;
+					//
+				} else if (Objects.equals(methodName, "getDescription")) {
+					//
+					return description;
+					//
+				} else if (Objects.equals(methodName, "getUrl")) {
+					//
+					return url;
+					//
+				} else if (Objects.equals(methodName, "getCategory")) {
+					//
+					return category;
+					//
+				} else if (Objects.equals(methodName, "getNumber")) {
+					//
+					return number;
+					//
+				} // if
+					//
+			} // if
+				//
+			throw new Throwable(methodName);
+			//
+		}
+
+	}
+
+	@Override
+	public List<Link> getObject() throws Exception {
+		//
+		return getLinks(Util.toList(Util.filter(Util.stream(ElementUtil.select(
+				getElement(testAndApply(StringUtils::isNotBlank, url, x -> new URI(x).toURL(), null), title), "tr")),
+				x -> x != null && x.childrenSize() >= 3)));
+		//
+	}
+
+	private static List<Link> getLinks(final List<Element> es) {
+		//
+		Element e = null;
+		//
+		String category = null;
+		//
+		int childrenSize = 0;
+		//
+		IH ih = null;
+		//
+		Element child, a1, a2 = null;
+		//
+		List<Link> links = null;
+		//
+		Integer number = null;
+		//
+		int offset, index, size = 0;
+		//
+		boolean hasAttrRowSpan = false;
+		//
+		List<Element> as1 = null, as2 = null;
+		//
+		for (int i = 0; es != null && i < es.size(); i++) {
+			//
+			if ((e = es.get(i)) == null) {
+				//
+				continue;
+				//
+			} // if
+				//
+			if ((childrenSize = childrenSize(e)) > 0 && (child = ElementUtil.child(e, 0)) != null
+					&& (hasAttrRowSpan = child.hasAttr("rowspan"))) {
+				//
+				category = ElementUtil.text(child);
+				//
+			} // if
+				//
+				// number
+				//
+			if (childrenSize > (index = (0 + (offset = hasAttrRowSpan ? 1 : 0)))
+					&& (child = ElementUtil.child(e, index)) != null) {
+				//
+				number = valueOf(ElementUtil.text(child));
+				//
+			} // if
+				//
+			as2 = childrenSize > (index = 2 + offset) && (child = ElementUtil.child(e, index)) != null
+					? ElementUtil.select(child, "a")
+					: null;
+			//
+			for (int j = 0; j < IterableUtils
+					.size(as1 = childrenSize > (index = 1 + offset) && (child = ElementUtil.child(e, index)) != null
+							? ElementUtil.select(child, "a")
+							: null); j++) {
+				//
+				if ((a1 = IterableUtils.get(as1, j)) == null) {
+					//
+					continue;
+					//
+				} // if
+					//
+				if ((size = IterableUtils.size(as2)) > 0) {
+					//
+					for (int k = 0; k < size; k++) {
+						//
+						if ((a2 = IterableUtils.get(as2, k)) == null) {
+							//
+							continue;
+							//
+						} // if
+							//
+						(ih = new IH()).category = category;
+						//
+						ih.number = number;
+						//
+						try {
+							//
+							new URL(a1.attr("href"));
+							//
+							ih.description = ElementUtil.text(a1);
+							//
+							ih.url = a1.absUrl("href");
+							//
+							ih.text = ElementUtil.text(a2);
+							//
+						} catch (final MalformedURLException e1) {
+							//
+							ih.description = StringUtils.joinWith(" ", ElementUtil.text(a1), ElementUtil.text(a2));
+							//
+							ih.url = a2.absUrl("href");
+							//
+							ih.text = StringUtils.joinWith(" ", ElementUtil.text(a1), ElementUtil.text(a2));
+							//
+						} // try
+							//
+						Util.add(links = ObjectUtils.getIfNull(links, ArrayList::new),
+								Reflection.newProxy(Link.class, ih));
+						//
+					} // for
+						//
+				} else {
+					//
+					(ih = new IH()).category = category;
+					//
+					ih.number = number;
+					//
+					if (childrenSize > (index = 2 + offset) && (child = ElementUtil.child(e, index)) != null) {
+						//
+						ih.description = ElementUtil.text(child);
+						//
+					} // if
+						//
+					ih.url = a1.absUrl("href");
+					//
+					ih.text = ElementUtil.text(a1);
+					//
+					Util.add(links = ObjectUtils.getIfNull(links, ArrayList::new), Reflection.newProxy(Link.class, ih));
+					//
+				} // for
+					//
+			} // for
+				//
+		} // for
+			//
+		return links;
+		//
+	}
+
+	private static int childrenSize(final Element instance) {
+		//
+		if (instance == null) {
+			//
+			return 0;
+			//
+		} // if
+			//
+		final Class<?> clz = Util.getClass(instance);
+		//
+		try {
+			//
+			final Field field = clz != null ? clz.getDeclaredField("childNodes") : null;
+			//
+			if (field != null) {
+				//
+				field.setAccessible(true);
+				//
+				if (field.get(instance) == null) {
+					//
+					return 0;
+					//
+				} // if
+					//
+			} // if
+				//
+		} catch (final IllegalAccessException | NoSuchFieldException e) {
+			//
+			LoggerUtil.error(LOG, e.getMessage(), e);
+			//
+		} // try
+			//
+		return instance.childrenSize();
+		//
+	}
+
+	private static Integer valueOf(final String instance) {
+		try {
+			return StringUtils.isNotBlank(instance) ? Integer.valueOf(instance) : null;
+		} catch (final NumberFormatException e) {
+			return null;
+		}
+	}
+
+	private static Element getParentByNodeName(final Element element, final String nodeName) {
+		//
+		return orElse(findFirst(
+				Util.filter(Util.stream(parents(element)), x -> Objects.equals(nodeName, NodeUtil.nodeName(x)))), null);
+		//
+	}
+
+	private static Element getElement(final URL url, final String title) throws IOException {
+		//
+		final List<Element> bs = ElementUtil.select(testAndApply(Objects::nonNull, url, x -> Jsoup.parse(x, 0), null),
+				"b");
+		//
+		final List<Element> es = Util.collect(Util.filter(Util.stream(bs),
+				x -> StringUtils.equals(StringUtils.defaultIfBlank(title, "音訳の部屋読み方辞典"), trim(ElementUtil.text(x)))),
+				Collectors.toList());
+		//
+		final int size = IterableUtils.size(es);
+		//
+		if (size > 1) {
+			//
+			throw new IllegalStateException();
+			//
+		} // if
+			//
+		return getParentByNodeName(size == 1 ? IterableUtils.get(es, 0) : null, "table");
+		//
+	}
+
+	private static <T> T orElse(final Optional<T> instance, final T value) {
+		return instance != null ? instance.orElse(value) : value;
+	}
+
+	private static <T> Optional<T> findFirst(final Stream<T> instance) {
+		return instance != null ? instance.findFirst() : null;
+	}
+
+	private static List<Element> parents(final Element instance) {
+		return instance != null ? instance.parents() : null;
+	}
+
+	private static String trim(final String string) {
+		//
+		if (StringUtils.isEmpty(string)) {
+			//
+			return string;
+			//
+		} // if
+			//
+		final char[] cs = Util.toCharArray(string);
+		//
+		StringBuilder sb = null;
+		//
+		char c = ' ';
+		//
+		for (int i = 0; cs != null && i < cs.length; i++) {
+			//
+			if (Character.isWhitespace(c = cs[i])) {
+				//
+				continue;
+				//
+			} // if
+				//
+			append(sb = ObjectUtils.getIfNull(sb, StringBuilder::new), c);
+			//
+		} // for
+			//
+		return StringUtils.defaultString(Util.toString(sb));
+		//
+	}
+
+	private static void append(final StringBuilder instance, final char c) {
+		if (instance != null) {
+			instance.append(c);
+		}
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse) throws E {
+		return predicate != null && predicate.test(value) ? FailableFunctionUtil.apply(functionTrue, value)
+				: FailableFunctionUtil.apply(functionFalse, value);
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return List.class;
+	}
+
+}
---
+++ src/test/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenLinkMapFactoryBeanTest.java
@@ -0,0 +1,340 @@
+package org.springframework.beans.factory;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.commons.lang3.stream.Streams.FailableStream;
+import org.jsoup.nodes.Element;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.meeuw.functional.Predicates;
+import org.springframework.beans.factory.OtoYakuNoHeyaYomikataJitenLinkMapFactoryBean.Link;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapperUtil;
+import com.google.common.reflect.Reflection;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+public class OtoYakuNoHeyaYomikataJitenLinkMapFactoryBeanTest {
+
+	private static Method METHOD_GET_LINKS, METHOD_CHILDREN_SIZE, METHOD_VALUE_OF, METHOD_OR_ELSE, METHOD_FIND_FIRST,
+			METHOD_PARENTS, METHOD_TRIM, METHOD_APPEND, METHOD_TEST_AND_APPLY = null;
+
+	@BeforeAll
+	public static void beforeClass() throws NoSuchMethodException {
+		//
+		final Class<?> clz = OtoYakuNoHeyaYomikataJitenLinkMapFactoryBean.class;
+		//
+		(METHOD_GET_LINKS = clz.getDeclaredMethod("getLinks", List.class)).setAccessible(true);
+		//
+		(METHOD_CHILDREN_SIZE = clz.getDeclaredMethod("childrenSize", Element.class)).setAccessible(true);
+		//
+		(METHOD_VALUE_OF = clz.getDeclaredMethod("valueOf", String.class)).setAccessible(true);
+		//
+		(METHOD_OR_ELSE = clz.getDeclaredMethod("orElse", Optional.class, Object.class)).setAccessible(true);
+		//
+		(METHOD_FIND_FIRST = clz.getDeclaredMethod("findFirst", Stream.class)).setAccessible(true);
+		//
+		(METHOD_PARENTS = clz.getDeclaredMethod("parents", Element.class)).setAccessible(true);
+		//
+		(METHOD_TRIM = clz.getDeclaredMethod("trim", String.class)).setAccessible(true);
+		//
+		(METHOD_APPEND = clz.getDeclaredMethod("append", StringBuilder.class, Character.TYPE)).setAccessible(true);
+		//
+		(METHOD_TEST_AND_APPLY = clz.getDeclaredMethod("testAndApply", Predicate.class, Object.class,
+				FailableFunction.class, FailableFunction.class)).setAccessible(true);
+		//
+	}
+
+	private static class IH implements InvocationHandler {
+
+		@Override
+		public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+			//
+			final String methodName = Util.getName(method);
+			//
+			if (proxy instanceof Stream) {
+				//
+				if (Objects.equals(methodName, "findFirst")) {
+					//
+					return null;
+					//
+				} // if
+					//
+			} // if
+				//
+			throw new Throwable(methodName);
+			//
+		}
+
+	}
+
+	private OtoYakuNoHeyaYomikataJitenLinkMapFactoryBean instance = null;
+
+	@BeforeEach
+	public void beforeEach() {
+		//
+		instance = new OtoYakuNoHeyaYomikataJitenLinkMapFactoryBean();
+		//
+	}
+
+	@Test
+	public void testGetObject() throws Exception {
+		//
+		if (instance != null) {
+			//
+			instance.setTitle("音訳の部屋読み方辞典");
+			//
+		} // if
+			//
+		final Map<Object, Object> properties = System.getProperties();
+		//
+		if (properties != null && properties
+				.containsKey("org.springframework.beans.factory.OtoYakuNoHeyaYomikataJitenLinkMapFactoryBean.url")) {
+			//
+			if (instance != null) {
+				//
+				instance.setUrl(Util.toString(Util.get(properties,
+						"org.springframework.beans.factory.OtoYakuNoHeyaYomikataJitenLinkMapFactoryBean.url")));
+				//
+			} // if
+				//
+			new FailableStream<>(
+					ObjectUtils.getIfNull(Util.stream(instance != null ? instance.getObject() : null), Stream::empty))
+					.forEach(x -> {
+						//
+						System.out
+								.println(
+										ObjectMapperUtil.writeValueAsString(
+												new ObjectMapper().setDefaultPropertyInclusion(Include.NON_NULL)
+														.configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true),
+												x));
+						//
+					});
+		} else {
+			//
+			Assertions.assertNull(instance != null ? instance.getObject() : null);
+			//
+		} // if
+			//
+	}
+
+	@Test
+	public void testGetObjectType() {
+		//
+		Assertions.assertSame(List.class, instance != null ? instance.getObjectType() : null);
+		//
+	}
+
+	@Test
+	public void testGetLinks() throws Throwable {
+		//
+		Assertions.assertNull(getLinks(Collections.singletonList(null)));
+		//
+		Assertions.assertNull(getLinks(
+				Collections.singletonList(Util.cast(Element.class, Narcissus.allocateInstance(Element.class)))));
+		//
+	}
+
+	private static List<Link> getLinks(final List<Element> es) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_LINKS.invoke(null, es);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof List) {
+				return (List) obj;
+			}
+			throw new Throwable(Util.toString(Util.getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	public void testChildrenSize() throws Throwable {
+		//
+		Assertions.assertEquals(0, childrenSize(null));
+		//
+		final Element element = Util.cast(Element.class, Narcissus.allocateInstance(Element.class));
+		//
+		FieldUtils.writeDeclaredField(element, "childNodes", Collections.emptyList(), true);
+		//
+		Assertions.assertEquals(0, childrenSize(element));
+		//
+	}
+
+	private static int childrenSize(final Element instance) throws Throwable {
+		try {
+			final Object obj = METHOD_CHILDREN_SIZE.invoke(null, instance);
+			if (obj instanceof Integer) {
+				return ((Integer) obj).intValue();
+			}
+			throw new Throwable(Util.toString(Util.getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	public void testValueOf() throws Throwable {
+		//
+		Assertions.assertNull(valueOf(null));
+		//
+		Assertions.assertNull(valueOf(""));
+		//
+		Assertions.assertNull(valueOf(" "));
+		//
+		Assertions.assertNull(valueOf("A"));
+		//
+		final int one = 1;
+		//
+		Assertions.assertEquals(Integer.valueOf(one), valueOf(Integer.toString(one)));
+		//
+	}
+
+	private static Integer valueOf(final String instance) throws Throwable {
+		try {
+			final Object obj = METHOD_VALUE_OF.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Integer) {
+				return (Integer) obj;
+			}
+			throw new Throwable(Util.toString(Util.getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	public void testOrElse() throws Throwable {
+		//
+		Assertions.assertNull(orElse(Util.cast(Optional.class, Narcissus.allocateInstance(Optional.class)), null));
+		//
+	}
+
+	private static <T> T orElse(final Optional<T> instance, final T value) throws Throwable {
+		try {
+			return (T) METHOD_OR_ELSE.invoke(null, instance, value);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	public void testFindFirst() throws Throwable {
+		//
+		Assertions.assertNull(findFirst(Reflection.newProxy(Stream.class, new IH())));
+		//
+	}
+
+	private static <T> Optional<T> findFirst(final Stream<T> instance) throws Throwable {
+		try {
+			final Object obj = METHOD_FIND_FIRST.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Optional) {
+				return (Optional) obj;
+			}
+			throw new Throwable(Util.toString(Util.getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	public void testParents() throws Throwable {
+		//
+		Assertions.assertEquals(Collections.emptyList(),
+				parents(Util.cast(Element.class, Narcissus.allocateInstance(Element.class))));
+		//
+	}
+
+	private static List<Element> parents(final Element instance) throws Throwable {
+		try {
+			final Object obj = METHOD_PARENTS.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof List) {
+				return (List) obj;
+			}
+			throw new Throwable(Util.toString(Util.getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	public void testTrim() throws Throwable {
+		//
+		Assertions.assertNull(trim(null));
+		//
+		Assertions.assertEquals("A", trim(" A "));
+		//
+	}
+
+	private static String trim(final String string) throws Throwable {
+		try {
+			final Object obj = METHOD_TRIM.invoke(null, string);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof String) {
+				return (String) obj;
+			}
+			throw new Throwable(Util.toString(Util.getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	public void testAppend() {
+		//
+		Assertions.assertDoesNotThrow(() -> append(null, ' '));
+		//
+	}
+
+	private static void append(final StringBuilder instance, final char c) throws Throwable {
+		try {
+			METHOD_APPEND.invoke(null, instance, c);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	public void testTestAndApply() throws Throwable {
+		//
+		Assertions.assertNull(testAndApply(null, null, null, null));
+		//
+		Assertions.assertNull(testAndApply(Predicates.alwaysTrue(), null, null, null));
+		//
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse)
+			throws Throwable {
+		try {
+			return (R) METHOD_TEST_AND_APPLY.invoke(null, predicate, value, functionTrue, functionFalse);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+}
