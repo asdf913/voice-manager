6491dce89fc1d9aabd85756ea4f9218b226bfa1e
asdf913 <asdf913@gmail.com>
2024-01-03 12:44:47 +0900

    Apply rule "Conditionally executed code should be reachable"

--- src/main/java/org/springframework/beans/factory/Util.java
+++ src/main/java/org/springframework/beans/factory/Util.java
@@ -469,7 +469,7 @@ abstract class Util {
 		return instance != null ? instance.orElse(value) : value;
 	}
 
-	static <T> Optional<T> findFirst(final Stream<T> instance) {
+	static <T> Optional<T> findFirst(@Nullable final Stream<T> instance) {
 		return instance != null ? instance.findFirst() : null;
 	}
 
