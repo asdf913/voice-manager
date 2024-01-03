d0d818caa99d4922d62845e69710a99eaa3a4d3c
asdf913 <asdf913@gmail.com>
2024-01-03 12:45:47 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenLinkMapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenLinkMapFactoryBean.java
@@ -19,11 +19,9 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.annotation.Nullable;
 
