71ad55ade94f144b1f190ca3eb68844d1a765ef4
asdf913 <asdf913@gmail.com>
2024-01-03 11:48:33 +0900

    Apply rule ""@NonNull" values should not be set to null"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenLinkMapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenLinkMapFactoryBean.java
@@ -375,6 +375,7 @@ public class OtoYakuNoHeyaYomikataJitenLinkMapFactoryBean implements FactoryBean
 		//
 	}
 
+	@Nullable
 	private static String handleHSSFCell(@Nullable final Cell instance, final FormulaEvaluator formulaEvaluator) {
 		//
 		final CellType cellType = instance != null ? instance.getCellType() : null;
