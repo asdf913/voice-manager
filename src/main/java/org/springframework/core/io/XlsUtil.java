49009a9779e19468e94b6a8b08921713d94d0353
asdf913 <asdf913@gmail.com>
2024-01-06 00:10:22 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/springframework/core/io/XlsUtil.java
+++ src/main/java/org/springframework/core/io/XlsUtil.java
@@ -250,7 +250,7 @@ public abstract class XlsUtil {
 		return clz != null ? clz.getDeclaredMethod(name, parameterTypes) : null;
 	}
 
-	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, @Nullable final T value,
 			final FailableFunction<T, R, E> functionTrue, @Nullable final FailableFunction<T, R, E> functionFalse)
 			throws E {
 		return test(predicate, value) ? FailableFunctionUtil.apply(functionTrue, value)
