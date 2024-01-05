2bb55adbf94bf72b2ad4cf65c7b3c446afafbbcb
asdf913 <asdf913@gmail.com>
2024-01-05 20:37:08 +0900

    Add "org.apache.nutch:nutch" testing dependency Add "org.springframework.core.io.XlsUtil" class

---
+++ src/main/java/org/springframework/core/io/XlsUtil.java
@@ -0,0 +1,291 @@
+package org.springframework.core.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+import org.apache.bcel.classfile.ClassParser;
+import org.apache.bcel.classfile.ClassParserUtil;
+import org.apache.bcel.classfile.ConstantPool;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.classfile.JavaClassUtil;
+import org.apache.bcel.generic.ARETURN;
+import org.apache.bcel.generic.ConstantPoolGen;
+import org.apache.bcel.generic.Instruction;
+import org.apache.bcel.generic.InstructionListUtil;
+import org.apache.bcel.generic.LDC;
+import org.apache.bcel.generic.MethodGen;
+import org.apache.bcel.generic.MethodGenUtil;
+import org.apache.commons.collections4.IterableUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.function.FailableFunctionUtil;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.poi.poifs.filesystem.DirectoryEntry;
+import org.apache.poi.poifs.filesystem.DirectoryNode;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerUtil;
+
+import com.j256.simplemagic.ContentInfo;
+import com.j256.simplemagic.ContentInfoUtil;
+
+public abstract class XlsUtil {
+
+	private static final Logger LOG = LoggerFactory.getLogger(XlsUtil.class);
+
+	private XlsUtil() {
+	}
+
+	public static boolean isXls(final InputStreamSource iss) throws IOException {
+		//
+		ContentInfo ci = null;
+		//
+		try (final InputStream is = InputStreamSourceUtil.getInputStream(iss)) {
+			//
+			ci = testAndApply(Objects::nonNull, is, x -> new ContentInfoUtil().findMatch(x), null);
+			//
+		} // try
+			//
+		if (!Objects.equals(getMessage(ci), "OLE 2 Compound Document")) {
+			//
+			return false;
+			//
+		} // if
+			//
+		DirectoryNode directoryNode = null;
+		//
+		try (final InputStream is = InputStreamSourceUtil.getInputStream(iss);
+				final POIFSFileSystem poifsFileSystem = new POIFSFileSystem(is)) {
+			//
+			directoryNode = poifsFileSystem.getRoot();
+			//
+		} // try
+			//
+		Class<?> clz = null;
+		//
+		try {
+			//
+			clz = Class.forName("org.apache.poi.hssf.usermodel.HSSFWorkbook");
+			//
+		} catch (final ClassNotFoundException e) {
+			//
+			LoggerUtil.error(LOG, e.getMessage(), e);
+			//
+		} // try
+			//
+		try (final InputStream is = getResourceAsStream(clz,
+				String.format("/%1$s.class", StringUtils.replace(getName(clz), ".", "/")))) {
+			//
+			JavaClass javaClass = null;
+			//
+			final Instruction[] instructions = InstructionListUtil
+					.getInstructions(MethodGenUtil.getInstructionList(testAndApply(Objects::nonNull,
+							JavaClassUtil.getMethod(
+									javaClass = ClassParserUtil.parse(
+											testAndApply(Objects::nonNull, is, x -> new ClassParser(x, null), null)),
+									getDeclaredMethod(clz, "getWorkbookDirEntryName", DirectoryNode.class)),
+							x -> new MethodGen(x, null, null), null)));
+			//
+			Instruction instruction = null;
+			//
+			ConstantPoolGen cpg = null;
+			//
+			final ConstantPool cp = getConstantPool(javaClass);
+			//
+			List<Object> entryNames = null;
+			//
+			for (int i = 0; instructions != null && i < instructions.length; i++) {
+				//
+				if ((instruction = instructions[i]) == null) {
+					//
+					continue;
+					//
+				} // if
+					//
+				if (instruction instanceof ARETURN ar && i > 0) {
+					//
+					if (instructions[i - 1] instanceof LDC ldc) {
+						//
+						if (cpg == null) {
+							//
+							cpg = ObjectUtils.getIfNull(cpg,
+									() -> testAndApply(Objects::nonNull, cp, ConstantPoolGen::new, null));
+							//
+						} // if
+							//
+						add(entryNames = ObjectUtils.getIfNull(entryNames, ArrayList::new), ldc.getValue(cpg));
+						//
+					} // if
+						//
+				} // if
+					//
+			} // for
+				//
+			for (int i = 0; entryNames != null && i < entryNames.size(); i++) {
+				//
+				if (hasEntryCaseInsensitive(directoryNode, toString(entryNames.get(i)))) {
+					//
+					return true;
+					//
+				} // if
+					//
+			} // for
+				//
+		} catch (final NoSuchMethodException e) {
+			//
+			LoggerUtil.error(LOG, e.getMessage(), e);
+			//
+		} // try
+			//
+		return false;
+		//
+	}
+
+	private static InputStream getResourceAsStream(final Class<?> clz, final String name) {
+		return clz != null && name != null ? clz.getResourceAsStream(name) : null;
+	}
+
+	private static boolean hasEntryCaseInsensitive(final DirectoryEntry instance, final String name) {
+		//
+		if (instance == null) {
+			//
+			return false;
+			//
+		} // if
+			//
+		final Class<?> clz = getClass(instance);
+		//
+		final String className = getName(clz);
+		//
+		if (Objects.equals(className, "org.apache.poi.poifs.filesystem.DirectoryNode") && name != null) {
+			//
+			final List<Field> fs = FieldUtils.getAllFieldsList(clz).stream()
+					.filter(f -> Objects.equals(getName(f), "_byUCName")).toList();
+			//
+			final int size = IterableUtils.size(fs);
+			//
+			if (size > 1) {
+				//
+				throw new IllegalStateException();
+				//
+			} // if
+				//
+			final Field f = size == 1 ? IterableUtils.get(fs, 0) : null;
+			//
+			try {
+				//
+				if (f != null) {
+					//
+					f.setAccessible(true);
+					//
+					if (f.get(instance) == null) {
+						//
+						return false;
+						//
+					} // if
+						//
+				} // if
+					//
+			} catch (final IllegalAccessException e) {
+				//
+				LoggerUtil.error(LOG, e.getMessage(), e);
+				//
+			} // try
+				//
+		} else if (Objects.equals(className, "org.apache.poi.poifs.filesystem.FilteringDirectoryNode")) {
+			//
+			final List<Field> fs = FieldUtils.getAllFieldsList(clz).stream()
+					.filter(f -> Objects.equals(getName(f), "excludes")).toList();
+			//
+			final int size = IterableUtils.size(fs);
+			//
+			if (size > 1) {
+				//
+				throw new IllegalStateException();
+				//
+			} // if
+				//
+			final Field f = size == 1 ? IterableUtils.get(fs, 0) : null;
+			//
+			try {
+				//
+				if (f != null) {
+					//
+					f.setAccessible(true);
+					//
+					if (f.get(instance) == null) {
+						//
+						return false;
+						//
+					} // if
+						//
+				} // if
+					//
+			} catch (final IllegalAccessException e) {
+				//
+				LoggerUtil.error(LOG, e.getMessage(), e);
+				//
+			} // try
+				//
+		} // if
+			//
+		return instance.hasEntryCaseInsensitive(name);
+		//
+	}
+
+	private static String getName(final Member instance) {
+		return instance != null ? instance.getName() : null;
+	}
+
+	private static Class<?> getClass(final Object instance) {
+		return instance != null ? instance.getClass() : null;
+	}
+
+	private static String getName(final Class<?> instance) {
+		return instance != null ? instance.getName() : null;
+	}
+
+	private static ConstantPool getConstantPool(final JavaClass instance) {
+		return instance != null ? instance.getConstantPool() : null;
+	}
+
+	private static String toString(final Object instance) {
+		return instance != null ? instance.toString() : null;
+	}
+
+	private static <E> void add(final Collection<E> items, final E item) {
+		if (items != null) {
+			items.add(item);
+		}
+	}
+
+	private static Method getDeclaredMethod(final Class<?> clz, final String name, final Class<?>... parameterTypes)
+			throws NoSuchMethodException {
+		return clz != null ? clz.getDeclaredMethod(name, parameterTypes) : null;
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse) throws E {
+		return test(predicate, value) ? FailableFunctionUtil.apply(functionTrue, value)
+				: FailableFunctionUtil.apply(functionFalse, value);
+	}
+
+	private static <T> boolean test(final Predicate<T> instance, final T value) {
+		return instance != null && instance.test(value);
+	}
+
+	private static String getMessage(final ContentInfo instance) {
+		return instance != null ? instance.getMessage() : null;
+	}
+
+}
---
+++ src/test/java/org/springframework/core/io/XlsUtilTest.java
@@ -0,0 +1,300 @@
+package org.springframework.core.io;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+import org.apache.bcel.classfile.ConstantPool;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.commons.lang3.stream.Streams.FailableStream;
+import org.apache.nutch.util.DeflateUtils;
+import org.apache.poi.poifs.filesystem.DirectoryEntry;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.usermodel.WorkbookUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+class XlsUtilTest {
+
+	private static Method METHOD_HAS_ENTRY_CASE_INSENSITIVE, METHOD_GET_CLASS, METHOD_TO_STRING, METHOD_ADD,
+			METHOD_GET_CONSTANT_POOL, METHOD_GET_DECLARED_METHOD, METHOD_GET_NAME, METHOD_TEST,
+			METHOD_GET_RESOURCE_AS_STREAM = null;
+
+	@BeforeAll
+	static void beforeClass() throws NoSuchMethodException, ClassNotFoundException {
+		//
+		final Class<?> clz = XlsUtil.class;
+		//
+		(METHOD_HAS_ENTRY_CASE_INSENSITIVE = clz.getDeclaredMethod("hasEntryCaseInsensitive", DirectoryEntry.class,
+				String.class)).setAccessible(true);
+		//
+		(METHOD_GET_CLASS = clz.getDeclaredMethod("getClass", Object.class)).setAccessible(true);
+		//
+		(METHOD_TO_STRING = clz.getDeclaredMethod("toString", Object.class)).setAccessible(true);
+		//
+		(METHOD_ADD = clz.getDeclaredMethod("add", Collection.class, Object.class)).setAccessible(true);
+		//
+		(METHOD_GET_CONSTANT_POOL = clz.getDeclaredMethod("getConstantPool", JavaClass.class)).setAccessible(true);
+		//
+		(METHOD_GET_DECLARED_METHOD = clz.getDeclaredMethod("getDeclaredMethod", Class.class, String.class,
+				Class[].class)).setAccessible(true);
+		//
+		(METHOD_GET_NAME = clz.getDeclaredMethod("getName", Member.class)).setAccessible(true);
+		//
+		(METHOD_TEST = clz.getDeclaredMethod("test", Predicate.class, Object.class)).setAccessible(true);
+		//
+		(METHOD_GET_RESOURCE_AS_STREAM = clz.getDeclaredMethod("getResourceAsStream", Class.class, String.class))
+				.setAccessible(true);
+		//
+	}
+
+	@Test
+	void testIsXls() throws IOException {
+		//
+		Assertions.assertFalse(XlsUtil.isXls(null));
+		//
+		final Decoder decoder = Base64.getDecoder();
+		//
+		Assertions.assertFalse(XlsUtil.isXls(new ByteArrayResource(DeflateUtils.inflate(decoder != null
+				? decoder.decode(
+						"eJztmVtMXEUYx785e3bZxRaWBWmlKFu6AlLu2BZslWsBty5QQGmt1iy3sgq7yCXSxAejMfHBJhgfTBoT0wTjg6ZBffPF+iBvXmLSh/pUn9TEh2p84aGs/2/OHFiW21lsSqvnT37MObNn5vtm5pvZ2Tk//pB188rneb9Qko6Tg5bjHnIl5AmQad54iTSVtxyPx83suK37SrdVymOoY/ycgMc8DbiBB6SDB8AesBdkqDH3qtTW/aseiuFvmvx0kqJIJ+li8lKwpXIRMYn1WSmzbPE5q7Lt79y+uX6nOv/5e4DnfxbwgWyQAx4kjgmifWA/eAjkgQMgHzys7BUg9avrQqSHQAA8CopAMSgBj4FScBiUgXJQASpBFagGNaAWPA6OgKPgGKgD9eAJ4u8zohPgSfAUaACNoAk0gxbQCk6CNtAOOsDTIAhOgWdACHSCLtANToMe0Av6wLPgOdAPzoCzqo3n6N5cKwW8cqQbMeTyaDImrhmh0cbjF4oMTsamYiPT/v7Y5FB5a+yVmfHh6LSMiVAv57XGBmUk8HUFbuTnFXX0d/0Xr24agisSxjZix/Ih6tJRy7nVnQmdR2j+Gdc4RSx3Yn2bpHEK0xgZcVwdEKUB0RTgqGkupa6gg06DluB+Gu/w6FPgVIdG3UGdoh1Z+jQ4H3RSGJ+/1FGvb+lPGy0XCNIEx1AaImgYdocogrX1AvHsyaTs+VuUMz9LroDADOkKumDcBeP5MJhGhqF8xHKzl+vhmPYgQmOo5SJmTB9qnMU6zbPPS76hHCFQI+be/LvEM6rSK4RP+HGtI14jNCWf1TBPddTtRswXot5C0Sj9a4F3E3gigvqj0j8fapuV/u2Bfz7ZKUfdgoty+190iyLZwhrBs9SJ+RGFZ+wT32XLUoYlNbq4kwuGjDBeUjS3scqY1+YOU6hU8yZkdBurkFyG2nPnzGe2VB+aMw6XptAdnUhfQ8pfcxwC3MgjqMdKzPWiw8dpACU5cGqLrFlvQrBFVLBF2OOs7cvwQA3A00mUMwfDD/vDsq4RC2067rLm3VmaQcmQDMhBGkUN6A+LZUPS1gXVtl7uD++2hTZtG9c1RWv7K7VWtMuNyyhKD3Ir9DlhpfDaVjQjPIX7Ela6bxESo7KC78u++cR8em2Nxp1Ob82tzzVV7FAXW68TtraRr+rO17l+1lsapJQ9ecP6o8txDqD1jnFQ3Xz7o7+Wuka9n77npsPFX95gP14nY8Xkz3l/wwtjIxl7NN6f8Pcb7zt4rzZExn5tgoym/36b6BF1XaXKmUq+9v92TLvW8LNIzN9If3wsRMjvQT/e6vnayDK6S63uGdQSjo2Fo3UbdGKWnkuVGWvbvJXi8Uz5zSHUtSvhOlnvyP+XVZWXLawM/EzO9o+t6KowevkrlZr6Ffdn/tWextCipaVwvRowFgMJ9n8Sxq7c1n9Ti0nxd7ek0Y1dsGrLlq17R3t32wFbuyu1z7P1v5Qwx39ptz2xdZeEX12OAIUDxAeoopxOVFFjN1Fzt0YFC29W+BcWmw4uRPVCcGguqgdALT4vqaGMDX4Dbvb7l3O0699d/7DigPf9D/D7t2zpKp/PO5PyXiDjPYNQeGn1t+5m+bZWdSff/3E/J79D2KiMPCHbZ1wLapHHmBPURQP0csr++zCqbJEPcuQJs0WZlgTsjtFwynZNpcM623WkYJ/9NU8aqqmPwmj5Tn3IUPZNWSnDvl5Rx+5O6qUZ9D8fJPPY83n+yMobk9U3A5upBPbNd4ZW7fMZ1Gfqul/aGqJWpIPSk2EZh1aVt4P2HwTmuZNzneXU+qNuB/afB9ZbuL1StZ/4/vcfnO3KVg==")
+				: null))));
+		//
+		try (final ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+			//
+			WorkbookUtil.write(WorkbookFactory.create(false), baos);
+			//
+			Assertions.assertTrue(XlsUtil.isXls(new ByteArrayResource(baos.toByteArray())));
+			//
+		} // try
+			//
+	}
+
+	@Test
+	void testHasEntryCaseInsensitive() throws Throwable {
+		//
+		Assertions.assertFalse(hasEntryCaseInsensitive(null, null));
+		//
+		Assertions.assertFalse(hasEntryCaseInsensitive(
+				cast(DirectoryEntry.class,
+						Narcissus.allocateInstance(Class.forName("org.apache.poi.poifs.filesystem.DirectoryNode"))),
+				""));
+		//
+		Assertions
+				.assertFalse(
+						hasEntryCaseInsensitive(
+								cast(DirectoryEntry.class,
+										Narcissus.allocateInstance(Class
+												.forName("org.apache.poi.poifs.filesystem.FilteringDirectoryNode"))),
+								""));
+		//
+	}
+
+	private static <T> T cast(final Class<T> clz, final Object instance) {
+		return clz != null && clz.isInstance(instance) ? clz.cast(instance) : null;
+	}
+
+	private static boolean hasEntryCaseInsensitive(final DirectoryEntry instance, final String name) throws Throwable {
+		try {
+			final Object obj = METHOD_HAS_ENTRY_CASE_INSENSITIVE.invoke(null, instance, name);
+			if (obj instanceof Boolean) {
+				return (Boolean) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testGetClass() throws Throwable {
+		//
+		Assertions.assertNull(getClass(null));
+		//
+	}
+
+	private static Class<?> getClass(final Object instance) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_CLASS.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Class) {
+				return (Class<?>) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testToString() throws Throwable {
+		//
+		Assertions.assertNull(toString(null));
+		//
+	}
+
+	private static String toString(final Object instance) throws Throwable {
+		try {
+			final Object obj = METHOD_TO_STRING.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof String) {
+				return (String) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testAdd() throws Throwable {
+		//
+		Assertions.assertDoesNotThrow(() -> add(null, null));
+		//
+	}
+
+	private static <E> void add(final Collection<E> items, final E item) throws Throwable {
+		try {
+			METHOD_ADD.invoke(null, items, item);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testGetDeclaredMethod() throws Throwable {
+		//
+		Assertions.assertNull(getDeclaredMethod(null, null));
+		//
+	}
+
+	private static Method getDeclaredMethod(final Class<?> clz, final String name, final Class<?>... parameterTypes)
+			throws Throwable {
+		try {
+			final Object obj = METHOD_GET_DECLARED_METHOD.invoke(null, clz, name, parameterTypes);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Method) {
+				return (Method) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testGetName() {
+		//
+		new FailableStream<>(Arrays.stream(XlsUtil.class.getDeclaredMethods()))
+				.filter(m -> m != null && Objects.equals(getName(m), "getName") && Modifier.isStatic(m.getModifiers())
+						&& m.getParameterCount() == 1)
+				.forEach(m -> {
+					//
+					if (m == null) {
+						//
+						return;
+						//
+					} // if
+						//
+					m.setAccessible(true);
+					//
+					m.invoke(null, (Object) null);
+					//
+				});
+		//
+	}
+
+	private static String getName(final Member instance) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_NAME.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof String) {
+				return (String) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testGetConstantPool() throws Throwable {
+		//
+		Assertions.assertNull(getConstantPool(null));
+		//
+	}
+
+	private static ConstantPool getConstantPool(final JavaClass instance) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_CONSTANT_POOL.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof ConstantPool) {
+				return (ConstantPool) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testTest() throws Throwable {
+		//
+		Assertions.assertFalse(test(null, null));
+		//
+	}
+
+	private static <T> boolean test(final Predicate<T> instance, final T value) throws Throwable {
+		try {
+			final Object obj = METHOD_TEST.invoke(null, instance, value);
+			if (obj instanceof Boolean) {
+				return ((Boolean) obj).booleanValue();
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testGetResourceAsStream() throws Throwable {
+		//
+		Assertions.assertNull(getResourceAsStream(null, null));
+		//
+		Assertions.assertNull(getResourceAsStream(Class.class, null));
+		//
+	}
+
+	private static InputStream getResourceAsStream(final Class<?> clz, final String name) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_RESOURCE_AS_STREAM.invoke(null, clz, name);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof InputStream) {
+				return (InputStream) obj;
+			}
+			throw new Throwable(toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+}
