741da42ab0ac6797867c375019a8e05e9ab5a073
asdf913 <asdf913@gmail.com>
2024-01-05 15:07:55 +0900

    Appy rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/beans/factory/TiZuKiGouKanjiHiraganaMapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/TiZuKiGouKanjiHiraganaMapFactoryBean.java
@@ -5,7 +5,6 @@ import java.net.URI;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Predicate;
