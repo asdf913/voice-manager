f3030a7b046fd20bf383d16dea404f1bd03f2951
asdf913 <asdf913@gmail.com>
2024-01-13 01:46:16 +0900

    Update "org.springframework.beans.factory.TiZuKiGouKanjiHiraganaMapFactoryBeanTest" class

--- src/test/java/org/springframework/beans/factory/TiZuKiGouKanjiHiraganaMapFactoryBeanTest.java
+++ src/test/java/org/springframework/beans/factory/TiZuKiGouKanjiHiraganaMapFactoryBeanTest.java
@@ -9,7 +9,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URI;
 import java.net.URL;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Map.Entry;
