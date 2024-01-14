acc8bc7323a0bb70f331fb465645cb0206e8f1fd
asdf913 <asdf913@gmail.com>
2024-01-15 01:02:09 +0900

    Apply rule "Cognitive Complexity of methods should not be too high"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenNipponIkaJinmeiJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenNipponIkaJinmeiJitenMultimapFactoryBean.java
@@ -67,7 +67,7 @@ public class OtoYakuNoHeyaYomikataJitenNipponIkaJinmeiJitenMultimapFactoryBean
 					//
 				} // if
 					//
-				for (int i = 0; children != null && i < children.size(); i++) {
+				for (int i = 0; i < IterableUtils.size(children); i++) {
 					//
 					if (Util.matches(pattern.matcher(ElementUtil.text(element = IterableUtils.get(children, i))))) {
 						//
