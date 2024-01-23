176af586b436916a813c96bbdf40a6fab206ac77
asdf913 <asdf913@gmail.com>
2024-01-23 13:36:28 +0900

    Apply rule "Conditionally executed code should be reachable"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenSansuuSuugakuYougoYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenSansuuSuugakuYougoYomikataJitenMultimapFactoryBean.java
@@ -124,7 +124,7 @@ public class OtoYakuNoHeyaYomikataJitenSansuuSuugakuYougoYomikataJitenMultimapFa
 		//
 	}
 
-	private static int length(final Object[] instance) {
+	private static int length(@Nullable final Object[] instance) {
 		return instance != null ? instance.length : 0;
 	}
 
