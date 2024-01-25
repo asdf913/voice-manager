59f8a59bc1366df84de1172719f6e8f83d317654
asdf913 <asdf913@gmail.com>
2024-01-25 15:21:49 +0900

    Apply rule ""@NonNull" values should not be set to null"

--- src/main/java/org/springframework/context/support/Util.java
+++ src/main/java/org/springframework/context/support/Util.java
@@ -132,6 +132,7 @@ public abstract class Util {
 		return instance != null ? instance.getKey() : null;
 	}
 
+	@Nullable
 	static <V> V getValue(@Nullable final Entry<?, V> instance) {
 		return instance != null ? instance.getValue() : null;
 	}
