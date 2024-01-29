44501bdd28c2f77b5931e87ed6c9523b2540bc82
asdf913 <asdf913@gmail.com>
2024-01-29 23:43:48 +0900

    Fix "java.lang.UnsatisfiedLinkError,Unable to load library 'ole32'"

--- src/test/java/org/springframework/context/support/UtilTest.java
+++ src/test/java/org/springframework/context/support/UtilTest.java
@@ -635,7 +635,8 @@ class UtilTest {
 						"com.sun.jna.platform.win32.Wininet", "com.sun.jna.platform.win32.Winsock2",
 						"com.sun.jna.platform.win32.Winspool", "com.sun.jna.platform.win32.Wtsapi32",
 						"com.sun.jna.platform.win32.Wtsapi32$WTS_SESSION_INFO",
-						"com.sun.jna.platform.win32.Wtsapi32$WTS_PROCESS_INFO_EX");
+						"com.sun.jna.platform.win32.Wtsapi32$WTS_PROCESS_INFO_EX",
+						"com.sun.jna.platform.win32.Wtsapi32$WTS_INFO_CLASS");
 				//
 				commonPrefix = Unit
 						.with(StringUtils.getCommonPrefix(list != null ? list.toArray(new String[] {}) : null));
