a6c74a1829bc6a396455e98053b58c738e63cbb6
asdf913 <asdf913@gmail.com>
2024-01-29 23:15:39 +0900

    Fix "java.lang.UnsatisfiedLinkError,Unable to load library 'user32'"

--- src/test/java/org/springframework/context/support/UtilTest.java
+++ src/test/java/org/springframework/context/support/UtilTest.java
@@ -630,7 +630,7 @@ class UtilTest {
 						"com.sun.jna.platform.win32.Psapi", "com.sun.jna.platform.win32.Rasapi32",
 						"com.sun.jna.platform.win32.Secur32", "com.sun.jna.platform.win32.SetupApi",
 						"com.sun.jna.platform.win32.Shell32", "com.sun.jna.platform.win32.Shlwapi",
-						"com.sun.jna.platform.win32.Sspi");
+						"com.sun.jna.platform.win32.Sspi", "com.sun.jna.platform.win32.User32");
 				//
 				commonPrefix = Unit
 						.with(StringUtils.getCommonPrefix(list != null ? list.toArray(new String[] {}) : null));
