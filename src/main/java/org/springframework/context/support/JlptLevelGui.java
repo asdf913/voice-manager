c039c4e629258ac6fa137a5c17db1302d36c9e39
asdf913 <asdf913@gmail.com>
2024-01-30 00:05:26 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/context/support/JlptLevelGui.java
+++ src/main/java/org/springframework/context/support/JlptLevelGui.java
@@ -29,7 +29,6 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
--- src/main/java/org/springframework/context/support/OnlineNHKJapanesePronunciationAccentGui.java
+++ src/main/java/org/springframework/context/support/OnlineNHKJapanesePronunciationAccentGui.java
@@ -35,7 +35,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
