b41cd63d323c5d7593f1c23f9490eac177a1582b
asdf913 <asdf913@gmail.com>
2024-01-29 23:06:26 +0900

    Fix "java.lang.UnsatisfiedLinkError,Unable to load library 'setupapi'"

--- src/test/java/org/springframework/context/support/UtilTest.java
+++ src/test/java/org/springframework/context/support/UtilTest.java
@@ -628,7 +628,7 @@ class UtilTest {
 						"com.sun.jna.platform.win32.OleAuto", "com.sun.jna.platform.win32.OpenGL32",
 						"com.sun.jna.platform.win32.Pdh", "com.sun.jna.platform.win32.PowrProf",
 						"com.sun.jna.platform.win32.Psapi", "com.sun.jna.platform.win32.Rasapi32",
-						"com.sun.jna.platform.win32.Secur32");
+						"com.sun.jna.platform.win32.Secur32", "com.sun.jna.platform.win32.SetupApi");
 				//
 				commonPrefix = Unit
 						.with(StringUtils.getCommonPrefix(list != null ? list.toArray(new String[] {}) : null));
