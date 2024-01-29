1a6f91cc442d01003baf3930493174895b8a9021
asdf913 <asdf913@gmail.com>
2024-01-29 23:12:51 +0900

    Fix "java.lang.NoClassDefFoundError: Could not initialize class com.sun.jna.platform.win32.Kernel32,java.lang.UnsatisfiedLinkError: Unable to load library 'kernel32'"

--- src/test/java/org/springframework/context/support/UtilTest.java
+++ src/test/java/org/springframework/context/support/UtilTest.java
@@ -629,7 +629,8 @@ class UtilTest {
 						"com.sun.jna.platform.win32.Pdh", "com.sun.jna.platform.win32.PowrProf",
 						"com.sun.jna.platform.win32.Psapi", "com.sun.jna.platform.win32.Rasapi32",
 						"com.sun.jna.platform.win32.Secur32", "com.sun.jna.platform.win32.SetupApi",
-						"com.sun.jna.platform.win32.Shell32", "com.sun.jna.platform.win32.Shlwapi");
+						"com.sun.jna.platform.win32.Shell32", "com.sun.jna.platform.win32.Shlwapi",
+						"com.sun.jna.platform.win32.Sspi");
 				//
 				commonPrefix = Unit
 						.with(StringUtils.getCommonPrefix(list != null ? list.toArray(new String[] {}) : null));
