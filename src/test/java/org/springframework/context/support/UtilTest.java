1d4ae6792f4fe9f71d411afc98b8e9fef2a8b7f8
asdf913 <asdf913@gmail.com>
2024-01-29 23:21:20 +0900

    Fix "java.lang.UnsatisfiedLinkError,Unable to load library 'version'"

--- src/test/java/org/springframework/context/support/UtilTest.java
+++ src/test/java/org/springframework/context/support/UtilTest.java
@@ -630,7 +630,8 @@ class UtilTest {
 						"com.sun.jna.platform.win32.Psapi", "com.sun.jna.platform.win32.Rasapi32",
 						"com.sun.jna.platform.win32.Secur32", "com.sun.jna.platform.win32.SetupApi",
 						"com.sun.jna.platform.win32.Shell32", "com.sun.jna.platform.win32.Shlwapi",
-						"com.sun.jna.platform.win32.Sspi", "com.sun.jna.platform.win32.User32");
+						"com.sun.jna.platform.win32.Sspi", "com.sun.jna.platform.win32.User32",
+						"com.sun.jna.platform.win32.Version");
 				//
 				commonPrefix = Unit
 						.with(StringUtils.getCommonPrefix(list != null ? list.toArray(new String[] {}) : null));
