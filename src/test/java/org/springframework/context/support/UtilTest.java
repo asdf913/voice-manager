72c31385f4029e3da57ddc169fecea73627336d7
asdf913 <asdf913@gmail.com>
2024-01-29 21:36:25 +0900

    Fix "java.lang.UnsatisfiedLinkError,Unable to load library 'Cryptui'"

--- src/test/java/org/springframework/context/support/UtilTest.java
+++ src/test/java/org/springframework/context/support/UtilTest.java
@@ -613,7 +613,7 @@ class UtilTest {
 						"com.sun.jna.platform.win32.COM.TypeInfoUtil", "com.sun.jna.platform.win32.COM.TypeLibUtil",
 						"com.sun.jna.platform.win32.COM.util.Factory",
 						"com.sun.jna.platform.win32.COM.util.ObjectFactory", "com.sun.jna.platform.win32.Cfgmgr32",
-						"com.sun.jna.platform.win32.Crypt32"));
+						"com.sun.jna.platform.win32.Crypt32", "com.sun.jna.platform.win32.Cryptui"));
 				//
 			} // if
 				//
