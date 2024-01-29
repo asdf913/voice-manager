134c7380152ab387e081377f15b6c20312c2283e
asdf913 <asdf913@gmail.com>
2024-01-29 23:24:44 +0900

    Fix "java.lang.UnsatisfiedLinkError,Unable to load library 'wevtapi'"

--- src/test/java/org/springframework/context/support/UtilTest.java
+++ src/test/java/org/springframework/context/support/UtilTest.java
@@ -631,7 +631,7 @@ class UtilTest {
 						"com.sun.jna.platform.win32.Secur32", "com.sun.jna.platform.win32.SetupApi",
 						"com.sun.jna.platform.win32.Shell32", "com.sun.jna.platform.win32.Shlwapi",
 						"com.sun.jna.platform.win32.Sspi", "com.sun.jna.platform.win32.User32",
-						"com.sun.jna.platform.win32.Version");
+						"com.sun.jna.platform.win32.Version", "com.sun.jna.platform.win32.Wevtapi");
 				//
 				commonPrefix = Unit
 						.with(StringUtils.getCommonPrefix(list != null ? list.toArray(new String[] {}) : null));
