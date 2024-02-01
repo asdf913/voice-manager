5bb7bedacdf8cd1b1d7a97751d35d0b64ddee86d
asdf913 <asdf913@gmail.com>
2024-02-01 18:39:15 +0900

    Update "org.springframework.beans.factory.OtoYakuNoHeyaYomikataJitenFrameMultiMapFactoryBeanTest.testCreateFrame()" method

--- src/test/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenFrameMultiMapFactoryBeanTest.java
+++ src/test/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenFrameMultiMapFactoryBeanTest.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.function.Predicate;
 
@@ -113,8 +114,11 @@ class OtoYakuNoHeyaYomikataJitenFrameMultiMapFactoryBeanTest {
 			//
 		} // if
 			//
-		Assertions.assertEquals("{\"name\":null,\"src\":null}",
-				ObjectMapperUtil.writeValueAsString(objectMapper, createFrame(null)));
+		final String json = ObjectMapperUtil.writeValueAsString(objectMapper, createFrame(null));
+		//
+		Assertions.assertTrue(
+				Util.contains(Arrays.asList("{\"name\":null,\"src\":null}", "{\"src\":null,\"name\":null}"), json),
+				json);
 		//
 	}
 
