fc34405a498b5d3f2dc198af3db31bd017ff9635
asdf913 <asdf913@gmail.com>
2024-02-13 22:54:36 +0900

    Apply rule "Boolean expressions should not be gratuitous"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenKisyoYougoYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenKisyoYougoYomikataJitenMultimapFactoryBean.java
@@ -272,7 +272,7 @@ public class OtoYakuNoHeyaYomikataJitenKisyoYougoYomikataJitenMultimapFactoryBea
 		//
 	}
 
-	private static boolean or(final boolean a, final boolean b, final boolean... bs) {
+	private static boolean or(final boolean a, final boolean b, @Nullable final boolean... bs) {
 		//
 		boolean result = a || b;
 		//
