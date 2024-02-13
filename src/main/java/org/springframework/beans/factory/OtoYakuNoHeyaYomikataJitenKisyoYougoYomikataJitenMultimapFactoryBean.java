6075b78003507d9d0426b771ba0f798a0e5623a8
asdf913 <asdf913@gmail.com>
2024-02-14 02:55:42 +0900

    Apply rule ""@NonNull" values should not be set to null"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenKisyoYougoYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenKisyoYougoYomikataJitenMultimapFactoryBean.java
@@ -326,6 +326,7 @@ public class OtoYakuNoHeyaYomikataJitenKisyoYougoYomikataJitenMultimapFactoryBea
 		//
 	}
 
+	@Nullable
 	private static IValue0<Multimap<String, String>> createMultimap4(final String s1, final String s2) {
 		//
 		IValue0<Multimap<String, String>> multimap = null;
