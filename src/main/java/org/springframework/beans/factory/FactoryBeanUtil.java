c56c296fcc3879bd9be7ec016a5418d34da75089
asdf913 <asdf913@gmail.com>
2024-02-29 17:36:37 +0900

    Apply rule ""@NonNull" values should not be set to null"

--- src/main/java/org/springframework/beans/factory/FactoryBeanUtil.java
+++ src/main/java/org/springframework/beans/factory/FactoryBeanUtil.java
@@ -4,6 +4,7 @@ import javax.annotation.Nullable;
 
 public interface FactoryBeanUtil {
 
+	@Nullable
 	static Class<?> getObjectType(@Nullable final FactoryBean<?> instance) {
 		return instance != null ? instance.getObjectType() : null;
 	}
