87c3e7610ce577609c02a72c88fc44a7af7ef841
asdf913 <asdf913@gmail.com>
2024-02-29 17:52:27 +0900

    Update "org.springframework.beans.factory.FactoryBeanUtil.getObjectType(org.springframework.beans.factory.FactoryBean)" method

--- src/main/java/org/springframework/beans/factory/FactoryBeanUtil.java
+++ src/main/java/org/springframework/beans/factory/FactoryBeanUtil.java
@@ -2,11 +2,50 @@ package org.springframework.beans.factory;
 
 import javax.annotation.Nullable;
 
+import org.apache.commons.lang3.reflect.FieldUtils;
+
 public interface FactoryBeanUtil {
 
 	@Nullable
 	static Class<?> getObjectType(@Nullable final FactoryBean<?> instance) {
-		return instance != null ? instance.getObjectType() : null;
+		//
+		if (instance == null) {
+			//
+			return null;
+			//
+		} // if
+			//
+		try {
+			//
+			final Class<?> clz = Util.getClass(instance);
+			//
+			if (Util.isAssignableFrom(clz, Class.forName("org.springframework.aop.framework.ProxyFactoryBean"))
+					&& FieldUtils.readField(instance, "listeners", true) == null) {
+				//
+				return null;
+				//
+			} else if (Util.isAssignableFrom(clz, Class.forName("org.springframework.aop.scope.ScopedProxyFactoryBean"))
+					&& FieldUtils.readField(instance, "proxy", true) == null
+					&& FieldUtils.readField(instance, "scopedTargetSource", true) == null) {
+				//
+				return null;
+				//
+			} else if (Util.isAssignableFrom(clz,
+					Class.forName("org.springframework.transaction.config.JtaTransactionManagerFactoryBean"))
+					&& FieldUtils.readField(instance, "transactionManager", true) == null) {
+				//
+				return null;
+				//
+			} // if
+				//
+		} catch (final ClassNotFoundException | IllegalAccessException e) {
+			//
+			throw new RuntimeException(e);
+			//
+		} // try
+			//
+		return instance.getObjectType();
+		//
 	}
 
 }
--- src/test/java/org/springframework/beans/factory/FactoryBeanUtilTest.java
+++ src/test/java/org/springframework/beans/factory/FactoryBeanUtilTest.java
@@ -1,15 +1,58 @@
 package org.springframework.beans.factory;
 
+import java.lang.reflect.Modifier;
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import io.github.classgraph.ClassInfo;
+import io.github.classgraph.ClassInfoUtil;
+import io.github.toolfactory.narcissus.Narcissus;
 
 class FactoryBeanUtilTest {
 
 	@Test
-	void testGetObjectType() {
+	void testGetObjectType() throws Throwable {
 		//
 		Assertions.assertNull(FactoryBeanUtil.getObjectType(null));
 		//
+		final List<ClassInfo> classInfos = ClassInfoUtil.getClassInfos();
+		//
+		if (classInfos == null || classInfos.iterator() == null) {
+			//
+			return;
+			//
+		} // if
+			//
+		String name = null;
+		//
+		Class<?> clz = null;
+		//
+		for (final ClassInfo classInfo : classInfos) {
+			//
+			try {
+				//
+				if (Util.isAssignableFrom(FactoryBean.class, Class.forName(name = classInfo.getName()))
+						&& !(clz = Class.forName(name)).isInterface() && !Modifier.isAbstract(clz.getModifiers())) {
+					//
+					final FactoryBean<?> factoryBean = Util.cast(FactoryBean.class, Narcissus.allocateInstance(clz));
+					//
+					System.out.println(name);
+					//
+					Assertions.assertDoesNotThrow(() -> FactoryBeanUtil.getObjectType(factoryBean), name);
+					//
+				} // if
+			} catch (final Throwable e) {
+				//
+				System.err.println(name);
+				//
+				throw e;
+				//
+			} // try
+				//
+		} // for
+			//
 	}
 
 }
