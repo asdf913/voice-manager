92bfe3bbe69c441b27d6ca1328fefed633f726a8
asdf913 <asdf913@gmail.com>
2024-04-16 14:14:04 +0900

    Apply rule "Boolean expressions should not be gratuitous"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenTennoYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenTennoYomikataJitenMultimapFactoryBean.java
@@ -124,7 +124,7 @@ public class OtoYakuNoHeyaYomikataJitenTennoYomikataJitenMultimapFactoryBean
 	}
 
 	@Nullable
-	private static Multimap<String, String> createMultimap(final Iterable<Element> tds) {
+	private static Multimap<String, String> createMultimap(@Nullable final Iterable<Element> tds) {
 		//
 		if (tds == null || IterableUtils.size(tds) < 5) {
 			//
