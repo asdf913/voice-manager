c6c99763a0336bb71be03ce41bf80485e1072ebc
asdf913 <asdf913@gmail.com>
2024-04-23 17:25:29 +0900

    Update "io.github.classgraph.ClassGraphUtil.scan(io.github.classgraph.ClassGraph)" method

--- src/main/java/io/github/classgraph/ClassGraphUtil.java
+++ src/main/java/io/github/classgraph/ClassGraphUtil.java
@@ -1,9 +1,34 @@
 package io.github.classgraph;
 
+import org.apache.commons.lang3.reflect.FieldUtils;
+
 public interface ClassGraphUtil {
 
 	static ScanResult scan(final ClassGraph instance) {
-		return instance != null ? instance.scan() : null;
+		//
+		if (instance == null) {
+			//
+			return null;
+			//
+		} // if
+			//
+		try {
+			//
+			if (FieldUtils.readDeclaredField(instance, "scanSpec", true) == null
+					|| FieldUtils.readDeclaredField(instance, "reflectionUtils", true) == null) {
+				//
+				return null;
+				//
+			} // if
+				//
+		} catch (final IllegalAccessException e) {
+			//
+			throw new RuntimeException(e);
+			//
+		} // try
+			//
+		return instance.scan();
+		//
 	}
 
 }
--- src/test/java/io/github/classgraph/ClassGraphUtilTest.java
+++ src/test/java/io/github/classgraph/ClassGraphUtilTest.java
@@ -1,15 +1,42 @@
 package io.github.classgraph;
 
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import io.github.toolfactory.narcissus.Narcissus;
 
 class ClassGraphUtilTest {
 
 	@Test
-	void testScan() {
+	void testScan() throws IllegalAccessException {
 		//
 		Assertions.assertNull(ClassGraphUtil.scan(null));
 		//
+		ClassGraph instance = cast(ClassGraph.class, Narcissus.allocateInstance(ClassGraph.class));
+		//
+		Assertions.assertNull(ClassGraphUtil.scan(instance));
+		//
+		if ((instance = new ClassGraph().enableClassInfo()) != null) {
+			//
+			FieldUtils.writeDeclaredField(instance, "reflectionUtils", null, true);
+			//
+			Assertions.assertNull(ClassGraphUtil.scan(instance));
+			//
+		} // if
+			//
+		if ((instance = new ClassGraph().enableClassInfo()) != null) {
+			//
+			FieldUtils.writeDeclaredField(instance, "topLevelLog", null, true);
+			//
+			Assertions.assertNotNull(ClassGraphUtil.scan(instance));
+			//
+		} // if
+			//
+	}
+
+	private static <T> T cast(final Class<T> clz, final Object instance) {
+		return clz != null && clz.isInstance(instance) ? clz.cast(instance) : null;
 	}
 
 }
