362aba51138b67dfe13275769cbd41ec7d153256
asdf913 <asdf913@gmail.com>
2024-04-23 15:34:08 +0900

    Apply rule "Conditionally executed code should be reachable"

--- src/main/java/org/springframework/beans/factory/config/BeanDefinitionUtil.java
+++ src/main/java/org/springframework/beans/factory/config/BeanDefinitionUtil.java
@@ -1,8 +1,10 @@
 package org.springframework.beans.factory.config;
 
+import javax.annotation.Nullable;
+
 public interface BeanDefinitionUtil {
 
-	static String getBeanClassName(final BeanDefinition instance) {
+	static String getBeanClassName(@Nullable final BeanDefinition instance) {
 		return instance != null ? instance.getBeanClassName() : null;
 	}
 
