ee2333c3081a4a3fbfd5ea70df1a29ecff4b571c
asdf913 <asdf913@gmail.com>
2024-07-07 08:35:34 +0900

    Add "LOG" field in "io.github.classgraph.ClassInfoUtil" class Add "apply(java.util.function.Function,java.lang.Object)","cast(java.lang.Class,java.lang.Object)","checkJavaClass(org.apache.bcel.classfile.JavaClass)","forName(java.lang.String)","getClassName(org.apache.bcel.generic.Type)","test(java.util.function.Predicate,java.lang.Object)","testAndApply(java.util.function.Predicate,java.lang.Object,java.util.function.Function,java.util.function.Function)" and "toJavaClass(java.lang.ClassLoader,java.lang.String)" methods in "io.github.classgraph.ClassInfoUtil" class Remove "cotntains(java.util.Collection,java.lang.Object)","getName(java.lang.Class) and "provider(java.nio.file.FileSystem)" methods from "io.github.classgraph.ClassInfoUtil" class Update "io.github.classgraph.ClassInfoUtil.getClassInfos()" method

--- src/test/java/io/github/classgraph/ClassInfoUtilTest.java
+++ src/test/java/io/github/classgraph/ClassInfoUtilTest.java
@@ -1,6 +1,5 @@
 package io.github.classgraph;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Member;
@@ -24,35 +23,48 @@ import org.apache.bcel.generic.ALOAD;
 import org.apache.bcel.generic.ARETURN;
 import org.apache.bcel.generic.ConstantPoolGen;
 import org.apache.bcel.generic.GOTO;
-import org.apache.bcel.generic.ICONST;
-import org.apache.bcel.generic.IFEQ;
 import org.apache.bcel.generic.IFNULL;
 import org.apache.bcel.generic.INVOKEINTERFACE;
 import org.apache.bcel.generic.INVOKEVIRTUAL;
-import org.apache.bcel.generic.IRETURN;
 import org.apache.bcel.generic.Instruction;
 import org.apache.bcel.generic.InstructionListUtil;
 import org.apache.bcel.generic.MethodGen;
 import org.apache.bcel.generic.MethodGenUtil;
+import org.apache.bcel.generic.Type;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import com.google.common.base.Predicates;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
 class ClassInfoUtilTest {
 
-	private static Method METHOD_REMOVE_IF = null;
+	private static Method METHOD_GET_CLASS_NAME, METHOD_FOR_NAME, METHOD_REMOVE_IF, METHOD_TEST_AND_APPLY,
+			METHOD_CAST = null;
 
 	@BeforeAll
 	static void beforeAll() throws ReflectiveOperationException {
 		//
-		(METHOD_REMOVE_IF = ClassInfoUtil.class.getDeclaredMethod("removeIf", Collection.class, Predicate.class))
-				.setAccessible(true);
+		final Class<?> clz = ClassInfoUtil.class;
+		//
+		(METHOD_GET_CLASS_NAME = clz.getDeclaredMethod("getClassName", Type.class)).setAccessible(true);
+		//
+		(METHOD_FOR_NAME = clz.getDeclaredMethod("forName", String.class)).setAccessible(true);
+		//
+		(METHOD_REMOVE_IF = clz.getDeclaredMethod("removeIf", Collection.class, Predicate.class)).setAccessible(true);
+		//
+		(METHOD_TEST_AND_APPLY = clz.getDeclaredMethod("testAndApply", Predicate.class, Object.class, Function.class,
+				Function.class)).setAccessible(true);
+		//
+		(METHOD_CAST = clz.getDeclaredMethod("cast", Class.class, Object.class)).setAccessible(true);
 		//
 	}
 
 	@Test
-	void testIif() throws IOException, IllegalAccessException, InvocationTargetException {
+	void testIif() throws Throwable {
 		//
 		final Class<?> clz = ClassInfoUtil.class;
 		//
@@ -164,7 +176,7 @@ class ClassInfoUtilTest {
 	}
 
 	@Test
-	void testBoolean() throws IOException, IllegalAccessException, InvocationTargetException {
+	void testBoolean() throws Throwable {
 		//
 		final Class<?> clz = ClassInfoUtil.class;
 		//
@@ -177,107 +189,44 @@ class ClassInfoUtilTest {
 			//
 			org.apache.bcel.classfile.Method method = null;
 			//
-			Instruction[] ins = null;
-			//
-			boolean[] bs = null;
-			//
-			List<java.lang.reflect.Method> ms = null;
+			Type[] argumentTypes = null;
 			//
 			for (int i = 0; methods != null && i < methods.length; i++) {
 				//
-				if ((method = methods[i]) == null || (ins = InstructionListUtil
-						.getInstructions(MethodGenUtil.getInstructionList(testAndApply(Objects::nonNull, method,
-								x -> new MethodGen(x, null, new ConstantPoolGen(FieldOrMethodUtil.getConstantPool(x))),
-								null)))) == null) {
+				if ((method = methods[i]) == null) {
 					//
 					continue;
 					//
 				} // if
 					//
-				if (bs == null) {
+				if ((argumentTypes = method.getArgumentTypes()) != null) {
 					//
-					bs = new boolean[10];
+					final org.apache.bcel.classfile.Method m = method;
 					//
-				} else {
+					final Type argumentType0 = argumentTypes.length > 0 ? argumentTypes[0] : null;
 					//
-					Arrays.fill(bs, false);
-					//
-				} // if
-					//
-				for (int j = 0; j < ins.length; j++) {
-					//
-					if (j == 0) {
+					if (argumentTypes.length == 1 && method.isStatic() && !method.isSynthetic()) {
 						//
-						bs[j] = ins[j] instanceof ALOAD;
+						Assertions.assertDoesNotThrow(() -> Narcissus.invokeStaticMethod(Narcissus.findMethod(clz,
+								FieldOrMethodUtil.getName(m), forName(getClassName(argumentType0))), (Object) null));
 						//
-					} else if (j == 1) {
+						if (Objects.equals("java.lang.Object", getClassName(argumentType0))) {
+							//
+							Assertions.assertDoesNotThrow(() -> Narcissus.invokeStaticMethod(Narcissus.findMethod(clz,
+									FieldOrMethodUtil.getName(m), forName(getClassName(argumentType0))), ""));
+							//
+						} // if
+							//
+					} else if (argumentTypes.length == 2 && method.isStatic()) {
 						//
-						bs[j] = ins[j] instanceof IFNULL;
+						final Type argumentType1 = argumentTypes[1];
 						//
-					} else if (j == 2) {
-						//
-						bs[j] = ins[j] instanceof ALOAD;
-						//
-					} else if (j == 3) {
-						//
-						bs[j] = ins[j] instanceof ALOAD;
-						//
-					} else if (j == 4) {
-						//
-						bs[j] = ins[j] instanceof INVOKEINTERFACE;
-						//
-					} else if (j == 5) {
-						//
-						bs[j] = ins[j] instanceof IFEQ;
-						//
-					} else if (j == 6) {
-						//
-						bs[j] = ins[j] instanceof ICONST;
-						//
-					} else if (j == 7) {
-						//
-						bs[j] = ins[j] instanceof GOTO;
-						//
-					} else if (j == 8) {
-						//
-						bs[j] = ins[j] instanceof ICONST;
-						//
-					} else if (j == 9) {
-						//
-						bs[j] = ins[j] instanceof IRETURN;
-						//
-					} else {
-						//
-						break;
+						Assertions.assertDoesNotThrow(() -> Narcissus.invokeStaticMethod(
+								Narcissus.findMethod(clz, FieldOrMethodUtil.getName(m),
+										forName(getClassName(argumentType0)), forName(getClassName(argumentType1))),
+								null, null));
 						//
 					} // if
-						//
-				} // for
-					//
-				final String name = FieldOrMethodUtil.getName(method);
-				//
-				if (Arrays.equals(new boolean[] { true, true, true, true, true, true, true, true, true, true }, bs)
-						&& (ms = filter(Arrays.stream(clz.getDeclaredMethods()), x -> Objects.equals(getName(x), name))
-								.toList()) != null
-						&& ms.iterator() != null) {
-					//
-					for (final java.lang.reflect.Method m : ms) {
-						//
-						if (m == null) {
-							//
-							continue;
-							//
-						} // if
-							//
-						m.setAccessible(true);
-						//
-						if (Modifier.isStatic(m.getModifiers())) {
-							//
-							Assertions.assertEquals(Boolean.FALSE, m.invoke(null, new Object[m.getParameterCount()]));
-							//
-						} // if
-							//
-					} // for
 						//
 				} // if
 					//
@@ -287,17 +236,41 @@ class ClassInfoUtilTest {
 			//
 	}
 
+	private static String getClassName(final Type instance) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_CLASS_NAME.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof String) {
+				return (String) obj;
+			}
+			throw new Throwable(Objects.toString(obj.getClass()));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	private static Class<?> forName(final String className) throws Throwable {
+		try {
+			final Object obj = METHOD_FOR_NAME.invoke(null, className);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Class) {
+				return (Class<?>) obj;
+			}
+			throw new Throwable(Objects.toString(obj.getClass()));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
 	private static <T, R> R testAndApply(final Predicate<T> predicate, final T value, final Function<T, R> functionTrue,
-			final Function<T, R> functionFalse) {
-		return test(predicate, value) ? apply(functionTrue, value) : apply(functionFalse, value);
-	}
-
-	private static <T> boolean test(final Predicate<T> instance, final T value) {
-		return instance != null && instance.test(value);
-	}
-
-	private static <T, R> R apply(final Function<T, R> instance, final T value) {
-		return instance != null ? instance.apply(value) : null;
+			final Function<T, R> functionFalse) throws Throwable {
+		try {
+			return (R) METHOD_TEST_AND_APPLY.invoke(null, predicate, value, functionTrue, functionFalse);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
 	}
 
 	private static String getName(final Member instance) {
@@ -315,11 +288,32 @@ class ClassInfoUtilTest {
 		//
 		Assertions.assertDoesNotThrow(() -> removeIf(Collections.emptySet(), null));
 		//
+		Assertions.assertDoesNotThrow(() -> removeIf(Collections.emptySet(), Predicates.alwaysFalse()));
+		//
 	}
 
 	private static <T> void removeIf(final Collection<T> instance, final Predicate<T> predicate) throws Throwable {
 		try {
 			METHOD_REMOVE_IF.invoke(null, instance, predicate);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testCast() throws Throwable {
+		//
+		final String string = "";
+		//
+		Assertions.assertSame(string, cast(String.class, string));
+		//
+		Assertions.assertNull(cast(Iterable.class, null));
+		//
+	}
+
+	private static <T> T cast(final Class<T> clz, final Object instance) throws Throwable {
+		try {
+			return (T) METHOD_CAST.invoke(null, clz, instance);
 		} catch (final InvocationTargetException e) {
 			throw e.getTargetException();
 		}
