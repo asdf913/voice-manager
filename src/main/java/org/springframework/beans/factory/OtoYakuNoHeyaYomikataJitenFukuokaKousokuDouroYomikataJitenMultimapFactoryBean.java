099583035de185d54c64aa7022a7dd28a6e2c9bc
asdf913 <asdf913@gmail.com>
2024-07-20 06:43:06 +0900

    Add "org.springframework.beans.factory.OtoYakuNoHeyaYomikataJitenFukuokaKousokuDouroYomikataJitenMultimapFactoryBean" class

---
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenFukuokaKousokuDouroYomikataJitenMultimapFactoryBean.java
@@ -0,0 +1,116 @@
+package org.springframework.beans.factory;
+
+import java.lang.Character.UnicodeBlock;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+import org.apache.commons.collections4.IterableUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.function.FailableFunctionUtil;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.ElementUtil;
+
+import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapUtil;
+
+public class OtoYakuNoHeyaYomikataJitenFukuokaKousokuDouroYomikataJitenMultimapFactoryBean
+		implements FactoryBean<Multimap<String, String>> {
+
+	@URL("https://hiramatu-hifuka.com/onyak/onyak2/tosiko01.html")
+	private String url = null;
+
+	@Override
+	public Multimap<String, String> getObject() throws Exception {
+		//
+		final Document document = testAndApply(Objects::nonNull,
+				testAndApply(StringUtils::isNotBlank, url, x -> new URI(x).toURL(), null), x -> Jsoup.parse(x, 0),
+				null);
+		//
+		final List<Element> es1 = ElementUtil.select(document, "table[border=\"1\"] tr td[colspan=\"3\"]");
+		//
+		Element e = null;
+		//
+		List<Element> nextElementSiblings = null;
+		//
+		String s1, s2 = null;
+		//
+		Multimap<String, String> multimap = null;
+		//
+		for (int i = 0; es1 != null && i < es1.size(); i++) {
+			//
+			if ((e = es1.get(i)) == null) {
+				//
+				continue;
+				//
+			} // if
+				//
+			if (Objects.equals(Collections.singletonList(UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS),
+					getUnicodeBlocks(s1 = ElementUtil.text(e)))
+					&& IterableUtils.size(nextElementSiblings = e.nextElementSiblings()) > 1
+					&& Objects.equals(Collections.singletonList(UnicodeBlock.HIRAGANA),
+							getUnicodeBlocks(s2 = ElementUtil.text(IterableUtils.get(nextElementSiblings, 1))))) {
+				//
+				MultimapUtil.put(multimap = ObjectUtils.getIfNull(multimap, LinkedHashMultimap::create), s1, s2);
+				//
+			} // if
+				//
+		} // for
+			//
+		return multimap;
+		//
+	}
+
+	private static List<UnicodeBlock> getUnicodeBlocks(final String string) {
+		//
+		final char[] cs = Util.toCharArray(string);
+		//
+		if (cs != null) {
+			//
+			List<UnicodeBlock> unicodeBlocks = null;
+			//
+			for (final char c : cs) {
+				//
+				testAndAccept((a, b) -> b != null && !Util.contains(a, b),
+						unicodeBlocks = ObjectUtils.getIfNull(unicodeBlocks, ArrayList::new), UnicodeBlock.of(c),
+						Util::add);
+				//
+			} // for
+				//
+			return unicodeBlocks;
+			//
+		} // if
+			//
+		return null;
+		//
+	}
+
+	private static <T, U> void testAndAccept(final BiPredicate<T, U> instance, final T t, final U u,
+			final BiConsumer<T, U> consumer) {
+		if (Util.test(instance, t, u)) {
+			Util.accept(consumer, t, u);
+		}
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse) throws E {
+		return Util.test(predicate, value) ? FailableFunctionUtil.apply(functionTrue, value)
+				: FailableFunctionUtil.apply(functionFalse, value);
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return Multimap.class;
+	}
+
+}
---
+++ src/test/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenFukuokaKousokuDouroYomikataJitenMultimapFactoryBeanTest.java
@@ -0,0 +1,231 @@
+package org.springframework.beans.factory;
+
+import java.lang.Character.UnicodeBlock;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.function.Predicate;
+import java.util.Objects;
+
+import org.apache.commons.collections4.IterableUtils;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.ReflectionUtils;
+
+import com.google.common.base.Predicates;
+import com.google.common.collect.Multimap;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+class OtoYakuNoHeyaYomikataJitenFukuokaKousokuDouroYomikataJitenMultimapFactoryBeanTest {
+
+	private static Method METHOD_GET_UNICODE_BLOCKS, METHOD_TEST_AND_APPLY = null;
+
+	@BeforeAll
+	static void beforeClass() throws NoSuchMethodException, ClassNotFoundException {
+		//
+		final Class<?> clz = OtoYakuNoHeyaYomikataJitenFukuokaKousokuDouroYomikataJitenMultimapFactoryBean.class;
+		//
+		(METHOD_GET_UNICODE_BLOCKS = clz.getDeclaredMethod("getUnicodeBlocks", String.class)).setAccessible(true);
+		//
+		(METHOD_TEST_AND_APPLY = clz.getDeclaredMethod("testAndApply", Predicate.class, Object.class,
+				FailableFunction.class, FailableFunction.class)).setAccessible(true);
+		//
+	}
+
+	private OtoYakuNoHeyaYomikataJitenFukuokaKousokuDouroYomikataJitenMultimapFactoryBean instance = null;
+
+	private boolean isSystemPropertiesContainsTestGetObject = false;
+
+	@BeforeEach
+	void beforeEach() {
+		//
+		instance = new OtoYakuNoHeyaYomikataJitenFukuokaKousokuDouroYomikataJitenMultimapFactoryBean();
+		//
+		isSystemPropertiesContainsTestGetObject = Util.containsKey(System.getProperties(),
+				"org.springframework.beans.factory.OtoYakuNoHeyaYomikataJitenFukuokaKousokuDouroYomikataJitenMultimapFactoryBeanTest.testGetObject");
+		//
+	}
+
+	@Test
+	void testGetObjectType() {
+		//
+		Assertions.assertEquals(Multimap.class, FactoryBeanUtil.getObjectType(instance));
+		//
+	}
+
+	@Test
+	void testGetObject() throws Exception {
+		//
+		Assertions.assertNull(instance != null ? instance.getObject() : null);
+		//
+		final Entry<Field, Object> entry = getFieldWithUrlAnnotationAndValue();
+		//
+		final Field url = entry != null ? entry.getKey() : null;
+		//
+		Narcissus.setObjectField(instance, url, "");
+		//
+		Assertions.assertNull(instance != null ? instance.getObject() : null);
+		//
+		Narcissus.setObjectField(instance, url, " ");
+		//
+		Assertions.assertNull(instance != null ? instance.getObject() : null);
+		//
+		if (isSystemPropertiesContainsTestGetObject) {
+			//
+			Narcissus.setObjectField(instance, url, entry != null ? entry.getValue() : null);
+			//
+			Assertions.assertDoesNotThrow(() -> instance != null ? instance.getObject() : null);
+			//
+		} // if
+			//
+	}
+
+	private static Entry<Field, Object> getFieldWithUrlAnnotationAndValue() throws ClassNotFoundException {
+		//
+		final Class<?> clz = Class.forName("org.springframework.beans.factory.URL");
+		//
+		final List<Field> fs = FieldUtils
+				.getAllFieldsList(OtoYakuNoHeyaYomikataJitenFukuokaKousokuDouroYomikataJitenMultimapFactoryBean.class);
+		//
+		Field f = null;
+		//
+		Annotation[] as = null;
+		//
+		InvocationHandler ih = null;
+		//
+		List<Field> fs2 = null;
+		//
+		int size = 0;
+		//
+		List<Method> ms = null;
+		//
+		Entry<Field, Object> entry = null;
+		//
+		for (int i = 0; fs != null && i < fs.size(); i++) {
+			//
+			if ((f = fs.get(i)) == null || (as = f != null ? f.getDeclaredAnnotations() : null) == null) {
+				//
+				continue;
+				//
+			} // if
+				//
+			for (final Annotation a : as) {
+				//
+				if (a == null) {
+					//
+					continue;
+					//
+				} // if
+					//
+				if ((ih = Proxy.isProxyClass(Util.getClass(a))
+						? Util.cast(InvocationHandler.class, Proxy.getInvocationHandler(a))
+						: null) != null) {
+					//
+					if ((size = IterableUtils.size(
+							fs2 = Util.toList(Util.filter(Util.stream(FieldUtils.getAllFieldsList(Util.getClass(ih))),
+									x -> Objects.equals(Util.getType(x), Class.class))))) == 1) {
+						//
+						if (Objects.equals(clz, Narcissus.getField(ih, IterableUtils.get(fs2, 0)))) {
+							//
+							if ((size = IterableUtils.size(
+									ms = Util.toList(Util.filter(Arrays.stream(Util.getClass(a).getDeclaredMethods()),
+											x -> Objects.equals(String.class, x != null ? x.getReturnType() : null)
+													&& !ReflectionUtils.isToStringMethod(x))))) == 1) {
+								//
+								if (entry == null) {
+									//
+									entry = Pair.of(f, Narcissus.invokeObjectMethod(a, IterableUtils.get(ms, 0)));
+									//
+								} else {
+									//
+									throw new IllegalStateException();
+									//
+								} // if
+									//
+							} else if (size > 1) {
+								//
+								throw new IllegalStateException();
+								//
+							} // if
+								//
+								//
+						} // if
+							//
+					} else if (size > 1) {
+						//
+						throw new IllegalStateException();
+						//
+					} // if
+						//
+				} // if
+					//
+			} // for
+				//
+		} // for
+			//
+		return entry;
+		//
+	}
+
+	@Test
+	void testGetUnicodeBlocks() throws Throwable {
+		//
+		Assertions.assertNull(getUnicodeBlocks(null));
+		//
+		if (!isSystemPropertiesContainsTestGetObject) {
+			//
+			Assertions.assertEquals(Collections.singletonList(UnicodeBlock.BASIC_LATIN), getUnicodeBlocks("  "));
+			//
+		} // if
+			//
+	}
+
+	private static List<UnicodeBlock> getUnicodeBlocks(final String string) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_UNICODE_BLOCKS.invoke(null, string);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof List) {
+				return (List) obj;
+			}
+			throw new Throwable(Util.toString(Util.getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testTestAndApply() throws Throwable {
+		//
+		if (!isSystemPropertiesContainsTestGetObject) {
+			//
+			Assertions.assertNull(testAndApply(Predicates.alwaysTrue(), null, null, null));
+			//
+		} // if
+			//
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse)
+			throws Throwable {
+		try {
+			return (R) METHOD_TEST_AND_APPLY.invoke(null, predicate, value, functionTrue, functionFalse);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+}
