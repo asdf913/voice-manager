d6d40801e14bac700ea545a27ca3f6a6c3d52993
asdf913 <asdf913@gmail.com>
2024-07-25 22:19:16 +0900

    Apply rule ""@NonNull" values should not be set to null"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenZenkokuKousokuDouroYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenZenkokuKousokuDouroYomikataJitenMultimapFactoryBean.java
@@ -83,6 +83,7 @@ public class OtoYakuNoHeyaYomikataJitenZenkokuKousokuDouroYomikataJitenMultimapF
 				: FailableFunctionUtil.apply(functionFalse, value);
 	}
 
+	@Nullable
 	private static List<UnicodeBlock> getUnicodeBlocks(final String string) {
 		//
 		final char[] cs = Util.toCharArray(string);
