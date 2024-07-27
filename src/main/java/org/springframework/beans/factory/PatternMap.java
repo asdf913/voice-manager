30db45b064895b92856a29856e8cac11ba9d44ea
asdf913 <asdf913@gmail.com>
2024-07-27 23:52:29 +0900

    Apply rule "Conditionally executed code should be reachable"

--- src/main/java/org/springframework/beans/factory/PatternMap.java
+++ src/main/java/org/springframework/beans/factory/PatternMap.java
@@ -2,11 +2,13 @@ package org.springframework.beans.factory;
 
 import java.util.regex.Pattern;
 
+import javax.annotation.Nullable;
+
 interface PatternMap {
 
 	Pattern getPattern(final String pattern);
 
-	static Pattern getPattern(final PatternMap instance, final String pattern) {
+	static Pattern getPattern(@Nullable final PatternMap instance, final String pattern) {
 		return instance != null ? instance.getPattern(pattern) : null;
 	}
 
