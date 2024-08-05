2235c8c047c840b0f87c911e3f39c73a85c543ab
asdf913 <asdf913@gmail.com>
2024-08-05 12:54:21 +0900

    Apply rule "Cognitive Complexity of methods should not be too high"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenIsekiKofunNoYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenIsekiKofunNoYomikataJitenMultimapFactoryBean.java
@@ -53,10 +53,10 @@ public class OtoYakuNoHeyaYomikataJitenIsekiKofunNoYomikataJitenMultimapFactoryB
 		//
 		for (int i = 0; i < IterableUtils.size(tables); i++) {
 			//
-			if (!StringUtils.equalsIgnoreCase(
+			if (Boolean.logicalOr(!StringUtils.equalsIgnoreCase(
 					ElementUtil.tagName(tbody = testAndApply(x -> IterableUtils.size(x) > 0,
 							ElementUtil.children(IterableUtils.get(tables, i)), x -> IterableUtils.get(x, 0), null)),
-					"tbody") || IterableUtils.size(children = ElementUtil.children(tbody)) <= 0) {
+					"tbody"), IterableUtils.size(children = ElementUtil.children(tbody)) <= 0)) {
 				//
 				continue;
 				//
