70acc739e48417e150bf84dcbea4f5dbdffd47ca
asdf913 <asdf913@gmail.com>
2024-08-19 15:51:24 +0900

    Apply rule ""@NonNull" values should not be set to null"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenMukashiNoShokugyouNoJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenMukashiNoShokugyouNoJitenMultimapFactoryBean.java
@@ -288,6 +288,7 @@ public class OtoYakuNoHeyaYomikataJitenMukashiNoShokugyouNoJitenMultimapFactoryB
 		//
 	}
 
+	@Nullable
 	private static IValue0<Multimap<String, String>> toMultimap3(final PatternMap patternMap, final String s) {
 		//
 		final Matcher m = Util.matcher(
