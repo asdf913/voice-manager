91c31a3a68dd6c40f9fdce076a6e409cbfe96b12
asdf913 <asdf913@gmail.com>
2024-08-20 19:28:36 +0900

    Apply rule "Whitespace and control characters in literals should be explicit"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBean.java
@@ -47,7 +47,7 @@ public class OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBean
 			for (final Node node : nodes) {
 				//
 				if (node == null || !(node instanceof TextNode)
-						|| (ss = StringUtils.split(Util.toString(node), "　　")) == null) {
+						|| (ss = StringUtils.split(Util.toString(node), "\u3000")) == null) {
 					//
 					continue;
 					//
