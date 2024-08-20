a2c9ea901e8d7354e44befc42860a6ea65550dbd
asdf913 <asdf913@gmail.com>
2024-08-20 19:22:12 +0900

    Add "org.springframework.beans.factory.OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBean" class

---
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBean.java
@@ -0,0 +1,90 @@
+package org.springframework.beans.factory;
+
+import java.net.URI;
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.annotation.Nullable;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.function.FailableFunctionUtil;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.NodeUtil;
+import org.jsoup.nodes.TextNode;
+
+import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapUtil;
+
+public class OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBean
+		implements FactoryBean<Multimap<String, String>> {
+
+	@URL("https://hiramatu-hifuka.com/onyak/sikime01.html")
+	private String url = null;
+
+	@Override
+	public Multimap<String, String> getObject() throws Exception {
+		//
+		final Iterable<Node> nodes = Util.toList(NodeUtil.nodeStream(testAndApply(Objects::nonNull,
+				testAndApply(StringUtils::isNotBlank, url, x -> new URI(x).toURL(), null), x -> Jsoup.parse(x, 0),
+				null)));
+		//
+		Multimap<String, String> multimap = null;
+		//
+		if (Util.iterator(nodes) != null) {
+			//
+			String[] ss = null;
+			//
+			Pattern pattern = null;
+			//
+			Matcher matcher = null;
+			//
+			for (final Node node : nodes) {
+				//
+				if (node == null || !(node instanceof TextNode)
+						|| (ss = StringUtils.split(Util.toString(node), "　　")) == null) {
+					//
+					continue;
+					//
+				} // if
+					//
+				for (final String s : ss) {
+					//
+					if (Util.matches(matcher = Util.matcher(
+							pattern = ObjectUtils.getIfNull(pattern, () -> Pattern.compile(
+									"^(\\p{InCJKUnifiedIdeographs}+)\\p{InHalfwidthAndFullwidthForms}(\\p{InHiragana}+)\\p{InHalfwidthAndFullwidthForms}$")),
+							StringUtils.trim(s))) && Util.groupCount(matcher) > 1) {
+						//
+						MultimapUtil.put(multimap = ObjectUtils.getIfNull(multimap, LinkedHashMultimap::create),
+								Util.group(matcher, 1), Util.group(matcher, 2));
+						//
+					} // if
+						//
+				} // for
+					//
+			} // for
+				//
+		} // if
+			//
+		return multimap;
+		//
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, @Nullable final FailableFunction<T, R, E> functionFalse)
+			throws E {
+		return Util.test(predicate, value) ? FailableFunctionUtil.apply(functionTrue, value)
+				: FailableFunctionUtil.apply(functionFalse, value);
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return Multimap.class;
+	}
+
+}
---
+++ src/test/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBeanTest.java
@@ -0,0 +1,139 @@
+package org.springframework.beans.factory;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+import org.apache.commons.lang3.function.FailableFunction;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.google.common.base.Predicates;
+import com.google.common.collect.Multimap;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+class OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBeanTest {
+
+	private static Method METHOD_TEST_AND_APPLY;
+
+	@BeforeAll
+	static void beforeClass() throws NoSuchMethodException {
+		//
+		(METHOD_TEST_AND_APPLY = OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBean.class
+				.getDeclaredMethod("testAndApply", Predicate.class, Object.class, FailableFunction.class,
+						FailableFunction.class))
+				.setAccessible(true);
+		//
+	}
+
+	private OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBean instance = null;
+
+	private boolean isSystemPropertiesContainsTestGetObject;
+
+	@BeforeEach
+	void beforeEach() {
+		//
+		instance = new OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBean();
+		//
+		isSystemPropertiesContainsTestGetObject = Util.containsKey(System.getProperties(),
+				"org.springframework.beans.factory.OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBeanTest.testGetObject");
+		//
+	}
+
+	@Test
+	void testGetObjecctType() {
+		//
+		Assertions.assertEquals(Multimap.class, FactoryBeanUtil.getObjectType(instance));
+		//
+	}
+
+	@Test
+	void testGetObject() throws Exception {
+		//
+		Assertions.assertNull(getObject(instance));
+		//
+		final Entry<Field, Object> entry = TestUtil.getFieldWithUrlAnnotationAndValue(
+				OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBean.class);
+		//
+		final Field url = Util.getKey(entry);
+		//
+		Narcissus.setObjectField(instance, url, "");
+		//
+		Assertions.assertNull(getObject(instance));
+		//
+		Narcissus.setObjectField(instance, url, " ");
+		//
+		Assertions.assertNull(getObject(instance));
+		//
+		if (isSystemPropertiesContainsTestGetObject) {
+			//
+			Narcissus.setObjectField(instance, url, Util.getValue(entry));
+			//
+			Assertions.assertDoesNotThrow(() -> getObject(instance));
+			//
+		} // if
+			//
+	}
+
+	private static <T> T getObject(final FactoryBean<T> instance) throws Exception {
+		return instance != null ? instance.getObject() : null;
+	}
+
+	@Test
+	void testNull() {
+		//
+		final Method[] ms = OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBean.class.getDeclaredMethods();
+		//
+		Method m = null;
+		//
+		for (int i = 0; ms != null && i < ms.length; i++) {
+			//
+			if ((m = ms[i]) == null || !Modifier.isStatic(m.getModifiers()) || m.isSynthetic()) {
+				//
+				continue;
+				//
+			} // if
+				//
+			Assertions.assertNull(
+					Narcissus.invokeStaticMethod(m, toArray(Collections.nCopies(m.getParameterCount(), null))),
+					Objects.toString(m));
+			//
+		} // for
+			//
+	}
+
+	private static Object[] toArray(final Collection<?> instance) {
+		return instance != null ? instance.toArray() : null;
+	}
+
+	@Test
+	void testTestAndApply() throws Throwable {
+		//
+		if (!isSystemPropertiesContainsTestGetObject) {
+			//
+			Assertions.assertNull(testAndApply(Predicates.alwaysTrue(), null, null, null));
+			//
+		} // if
+			//
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse)
+			throws Throwable {
+		try {
+			return (R) METHOD_TEST_AND_APPLY.invoke(null, predicate, value, functionTrue, functionFalse);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+}
