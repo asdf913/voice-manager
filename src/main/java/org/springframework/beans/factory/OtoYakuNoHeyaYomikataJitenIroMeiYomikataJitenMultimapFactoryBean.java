d49a39cd05691d2c429ea094cae06b3036468864
asdf913 <asdf913@gmail.com>
2024-08-31 19:53:08 +0900

    Apply rule "Boolean expressions should not be gratuitous"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBean.java
@@ -775,8 +775,8 @@ public class OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBean
 		//
 	}
 
-	private static <A, B, C> void testAndAccept(final TriPredicate<A, B, C> predicate, final A a, final B b, final C c,
-			final TriConsumer<A, B, C> consumer) {
+	private static <A, B, C> void testAndAccept(@Nullable final TriPredicate<A, B, C> predicate, final A a, final B b,
+			final C c, @Nullable final TriConsumer<A, B, C> consumer) {
 		if (predicate != null && predicate.test(a, b, c) && consumer != null) {
 			consumer.accept(a, b, c);
 		}
