cd7f4c753cd00b5b4349fe0ab4c6f1af8a0ef09e
asdf913 <asdf913@gmail.com>
2024-09-14 20:17:36 +0900

    Apply rule ""@NonNull" values should not be set to null"

--- src/main/java/org/springframework/beans/factory/Util.java
+++ src/main/java/org/springframework/beans/factory/Util.java
@@ -1299,6 +1299,7 @@ abstract class Util {
 		return test(predicate, value) ? apply(functionTrue, value) : apply(functionFalse, value);
 	}
 
+	@Nullable
 	private static <T, R> R apply(@Nullable final Function<T, R> instance, final T value) {
 		return instance != null ? instance.apply(value) : null;
 	}
