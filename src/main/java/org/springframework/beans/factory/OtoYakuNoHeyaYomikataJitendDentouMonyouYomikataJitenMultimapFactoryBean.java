5f20a858299ee2c83275eaed4abdf20e00dcff6b
asdf913 <asdf913@gmail.com>
2024-10-18 09:49:23 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
@@ -482,7 +482,7 @@ public class OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactory
 	}
 
 	private static Entry<Multimap<String, String>, IntCollection> toMultimapAndIntCollection4A1(
-			final PatternMap patternMap, final IntObjectPair<String> iop1, final IntObjectPair<String> iop2,
+			final PatternMap patternMap, @Nullable final IntObjectPair<String> iop1, final IntObjectPair<String> iop2,
 			final Quartet<String, String, String, String> quartet) {
 		//
 		IntCollection intCollection = null;
