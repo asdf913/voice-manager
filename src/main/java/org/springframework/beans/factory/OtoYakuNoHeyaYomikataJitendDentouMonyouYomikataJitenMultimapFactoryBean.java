64117f607d9b4133e617e4318ea663f235721bb1
asdf913 <asdf913@gmail.com>
2024-10-26 00:17:51 +0900

    Apply rule "Boolean expressions should not be gratuitous"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
@@ -3738,7 +3738,7 @@ public class OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactory
 
 	@Nullable
 	private static Entry<Multimap<String, String>, IntCollection> toMultimapAndIntCollection11A13(
-			final PatternMap patternMap, final IntObjectPair<String> iop, final int i, final String line,
+			final PatternMap patternMap, @Nullable final IntObjectPair<String> iop, final int i, final String line,
 			final Map<String, String> map) {
 		//
 		if (Boolean.logicalOr((iop != null && iop.keyInt() == i),
