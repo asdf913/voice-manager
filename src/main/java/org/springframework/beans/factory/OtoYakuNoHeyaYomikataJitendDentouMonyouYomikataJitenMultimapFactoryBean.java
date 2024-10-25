788b5057f91addb09c18e2a2535fe184f73660eb
asdf913 <asdf913@gmail.com>
2024-10-25 10:55:34 +0900

    Apply rule "Cognitive Complexity of methods should not be too high" Update "toMultimapAndIntCollection11A1(org.springframework.beans.factory.PatternMap,it.unimi.dsi.fastutil.ints.IntObjectPair,int,java.lang.String,java.utli.Map)" method to "toMultimapAndIntCollection11A2(org.springframework.beans.factory.PatternMap,it.unimi.dsi.fastutil.ints.IntObjectPair,int,java.lang.String,java.utli.Map)" methods in "org.springframework.beans.factory.OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean" class

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
@@ -2045,6 +2045,18 @@ public class OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactory
 							StringUtils.substring(g22, 0, indexOf + 1), StringUtils.substring(g21, 1, 2),
 							StringUtils.substring(g22, indexOf + 1)));
 			//
+		} else if (Util.matches(m2 = Util.matcher(PatternMap.getPattern(patternMap, String.format(
+				"^(%1$s%2$s)\\p{InHalfwidthAndFullwidthForms}(\\p{InHiragana}+)\\p{InHalfwidthAndFullwidthForms}$",
+				kFirst, kLast)), line)) && Util.groupCount(m2) > 1 && StringUtils.length(g21 = Util.group(m2, 1)) == 2
+				&& StringUtils.isNotBlank(csk = getCommonSuffix(g11, Util.group(m2, 1)))
+				&& StringUtils.isNotBlank(csv = getCommonSuffix(g12, g22 = Util.group(m2, 2)))) {
+			//
+			testAndAccept((a, b) -> !IntIterableUtil.containsInt(a, b), intCollection, i,
+					(a, b) -> IntCollectionUtil.addInt(intCollection, i));
+			//
+			MultimapUtil.putAll(multimap, ImmutableMultimap.of(StringUtils.substringBefore(g21, csk),
+					StringUtils.substringBefore(g22, csv), csk, csv));
+			//
 		} // if
 			//
 		return Pair.of(multimap, intCollection);
@@ -2239,17 +2251,6 @@ public class OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactory
 				//
 			} // if
 				//
-		} else if (Util.matches(m2 = Util.matcher(PatternMap.getPattern(patternMap, String.format(
-				"^(%1$s%2$s)\\p{InHalfwidthAndFullwidthForms}(\\p{InHiragana}+)\\p{InHalfwidthAndFullwidthForms}$",
-				kFirst, kLast)), line)) && Util.groupCount(m2) > 1 && StringUtils.length(g21 = Util.group(m2, 1)) == 2
-				&& StringUtils.isNotBlank(csk = getCommonSuffix(g11, Util.group(m2, 1)))
-				&& StringUtils.isNotBlank(csv = getCommonSuffix(g12, g22 = Util.group(m2, 2)))) {
-			//
-			IntCollectionUtil.addInt(intCollection, i);
-			//
-			MultimapUtil.putAll(multimap, ImmutableMultimap.of(StringUtils.substringBefore(g21, csk),
-					StringUtils.substringBefore(g22, csv), csk, csv));
-			//
 		} else if (Util.matches(m2 = Util.matcher(PatternMap.getPattern(patternMap, String.format(
 				"^(%1$s\\p{InCJKUnifiedIdeographs}%2$s)\\p{InHalfwidthAndFullwidthForms}(\\p{InHiragana}+)\\p{InHalfwidthAndFullwidthForms}[\\p{InHalfwidthAndFullwidthForms}\\p{InCJKUnifiedIdeographs}\\p{InKatakana}\\p{InHiragana}]+\\p{InBasicLatin}(\\p{InCJKUnifiedIdeographs}+)\\p{InHalfwidthAndFullwidthForms}(\\p{InHiragana}+)\\p{InHalfwidthAndFullwidthForms}$",
 				kFirst, kLast)), line)) && Util.groupCount(m2) > 3
