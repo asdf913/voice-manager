a7c9838ffc6aa0b8d21b8deeaae574c8b65c0d09
asdf913 <asdf913@gmail.com>
2024-10-25 10:10:36 +0900

    Apply rule "Cognitive Complexity of methods should not be too high"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
@@ -2034,9 +2034,8 @@ public class OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactory
 						StringUtils.substringBetween(g21, cpk, csk), StringUtils.substringBetween(g22, cpv, csv), csk,
 						csv, StringUtils.substringBetween(g11, cpk, csk), StringUtils.substringBetween(g12, cpv, csv)));
 				//
-			} else if (Boolean.logicalAnd(StringUtils.isNotBlank(cpk),
-					StringUtils.isNotBlank(csk = getCommonSuffix(g11, g21)))
-					&& StringUtils.isBlank(StringUtils.getCommonPrefix(g12, g22 = Util.group(m2, 2)))
+			} else if (Util.and(StringUtils.isNotBlank(cpk), StringUtils.isNotBlank(csk = getCommonSuffix(g11, g21)),
+					StringUtils.isBlank(StringUtils.getCommonPrefix(g12, g22 = Util.group(m2, 2))))
 					&& StringUtils.isNotBlank(csv = getCommonSuffix(g12, g22))) {
 				//
 				if (StringUtils.length(g21) == (countMatches = StringUtils.countMatches(g22, 'ん'))) {
