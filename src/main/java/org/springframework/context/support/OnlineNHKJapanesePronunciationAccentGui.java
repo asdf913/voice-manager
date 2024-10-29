9b2cef01fd287726facb35094480941b71e5dad3
asdf913 <asdf913@gmail.com>
2024-10-29 11:05:07 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/context/support/OnlineNHKJapanesePronunciationAccentGui.java
+++ src/main/java/org/springframework/context/support/OnlineNHKJapanesePronunciationAccentGui.java
@@ -45,7 +45,6 @@ import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.BiPredicate;
 import java.util.function.Consumer;
-import java.util.function.IntConsumer;
 import java.util.function.IntUnaryOperator;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
