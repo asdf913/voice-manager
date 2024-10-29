66cbff6d524a607d9b0542a11c60c747e3201379
asdf913 <asdf913@gmail.com>
2024-10-29 09:39:16 +0900

    Apply rule "Lambdas should be replaced with method references"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenSansuuSuugakuYougoYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenSansuuSuugakuYougoYomikataJitenMultimapFactoryBean.java
@@ -271,7 +271,7 @@ public class OtoYakuNoHeyaYomikataJitenSansuuSuugakuYougoYomikataJitenMultimapFa
 		Multimap<String, String> multimap = null;
 		//
 		final Matcher matcher = Util.matcher(Pattern.compile(String.format("((%1$s)+)（(\\p{InHiragana}+)）",
-				Util.collect(Stream.of("CJKUnifiedIdeographs", "Hiragana"), () -> new StringBuilder(), (a, b) -> {
+				Util.collect(Stream.of("CJKUnifiedIdeographs", "Hiragana"), StringBuilder::new, (a, b) -> {
 					//
 					if (StringUtils.isNotBlank(a)) {
 						//
