fb90fe94b7de8e1be1fd515622df1ffc73456b70
asdf913 <asdf913@gmail.com>
2024-11-01 10:01:39 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
@@ -5015,8 +5015,8 @@ public class OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactory
 				: FailableFunctionUtil.apply(functionFalse, value);
 	}
 
-	private static <T, U, R> R testAndApply(final BiPredicate<T, U> predicate, final T t, final U u,
-			final BiFunction<T, U, R> functionTrue, final BiFunction<T, U, R> functionFalse) {
+	private static <T, U, R> R testAndApply(final BiPredicate<T, U> predicate, final T t, @Nullable final U u,
+			final BiFunction<T, U, R> functionTrue, @Nullable final BiFunction<T, U, R> functionFalse) {
 		return Util.test(predicate, t, u) ? Util.apply(functionTrue, t, u) : Util.apply(functionFalse, t, u);
 	}
 
