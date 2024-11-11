f8416502d05f84da7788f1ba29c2b33d713af586
asdf913 <asdf913@gmail.com>
2024-11-12 00:42:37 +0900

    Apply rule "Cognitive Complexity of methods should not be too high"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
@@ -5585,18 +5585,17 @@ public class OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactory
 									StringUtils.substring(g12, ints[0] + 2, ints[0] + 4), StringUtils.substring(g11, 2),
 									StringUtils.substring(g12, ints[0] + 4))));
 			//
-			if (Boolean.logicalAnd(StringUtils.length(g11) == 4,
-					!StringUtils.equals(getCharacterName(g12, ints[0] + 1), HIRAGANA_LETTER_SMALL))) {
-				//
-				MultimapUtil.putAll(multimap,
-						ImmutableMultimap.of(StringUtils.substring(g11, 0, 3),
+			testAndRun(Boolean.logicalAnd(StringUtils.length(g11) == 4,
+					!StringUtils.equals(getCharacterName(g12, ints[0] + 1), HIRAGANA_LETTER_SMALL)), () -> {
+						//
+						MultimapUtil.putAll(multimap, ImmutableMultimap.of(StringUtils.substring(g11, 0, 3),
 								StringUtils.substring(g12, 0, StringUtils.length(g12) - 2),
 								StringUtils.substring(g11, 0, 1), StringUtils.substring(g12, 0, 1),
 								StringUtils.substring(g11, 1, 2), StringUtils.substring(g12, ints[0] - 1, ints[0] + 1),
 								StringUtils.substring(g11, 3), StringUtils.substring(g12, ints[0] + 3)));
-				//
-			} // if
-				//
+						//
+					});
+			//
 		} // if
 			//
 		return Pair.of(multimap, intCollection);
