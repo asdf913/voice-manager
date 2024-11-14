b3dc43a24068d0cbc1a9c340ae071223c17f3ea9
asdf913 <asdf913@gmail.com>
2024-11-14 12:26:50 +0900

    Update "org.springframework.beans.factory.OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.toMultimapAndIntCollection19(org.springframework.beans.factory.PatternMap,it.unimi.dsi.fastutil.ints.IntObjectPair,java.lang.Iterable)" method

--- src/test/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBeanTest.java
+++ src/test/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBeanTest.java
@@ -1168,6 +1168,15 @@ class OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBeanTes
 		Assertions.assertEquals("({竝文=[ならべもん], 文=[もん]},[0])",
 				Objects.toString(toMultimapAndIntCollection(patternMap, IntObjectPair.of(ZERO, "竝文（ならべもん）"), null)));
 		//
+		Assertions.assertEquals("({襷文=[たすきもん], 襷=[たすき], 文=[もん]},[0])",
+				Objects.toString(toMultimapAndIntCollection(patternMap, IntObjectPair.of(ZERO, "襷文（たすきもん）"), null)));
+		//
+		Assertions.assertEquals("({椿文=[つばきもん], 椿=[つばき], 文=[もん]},[0])",
+				Objects.toString(toMultimapAndIntCollection(patternMap, IntObjectPair.of(ZERO, "椿文（つばきもん）"), null)));
+		//
+		Assertions.assertEquals("({籬文=[まがきもん], 籬=[まがき], 文=[もん]},[0])",
+				Objects.toString(toMultimapAndIntCollection(patternMap, IntObjectPair.of(ZERO, "籬文（まがきもん）"), null)));
+		//
 	}
 
 	private static Entry<Multimap<String, String>, IntCollection> toMultimapAndIntCollection(
