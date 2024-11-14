b75794944df615e7f57060571640a8b9e04839c0
asdf913 <asdf913@gmail.com>
2024-11-15 00:14:32 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
@@ -6160,7 +6160,7 @@ public class OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactory
 		//
 	}
 
-	private static <T, U, V> void testAndAccept(final TriPredicate<T, U, V> instance, final T t, final U u,
+	private static <T, U, V> void testAndAccept(final TriPredicate<T, U, V> instance, @Nullable final T t, final U u,
 			@Nullable final V v, @Nullable final TriConsumer<T, U, V> consumer) {
 		if (TriPredicateUtil.test(instance, t, u, v) && consumer != null) {
 			consumer.accept(t, u, v);
