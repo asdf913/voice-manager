a38919592828d31817b0e87c9d15d9e103ca6325
asdf913 <asdf913@gmail.com>
2024-11-14 22:39:21 +0900

    Apply rule "Cognitive Complexity of methods should not be too high"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
@@ -5933,21 +5933,13 @@ public class OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactory
 					//
 					char c = charAt(g12, 2, space);
 					//
-					if (c == 'し') {
+					if (ArrayUtils.contains(new char[] { 'し', 'べ' }, c)) {
 						//
 						final Multimap<String, String> multimap = LinkedHashMultimap.create(ImmutableMultimap.of(g11,
 								g12, StringUtils.substring(g11, 0, 1), StringUtils.substring(g12, 0, length - 2),
 								StringUtils.substring(g11, 1), StringUtils.substring(g12, length - 2)));
 						//
 						testAndAccept(MultimapUtil::containsEntry, multimap, "隠", "かくし", MultimapUtil::remove);
-						//
-						return Pair.of(multimap, createIntCollection(iop));
-						//
-					} else if (c == 'べ') {
-						//
-						final Multimap<String, String> multimap = LinkedHashMultimap.create(ImmutableMultimap.of(g11,
-								g12, StringUtils.substring(g11, 0, 1), StringUtils.substring(g12, 0, length - 2),
-								StringUtils.substring(g11, 1), StringUtils.substring(g12, length - 2)));
 						//
 						testAndAccept(MultimapUtil::containsEntry, multimap, "竝", "ならべ", MultimapUtil::remove);
 						//
