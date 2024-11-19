0de206c7c82df7adf05d34978cfe2c8233980ead
asdf913 <asdf913@gmail.com>
2024-11-20 00:26:29 +0900

    Apply rule "Null pointers should not be dereferenced"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
@@ -7012,7 +7012,7 @@ public class OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactory
 			//
 			return Pair.of(ImmutableMultimap.of(g11, g12, StringUtils.substring(g11, 0, 1),
 					StringUtils.substring(g12, 0, get(ints, 0, 0) + 1), StringUtils.substring(g11, 1, 2),
-					StringUtils.substring(g12, ints[0] + 1, ints[1] + 1), StringUtils.substring(g11, 2),
+					StringUtils.substring(g12, get(ints, 0, 0) + 1, ints[1] + 1), StringUtils.substring(g11, 2),
 					StringUtils.substring(g12, ints[1] + 1)), IntList.create(index));
 			//
 		} else if (testAndApplyAsChar(x -> StringUtils.length(x) > 3, g12, space, x -> charAt(x, 3, space),
