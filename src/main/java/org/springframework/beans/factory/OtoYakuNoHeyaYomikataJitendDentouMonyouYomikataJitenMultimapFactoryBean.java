7484ef1f0b7c222418fc54da9b0eff24f522a6e5
asdf913 <asdf913@gmail.com>
2024-11-20 01:06:10 +0900

    Update "org.springframework.beans.factory.OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.toMultimapAndIntCollection26C(int,java.util.Map$Entry)" method

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactoryBean.java
@@ -7070,32 +7070,31 @@ public class OtoYakuNoHeyaYomikataJitendDentouMonyouYomikataJitenMultimapFactory
 							StringUtils.substring(g12, 0, 2), StringUtils.substring(g11, 1, 2),
 							StringUtils.substring(g12, 2, 4)), IntList.create(index));
 					//
-				} else {
-					//
-					final Multimap<String, String> multimap = LinkedHashMultimap.create(ImmutableMultimap.of(g11, g12,
-							StringUtils.substring(g11, 0, 1), StringUtils.substring(g12, 0, 2),
-							StringUtils.substring(g11, 1, 2), StringUtils.substring(g12, 2, 4),
-							StringUtils.substring(g11, 2), StringUtils.substring(g12, 4)));
-					//
-					Util.forEach(Arrays.asList(Triplet.with("菊", "ぎく", "きく")),
-							//
-							a -> testAndAccept(b -> MultimapUtil.containsEntry(multimap, IValue0Util.getValue0(b),
-									Util.getValue1(b)), a, b -> {
-										//
-										final String s1 = IValue0Util.getValue0(b);
-										//
-										MultimapUtil.remove(multimap, s1, Util.getValue1(b));
-										//
-										MultimapUtil.put(multimap, s1, Util.getValue2(b));
-										//
-									})
-					//
-					);
-					//
-					return Pair.of(multimap, IntList.create(index));
-					//
 				} // if
 					//
+				final Multimap<String, String> multimap = LinkedHashMultimap.create(ImmutableMultimap.of(g11, g12,
+						StringUtils.substring(g11, 0, 1), StringUtils.substring(g12, 0, 2),
+						StringUtils.substring(g11, 1, 2), StringUtils.substring(g12, 2, 4),
+						StringUtils.substring(g11, 2), StringUtils.substring(g12, 4)));
+				//
+				Util.forEach(Arrays.asList(Triplet.with("菊", "ぎく", "きく")),
+						//
+						a -> testAndAccept(
+								b -> MultimapUtil.containsEntry(multimap, IValue0Util.getValue0(b), Util.getValue1(b)),
+								a, b -> {
+									//
+									final String s1 = IValue0Util.getValue0(b);
+									//
+									MultimapUtil.remove(multimap, s1, Util.getValue1(b));
+									//
+									MultimapUtil.put(multimap, s1, Util.getValue2(b));
+									//
+								})
+				//
+				);
+				//
+				return Pair.of(multimap, IntList.create(index));
+				//
 			} // if
 				//
 		} // if
