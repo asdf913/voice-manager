c38f86c8daa7fc63f5606fdabc880544a825aa3d
asdf913 <asdf913@gmail.com>
2024-12-03 01:23:46 +0900

    Apply rule ""@NonNull" values should not be set to null"

--- src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBean.java
@@ -2890,6 +2890,7 @@ public class OtoYakuNoHeyaYomikataJitenIroMeiYomikataJitenMultimapFactoryBean
 		//
 	}
 
+	@Nullable
 	private static Entry<Multimap<String, String>, IntList> toMultimapAndIntList20(final PatternMap patternMap,
 			final List<String> list, final int i) {
 		//
