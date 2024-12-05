a88eb4451a4ce0a694836a6d9d3ba9278a007ca5
asdf913 <asdf913@gmail.com>
2024-12-05 10:59:55 +0900

    Apply rule "Conditionally executed code should be reachable"

--- src/main/java/org/springframework/context/support/Provider.java
+++ src/main/java/org/springframework/context/support/Provider.java
@@ -7,21 +7,21 @@ public interface Provider {
 	@Nullable
 	String getProviderName();
 
-	static String getProviderName(final Provider instance) {
+	static String getProviderName(@Nullable final Provider instance) {
 		return instance != null ? instance.getProviderName() : null;
 	}
 
 	@Nullable
 	String getProviderVersion();
 
-	static String getProviderVersion(final Provider instance) {
+	static String getProviderVersion(@Nullable final Provider instance) {
 		return instance != null ? instance.getProviderVersion() : null;
 	}
 
 	@Nullable
 	String getProviderPlatform();
 
-	static String getProviderPlatform(final Provider instance) {
+	static String getProviderPlatform(@Nullable final Provider instance) {
 		return instance != null ? instance.getProviderPlatform() : null;
 	}
 
