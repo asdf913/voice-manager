0a8eeb13545eb4399e49b16bb407cf49a7d574fb
asdf913 <asdf913@gmail.com>
2024-12-06 01:55:40 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/springframework/context/support/SpeechApi.java
+++ src/main/java/org/springframework/context/support/SpeechApi.java
@@ -28,7 +28,8 @@ public interface SpeechApi {
 	@Nullable
 	public String getVoiceAttribute(@Nullable final String voiceId, final String attribute);
 
-	static String getVoiceAttribute(@Nullable final SpeechApi instance, final String voiceId, final String attribute) {
+	static String getVoiceAttribute(@Nullable final SpeechApi instance, @Nullable final String voiceId,
+			final String attribute) {
 		return instance != null ? instance.getVoiceAttribute(voiceId, attribute) : null;
 	}
 
