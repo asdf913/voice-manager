32f3e0a153318cbeb99890ad840f2d40f2880a8c
asdf913 <asdf913@gmail.com>
2024-12-05 11:28:46 +0900

    Apply rule ""@NonNull" values should not be set to null"

--- src/main/java/org/springframework/context/support/Provider.java
+++ src/main/java/org/springframework/context/support/Provider.java
@@ -7,6 +7,7 @@ public interface Provider {
 	@Nullable
 	String getProviderName();
 
+	@Nullable
 	static String getProviderName(@Nullable final Provider instance) {
 		return instance != null ? instance.getProviderName() : null;
 	}
@@ -14,6 +15,7 @@ public interface Provider {
 	@Nullable
 	String getProviderVersion();
 
+	@Nullable
 	static String getProviderVersion(@Nullable final Provider instance) {
 		return instance != null ? instance.getProviderVersion() : null;
 	}
@@ -21,6 +23,7 @@ public interface Provider {
 	@Nullable
 	String getProviderPlatform();
 
+	@Nullable
 	static String getProviderPlatform(@Nullable final Provider instance) {
 		return instance != null ? instance.getProviderPlatform() : null;
 	}
