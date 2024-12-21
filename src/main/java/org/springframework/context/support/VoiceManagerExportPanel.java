6c5eea519b961dc5b48cbfd9a46ef23e75d4a88b
asdf913 <asdf913@gmail.com>
2024-12-21 12:14:50 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/springframework/context/support/VoiceManagerExportPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerExportPanel.java
@@ -772,7 +772,7 @@ public class VoiceManagerExportPanel extends JPanel
 		//
 	}
 
-	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, @Nullable final T value,
 			final FailableFunction<T, R, E> functionTrue, @Nullable final FailableFunction<T, R, E> functionFalse)
 			throws E {
 		return Util.test(predicate, value) ? FailableFunctionUtil.apply(functionTrue, value)
