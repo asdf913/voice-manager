f84c173ecfc3535e9349d9deb36f7196ba7e451e
asdf913 <asdf913@gmail.com>
2024-12-28 19:15:01 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/context/support/VoiceManagerExportPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerExportPanel.java
@@ -5,7 +5,6 @@ import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.ClipboardOwner;
-import java.awt.datatransfer.StringSelection;
 import java.awt.datatransfer.Transferable;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
