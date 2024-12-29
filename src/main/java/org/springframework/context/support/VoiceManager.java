557624d184f79b2352361f70aacce0cb77400e6d
asdf913 <asdf913@gmail.com>
2024-12-30 07:29:22 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/context/support/VoiceManager.java
+++ src/main/java/org/springframework/context/support/VoiceManager.java
@@ -222,7 +222,6 @@ import org.springframework.beans.factory.config.ConfigurableListableBeanFactoryU
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.EnvironmentAware;
-import org.springframework.core.AttributeAccessor;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.PropertyResolver;
 import org.springframework.core.env.PropertyResolverUtil;
