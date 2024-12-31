79309e764921f4d59aaea80bb7db46b8035a5f82
asdf913 <asdf913@gmail.com>
2025-01-01 08:38:13 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/test/java/org/springframework/context/support/VoiceManagerTest.java
+++ src/test/java/org/springframework/context/support/VoiceManagerTest.java
@@ -174,7 +174,6 @@ import org.w3c.dom.Node;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Predicates;
-import com.google.common.collect.Multimap;
 import com.google.common.reflect.Reflection;
 import com.healthmarketscience.jackcess.Database.FileFormat;
 
