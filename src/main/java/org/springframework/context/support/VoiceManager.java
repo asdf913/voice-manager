b2f6f002df1154447651e8056a38b0ff87a3cb0f
asdf913 <asdf913@gmail.com>
2025-01-01 08:02:58 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/context/support/VoiceManager.java
+++ src/main/java/org/springframework/context/support/VoiceManager.java
@@ -20,8 +20,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.ByteArrayInputStream;
