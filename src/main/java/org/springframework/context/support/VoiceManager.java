88a1fbfeda837d6939f598e1074da952ee3b7b58
asdf913 <asdf913@gmail.com>
2025-01-01 23:08:12 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/context/support/VoiceManager.java
+++ src/main/java/org/springframework/context/support/VoiceManager.java
@@ -97,7 +97,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
-import javax.swing.JSlider;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.MutableComboBoxModel;
