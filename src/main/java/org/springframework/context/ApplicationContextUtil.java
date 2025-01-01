52da7bbcb509aed7765f04ffb39b3cefa3e408d0
asdf913 <asdf913@gmail.com>
2025-01-02 01:37:20 +0900

    Apply rule ""@NonNull" values should not be set to null"

--- src/main/java/org/springframework/context/ApplicationContextUtil.java
+++ src/main/java/org/springframework/context/ApplicationContextUtil.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 
 public interface ApplicationContextUtil {
 
+	@Nullable
 	static AutowireCapableBeanFactory getAutowireCapableBeanFactory(@Nullable final ApplicationContext instance)
 			throws IllegalStateException {
 		return instance != null ? instance.getAutowireCapableBeanFactory() : null;
