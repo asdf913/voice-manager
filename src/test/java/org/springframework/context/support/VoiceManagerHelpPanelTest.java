940ecc51c1a963fcca446dd40b1fe5e01f64de5c
asdf913 <asdf913@gmail.com>
2025-01-02 17:06:58 +0900

    Add "org.springframework.context.support.VoiceManagerHelpPanel" class

---
+++ src/test/java/org/springframework/context/support/VoiceManagerHelpPanelTest.java
@@ -0,0 +1,118 @@
+package org.springframework.context.support;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Objects;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+class VoiceManagerHelpPanelTest {
+
+	@Test
+	void testNull() {
+		//
+		final Method[] ms = VoiceManagerHelpPanel.class.getDeclaredMethods();
+		//
+		Method m = null;
+		//
+		Class<?>[] parameterTypes = null;
+		//
+		Class<?> parameterType = null;
+		//
+		Collection<Object> collection = null;
+		//
+		Object[] os = null;
+		//
+		String name, toString;
+		//
+		Object invoke = null;
+		//
+		VoiceManagerHelpPanel instance = null;
+		//
+		for (int i = 0; ms != null && i < ms.length; i++) {
+			//
+			if ((m = ms[i]) == null || m.isSynthetic()) {
+				//
+				continue;
+				//
+			} // if
+				//
+			if ((collection = ObjectUtils.getIfNull(collection, ArrayList::new)) != null) {
+				//
+				collection.clear();
+				//
+			} // if
+				//
+			parameterTypes = m.getParameterTypes();
+			//
+			for (int j = 0; parameterTypes != null && j < parameterTypes.length; j++) {
+				//
+				if (Objects.equals(parameterType = parameterTypes[j], Integer.TYPE)) {
+					//
+					Util.add(collection, Integer.valueOf(0));
+					//
+				} else if (Objects.equals(parameterType, Boolean.TYPE)) {
+					//
+					Util.add(collection, Boolean.FALSE);
+					//
+				} else {
+					//
+					Util.add(collection, null);
+					//
+				} // if
+					//
+			} // for
+				//
+			name = Util.getName(m);
+			//
+			os = toArray(collection);
+			//
+			toString = Objects.toString(m);
+			//
+			if (Modifier.isStatic(m.getModifiers())) {
+				//
+				invoke = Narcissus.invokeStaticMethod(m, os);
+				//
+				if (Util.contains(Arrays.asList(Integer.TYPE, Boolean.TYPE), m.getReturnType())) {
+					//
+					Assertions.assertNotNull(invoke, toString);
+					//
+				} else {
+					//
+					Assertions.assertNull(invoke, toString);
+					//
+				} // if
+					//
+			} else {
+				//
+				invoke = Narcissus.invokeMethod(instance = ObjectUtils.getIfNull(instance, VoiceManagerHelpPanel::new),
+						m, os);
+				//
+				if (Boolean.logicalAnd(Util.contains(Arrays.asList("getTitle"), name), m.getParameterCount() == 0)) {
+					//
+					Assertions.assertNotNull(invoke, toString);
+					//
+				} else {
+					//
+					Assertions.assertNull(invoke, toString);
+					//
+				} // if
+					//
+			} // if
+				//
+		} // for
+			//
+	}
+
+	private static Object[] toArray(final Collection<?> instance) {
+		return instance != null ? instance.toArray() : null;
+	}
+
+}
