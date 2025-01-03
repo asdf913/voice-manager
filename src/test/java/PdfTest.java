bbb4cb513397764296b3b5aff4ea066c5e00f67c
asdf913 <asdf913@gmail.com>
2025-01-03 13:22:48 +0900

    Update "PdfTest" testing program

--- src/test/java/PdfTest.java
+++ src/test/java/PdfTest.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.fontbox.ttf.OTFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
@@ -10,19 +11,25 @@ import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
 public class PdfTest {
 
-	public static void main(final String[] args) throws IOException {
+	public static void main(final String[] args) throws Exception {
 		//
 		try (final PDDocument document = new PDDocument()) {
 			//
-			final PDPage page = new PDPage();
+			final PDPage pd = new PDPage();
 			//
-			document.addPage(page);
+			document.addPage(pd);
 			//
 			final File file = Path.of("test.pdf").toFile();
 			//
-			final PDPageContentStream cs = new PDPageContentStream(document, page);
+			final PDPageContentStream cs = new PDPageContentStream(document, pd);
 			//
 			cs.beginText();
 			//
@@ -40,8 +47,8 @@ public class PdfTest {
 			//
 			final String text = "席をお譲りください";
 			//
-			cs.newLineAtOffset((page.getMediaBox().getWidth() - getTextWidth(text, font, fontSize)) / 2,
-					page.getMediaBox().getHeight() - (font.getFontDescriptor().getAscent() / 1000 * fontSize)
+			cs.newLineAtOffset((pd.getMediaBox().getWidth() - getTextWidth(text, font, fontSize)) / 2,
+					pd.getMediaBox().getHeight() - (font.getFontDescriptor().getAscent() / 1000 * fontSize)
 							+ (font.getFontDescriptor().getDescent() / 1000 * fontSize));
 			//
 			cs.showText(text);
@@ -54,6 +61,38 @@ public class PdfTest {
 			//
 			document.save(file);
 			//
+		} // try
+			//
+		try (final Playwright playwright = Playwright.create()) {
+			//
+			final BrowserType browserType = playwright != null ? playwright.chromium() : null;
+			//
+			final Browser browser = browserType != null ? browserType.launch() : null;
+			//
+			final BrowserContext browserContext = browser != null ? browser.newContext() : null;
+			//
+			final Page page = browserContext != null ? browserContext.newPage() : null;
+			//
+			File f = Path.of("test.html").toFile();
+			//
+			System.out.println(f.getAbsolutePath());
+			//
+			FileUtils.writeStringToFile(f,
+					"<span style=\"font-size:64px\"><ruby>席<rt>せき</rt></ruby>をお<ruby>譲<rt>ゆず</rt></ruby>りください。</span>",
+					"utf-8", false);
+			//
+			if (page != null) {
+				//
+				page.navigate(f.toURI().toURL().toString());
+				//
+				final Path path = Path.of("test.png");
+				//
+				System.out.println(path.toFile().getAbsolutePath());
+				//
+				page.screenshot(new Page.ScreenshotOptions().setPath(path));
+				//
+			} // if
+				//
 		} // try
 			//
 	}
