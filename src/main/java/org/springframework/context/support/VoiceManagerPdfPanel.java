521741608bec194747a054a4108dac37dcb66392
asdf913 <asdf913@gmail.com>
2025-01-10 08:04:12 +0900

    Apply rule "Boolean expressions should not be gratuitous"

--- src/main/java/org/springframework/context/support/VoiceManagerPdfPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerPdfPanel.java
@@ -433,7 +433,7 @@ public class VoiceManagerPdfPanel extends JPanel implements Titled, Initializing
 		} // if
 	}
 
-	private static <T, U> void accept(final BiConsumer<T, U> instance, final T t, final U u) {
+	private static <T, U> void accept(@Nullable final BiConsumer<T, U> instance, final T t, final U u) {
 		if (instance != null) {
 			instance.accept(t, u);
 		}
