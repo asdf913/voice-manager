96acbaac084de1c73c71bb6e1156459cc88127ef
asdf913 <asdf913@gmail.com>
2025-01-11 22:03:09 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/context/support/VoiceManagerPdfPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerPdfPanel.java
@@ -2,7 +2,6 @@ package org.springframework.context.support;
 
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
