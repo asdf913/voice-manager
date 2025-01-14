fdb35e264e6e277ad4b601684f374070e4cdf50c
asdf913 <asdf913@gmail.com>
2025-01-14 22:10:51 +0900

    Update "setFontSizeAndUnit(int)" method in "org.springframework.context.support.VoiceManagerPdfPanel" class

--- src/main/java/org/springframework/context/support/VoiceManagerPdfPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerPdfPanel.java
@@ -823,12 +823,13 @@ public class VoiceManagerPdfPanel extends JPanel implements Titled, Initializing
 			//
 			// TODO
 			//
-			putAll(fontSizeAndUnitMap = new LinkedHashMap<>(Map.of(Integer.valueOf(6), "133px", Integer.valueOf(7),
-					"114px", Integer.valueOf(8), "100px", Integer.valueOf(9), "88px", Integer.valueOf(10), "80px",
-					Integer.valueOf(11), "72px", Integer.valueOf(12), "66px", Integer.valueOf(13), "61px",
-					Integer.valueOf(14), "56px", Integer.valueOf(15), "53px")),
-					Map.of(Integer.valueOf(16), "50px", Integer.valueOf(17), "45px", Integer.valueOf(18), "43px",
-							Integer.valueOf(19), "42px", Integer.valueOf(30), "26px", Integer.valueOf(36), "22px"));
+			putAll(fontSizeAndUnitMap = new LinkedHashMap<>(Map.of(Integer.valueOf(5), "160px", Integer.valueOf(6),
+					"133px", Integer.valueOf(7), "114px", Integer.valueOf(8), "100px", Integer.valueOf(9), "88px",
+					Integer.valueOf(10), "80px", Integer.valueOf(11), "72px", Integer.valueOf(12), "66px",
+					Integer.valueOf(13), "61px", Integer.valueOf(14), "56px")),
+					Map.of(Integer.valueOf(15), "53px", Integer.valueOf(16), "50px", Integer.valueOf(17), "45px",
+							Integer.valueOf(18), "43px", Integer.valueOf(19), "42px", Integer.valueOf(30), "26px",
+							Integer.valueOf(36), "22px"));
 			//
 		} // if
 			//
