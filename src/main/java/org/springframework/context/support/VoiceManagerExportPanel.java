95c40e107c15d2f3afffca918b31f77ad181abfc
asdf913 <asdf913@gmail.com>
2025-01-17 07:40:41 +0900

    Update "encrypt(java.io.File,org.apache.poi.poifs.crypt.EncryptionMode,java.lang.String)" method in "org.springframework.context.support.VoiceManagerExportPanel" class

--- src/main/java/org/springframework/context/support/VoiceManagerExportPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerExportPanel.java
@@ -2565,7 +2565,7 @@ public class VoiceManagerExportPanel extends JPanel implements Titled, Initializ
 				//
 				try (final POIFSFileSystem fs = new POIFSFileSystem(file, true);
 						final Workbook wb2 = new HSSFWorkbook(fs.getRoot(), true);
-						final OutputStream os = Files.newOutputStream(file.toPath(file))) {
+						final OutputStream os = Files.newOutputStream(Util.toPath(file))) {
 					//
 					WorkbookUtil.write(wb2, os);
 					//
