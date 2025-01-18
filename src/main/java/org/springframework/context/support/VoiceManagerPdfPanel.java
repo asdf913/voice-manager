f8a2c15c8db35dc77cae8ab997f92d1400870f79
asdf913 <asdf913@gmail.com>
2025-01-18 10:12:50 +0900

    Apply rule "Mergeable "if" statements should be combine"

--- src/main/java/org/springframework/context/support/VoiceManagerPdfPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerPdfPanel.java
@@ -932,20 +932,16 @@ public class VoiceManagerPdfPanel extends JPanel implements Titled, Initializing
 								//
 							} // if
 								//
-						} else if (i > 0 && ins[i - 1] instanceof GETSTATIC getstatic) {
+						} else if (i > 0 && ins[i - 1] instanceof GETSTATIC getstatic && Boolean.logicalAnd(
+								Boolean.logicalOr(in instanceof IF_ACMPNE, in instanceof IF_ACMPEQ),
+								!Util.contains(allowedFileType = ObjectUtils.getIfNull(allowedFileType, ArrayList::new),
+										object = Narcissus.getStaticField(
+												(clz = Util.forName(getstatic.getFieldType(cpg).getClassName())) != null
+														? clz.getDeclaredField(getstatic.getFieldName(cpg))
+														: null)))) {
 							//
-							if (Boolean.logicalAnd(Boolean.logicalOr(in instanceof IF_ACMPNE, in instanceof IF_ACMPEQ),
-									!Util.contains(
-											allowedFileType = ObjectUtils.getIfNull(allowedFileType, ArrayList::new),
-											object = Narcissus.getStaticField((clz = Util
-													.forName(getstatic.getFieldType(cpg).getClassName())) != null
-															? clz.getDeclaredField(getstatic.getFieldName(cpg))
-															: null)))) {
-								//
-								Util.add(allowedFileType, object);
-								//
-							} // if
-								//
+							Util.add(allowedFileType, object);
+							//
 						} // if
 							//
 					} // for
