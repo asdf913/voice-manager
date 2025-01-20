7b7a6a1b8423a9982fc39e57295da2761348b995
asdf913 <asdf913@gmail.com>
2025-01-21 07:48:30 +0900

    Add "org.springframework.beans.factory.ImageWriterSpiFormatIterableFactoryBean" class

---
+++ src/main/java/org/springframework/beans/factory/ImageWriterSpiFormatIterableFactoryBean.java
@@ -0,0 +1,83 @@
+package org.springframework.beans.factory;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.Predicate;
+
+import javax.imageio.spi.IIORegistry;
+import javax.imageio.spi.ImageWriterSpi;
+import javax.imageio.spi.ServiceRegistry;
+
+import org.apache.commons.collections4.IterableUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.function.FailableFunctionUtil;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+public class ImageWriterSpiFormatIterableFactoryBean implements FactoryBean<Iterable<String>> {
+
+	@Override
+	public Iterable<String> getObject() throws Exception {
+		//
+		final Map<?, ?> imageWriterSpis = Util.cast(Map.class,
+				testAndApply(Objects::nonNull,
+						Util.get(
+								Util.cast(Map.class,
+										Narcissus.getObjectField(IIORegistry.getDefaultInstance(),
+												getDeclaredField(ServiceRegistry.class, "categoryMap"))),
+								ImageWriterSpi.class),
+						x -> Narcissus.getField(x, getDeclaredField(Util.getClass(x), "map")), null));
+		//
+		final List<String> classNames = testAndApply(Objects::nonNull,
+				Util.toList(
+						Util.map(Util.stream(keySet(imageWriterSpis)), x -> Util.getName(Util.cast(Class.class, x)))),
+				ArrayList::new, null);
+		//
+		final String commonPrefix = StringUtils.getCommonPrefix(toArray(classNames, new String[] {}));
+		//
+		for (int i = 0; classNames != null && i < classNames.size(); i++) {
+			//
+			classNames.set(i, StringUtils
+					.substringBefore(StringUtils.replace(IterableUtils.get(classNames, i), commonPrefix, ""), '.'));
+			//
+		} // if
+			//
+		return classNames;
+		//
+	}
+
+	private static <T> T[] toArray(final Collection<T> instance, final T[] array) {
+		//
+		return instance != null && (array != null || Proxy.isProxyClass(Util.getClass(instance)))
+				? instance.toArray(array)
+				: null;
+		//
+	}
+
+	private static <K> Set<K> keySet(final Map<K, ?> instance) {
+		return instance != null ? instance.keySet() : null;
+	}
+
+	private static Field getDeclaredField(final Class<?> instance, final String name) throws NoSuchFieldException {
+		return instance != null ? instance.getDeclaredField(name) : null;
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse) throws E {
+		return Util.test(predicate, value) ? FailableFunctionUtil.apply(functionTrue, value)
+				: FailableFunctionUtil.apply(functionFalse, value);
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return Iterable.class;
+	}
+
+}
---
+++ src/test/java/org/springframework/beans/factory/ImageWriterSpiFormatIterableFactoryBeanTest.java
@@ -0,0 +1,80 @@
+package org.springframework.beans.factory;
+
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+class ImageWriterSpiFormatIterableFactoryBeanTest {
+
+	@Test
+	void testNull() {
+		//
+		final Method[] ms = ImageWriterSpiFormatIterableFactoryBean.class.getDeclaredMethods();
+		//
+		Method m = null;
+		//
+		ImageWriterSpiFormatIterableFactoryBean instance = null;
+		//
+		Object invoke = null;
+		//
+		String toString, name = null;
+		//
+		for (int i = 0; ms != null && i < ms.length; i++) {
+			//
+			if ((m = ms[i]) == null || m.isSynthetic()) {
+				//
+				continue;
+				//
+			} // if
+				//
+			toString = Util.toString(m);
+			//
+			if (Util.isStatic(m)) {
+				//
+				invoke = Narcissus.invokeStaticMethod(m, toArray(Collections.nCopies(m.getParameterCount(), null)));
+				//
+				if (Objects.equals(Boolean.TYPE, m.getReturnType())) {
+					//
+					Assertions.assertNotNull(invoke, toString);
+					//
+				} else {
+					//
+					Assertions.assertNull(invoke, toString);
+					//
+				} // if
+					//
+			} else {
+				//
+				if (Boolean.logicalOr(
+						Boolean.logicalAnd(Objects.equals(name = Util.getName(m), "getObject"),
+								m.getParameterCount() == 0),
+						Boolean.logicalAnd(Objects.equals(name, "getObjectType"), m.getParameterCount() == 0))) {
+					//
+					System.out.println(Narcissus.invokeMethod(
+							instance = ObjectUtils.getIfNull(instance, ImageWriterSpiFormatIterableFactoryBean::new), m,
+							toArray(Collections.nCopies(m.getParameterCount(), null))));
+					//
+					Assertions.assertNotNull(Narcissus.invokeMethod(
+							instance = ObjectUtils.getIfNull(instance, ImageWriterSpiFormatIterableFactoryBean::new), m,
+							toArray(Collections.nCopies(m.getParameterCount(), null))), toString);
+					//
+				} // if
+					//
+			} // if
+				//
+		} // for
+			//
+	}
+
+	private static Object[] toArray(final Collection<?> instance) {
+		return instance != null ? instance.toArray() : null;
+	}
+
+}
