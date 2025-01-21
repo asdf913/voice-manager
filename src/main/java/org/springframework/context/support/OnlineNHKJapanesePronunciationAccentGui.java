c3ddcee01e4ba897eb4bdbf9c7ea5e850cec9268
asdf913 <asdf913@gmail.com>
2025-01-22 07:22:35 +0900

    Apply rule "Boolean expressions should not be gratuitous"

--- src/main/java/org/springframework/context/support/OnlineNHKJapanesePronunciationAccentGui.java
+++ src/main/java/org/springframework/context/support/OnlineNHKJapanesePronunciationAccentGui.java
@@ -251,7 +251,7 @@ public class OnlineNHKJapanesePronunciationAccentGui extends JFrame
 		return instance != null ? instance.length : 0;
 	}
 
-	private static boolean hasNext(final Iterator<?> instance) {
+	private static boolean hasNext(@Nullable final Iterator<?> instance) {
 		return instance != null && instance.hasNext();
 	}
 
