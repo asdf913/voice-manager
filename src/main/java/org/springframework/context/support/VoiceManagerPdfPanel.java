2a88e53e04d1315b371340318c4b4d7d40131a6d
asdf913 <asdf913@gmail.com>
2025-02-11 23:34:30 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/context/support/VoiceManagerPdfPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerPdfPanel.java
@@ -105,7 +105,6 @@ import org.apache.bcel.classfile.ClassParser;
 import org.apache.bcel.classfile.ClassParserUtil;
 import org.apache.bcel.classfile.ConstantPool;
 import org.apache.bcel.classfile.FieldOrMethodUtil;
-import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.classfile.JavaClassUtil;
 import org.apache.bcel.generic.ConstantPoolGen;
 import org.apache.bcel.generic.FieldInstructionUtil;
