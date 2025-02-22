2da9e3d4f0cd17bb665de17c039dce6441632187
asdf913 <asdf913@gmail.com>
2025-02-23 02:56:49 +0900

    Apply rule "Cognitive Complexity of methods should not be too high" Update "actionPerformed(java.awt.event.ActionEvent)" and "actionPerformed2(java.lang.Object)" methods in "org.springframework.context.support.VoiceManagerPdfPanel" class

--- src/main/java/org/springframework/context/support/VoiceManagerPdfPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerPdfPanel.java
@@ -1629,27 +1629,6 @@ public class VoiceManagerPdfPanel extends JPanel implements Titled, Initializing
 				//
 			} // try
 				//
-		} else if (Objects.equals(source, btnImageClear)) {
-			//
-			setEnabled((renderedImage = null) != null, btnImageClear, btnImageView);
-			//
-		} else if (Objects.equals(source, btnImageView)) {
-			//
-			testAndRun(!isTestMode(),
-					() -> JOptionPane.showMessageDialog(null, testAndApply(Objects::nonNull,
-							Util.cast(BufferedImage.class, renderedImage), ImageIcon::new, null), "Image",
-							JOptionPane.PLAIN_MESSAGE, null),
-					null);
-			//
-		} else if (Objects.equals(source, btnCopyOutputFilePath)) {
-			//
-			setContents(testAndApply(x -> !GraphicsEnvironment.isHeadless(), Toolkit.getDefaultToolkit(),
-					x -> getSystemClipboard(x), null), new StringSelection(Util.getText(tfOutputFile)), null);
-			//
-		} else if (Objects.equals(source, btnCopyTextToHtml)) {
-			//
-			Util.setText(taHtml, Util.getText(tfText));
-			//
 		} // if
 			//
 		final Iterable<Predicate<Object>> predicates = Arrays.asList(this::actionPerformed2, this::actionPerformed3,
@@ -1697,7 +1676,28 @@ public class VoiceManagerPdfPanel extends JPanel implements Titled, Initializing
 
 	private boolean actionPerformed2(final Object source) {
 		//
-		if (Objects.equals(source, btnImageFile)) {
+		if (Objects.equals(source, btnImageClear)) {
+			//
+			setEnabled((renderedImage = null) != null, btnImageClear, btnImageView);
+			//
+		} else if (Objects.equals(source, btnImageView)) {
+			//
+			testAndRun(!isTestMode(),
+					() -> JOptionPane.showMessageDialog(null, testAndApply(Objects::nonNull,
+							Util.cast(BufferedImage.class, renderedImage), ImageIcon::new, null), "Image",
+							JOptionPane.PLAIN_MESSAGE, null),
+					null);
+			//
+		} else if (Objects.equals(source, btnCopyOutputFilePath)) {
+			//
+			setContents(testAndApply(x -> !GraphicsEnvironment.isHeadless(), Toolkit.getDefaultToolkit(),
+					x -> getSystemClipboard(x), null), new StringSelection(Util.getText(tfOutputFile)), null);
+			//
+		} else if (Objects.equals(source, btnCopyTextToHtml)) {
+			//
+			Util.setText(taHtml, Util.getText(tfText));
+			//
+		} else if (Objects.equals(source, btnImageFile)) {
 			//
 			final JFileChooser jfc = testAndGet(Boolean.logicalAnd(!GraphicsEnvironment.isHeadless(), !isTestMode()),
 					() -> new JFileChooser("."));
