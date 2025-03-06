b57ae4e4d8cf659f667d3dff2100226f26ff8367
asdf913 <asdf913@gmail.com>
2025-03-07 00:28:04 +0900

    Update "addTextAndVoice(org.springframework.context.support.VoiceManagerPdfPanel$ObjectMap,java.util.Map,int,boolean)" method in "org.springframework.context.support.VoiceManagerPdfPanel" class

--- src/main/java/org/springframework/context/support/VoiceManagerPdfPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerPdfPanel.java
@@ -3267,12 +3267,15 @@ public class VoiceManagerPdfPanel extends JPanel implements Titled, Initializing
 						//
 						cs.setFont(font, fontSize);
 						//
-						cs.newLineAtOffset((index - 1) * size + (size - getTextWidth(value, font, fontSize)) / 2,
+						cs.newLineAtOffset(
+								(index - 1) * size + (size - getTextWidth(
+										value = String.format("%1$s s", duration.toMillis() / 1000d), font, fontSize))
+										/ 2,
 								lastHeight = lastHeight - (font.getFontDescriptor().getAscent() / 1000 * fontSize)
 										+ (font.getFontDescriptor().getDescent() / 1000 * fontSize)
 						//
 						);
-						cs.showText(String.format("%1$s s", duration.toMillis() / 1000d));
+						cs.showText(value);
 						//
 						cs.endText();
 						//
