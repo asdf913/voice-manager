9c129d1f05aaf522248e8b2aeac60e6c2d8ebe1d
asdf913 <asdf913@gmail.com>
2025-03-20 10:10:17 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/jsoup/nodes/NodeUtil.java
+++ src/main/java/org/jsoup/nodes/NodeUtil.java
@@ -88,7 +88,7 @@ public class NodeUtil {
 		return test(predicate, value) ? apply(functionTrue, value) : apply(functionFalse, value);
 	}
 
-	private static final <T> boolean test(@Nullable final Predicate<T> instance, final T value) {
+	private static final <T> boolean test(@Nullable final Predicate<T> instance, @Nullable final T value) {
 		return instance != null && instance.test(value);
 	}
 
