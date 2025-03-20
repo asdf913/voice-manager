2ac84bf73bd98f99458e42cc77f86025be36986f
asdf913 <asdf913@gmail.com>
2025-03-20 10:01:58 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/jsoup/helper/ProtocolUtil.java
+++ src/main/java/org/jsoup/helper/ProtocolUtil.java
@@ -92,8 +92,8 @@ public interface ProtocolUtil {
 		return clz != null ? clz.getDeclaredMethod(name, parameterTypes) : null;
 	}
 
-	private static <T, R> R testAndApply(final Predicate<T> predicate, final T value, final Function<T, R> functionTrue,
-			@Nullable final Function<T, R> functionFalse) {
+	private static <T, R> R testAndApply(final Predicate<T> predicate, @Nullable final T value,
+			final Function<T, R> functionTrue, @Nullable final Function<T, R> functionFalse) {
 		return test(predicate, value) ? apply(functionTrue, value) : apply(functionFalse, value);
 	}
 
