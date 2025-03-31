4084909fc3745d7d20fe46a54b59ae7d37df3139
asdf913 <asdf913@gmail.com>
2025-03-31 11:16:51 +0900

    Apply rule "Conditionally executed code should be reachable"

--- src/main/java/org/springframework/context/ConfigurableApplicationContextUtil.java
+++ src/main/java/org/springframework/context/ConfigurableApplicationContextUtil.java
@@ -1,10 +1,12 @@
 package org.springframework.context;
 
+import javax.annotation.Nullable;
+
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 
 public interface ConfigurableApplicationContextUtil {
 
-	static ConfigurableListableBeanFactory getBeanFactory(final ConfigurableApplicationContext instance) {
+	static ConfigurableListableBeanFactory getBeanFactory(@Nullable final ConfigurableApplicationContext instance) {
 		return instance != null ? instance.getBeanFactory() : null;
 	}
 
