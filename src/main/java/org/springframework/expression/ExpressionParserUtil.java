91c82f9d2cc557c56decd5bac05309aa16f568e2
asdf913 <asdf913@gmail.com>
2025-04-06 01:56:59 +0900

    Apply rule ""@NonNull" values should not be set to null"

--- src/main/java/org/springframework/expression/ExpressionParserUtil.java
+++ src/main/java/org/springframework/expression/ExpressionParserUtil.java
@@ -4,6 +4,7 @@ import javax.annotation.Nullable;
 
 public interface ExpressionParserUtil {
 
+	@Nullable
 	static Expression parseExpression(@Nullable final ExpressionParser instance, final String expressionString) {
 		return instance != null ? instance.parseExpression(expressionString) : null;
 	}
