dffe1ac3d141f3e8f204bcc002cf43088a70ab6d
asdf913 <asdf913@gmail.com>
2025-04-09 10:59:59 +0900

    Apply rule "Two branches in a conditional structure should not have exactly the same implementation"

--- src/main/java/org/springframework/beans/factory/Util.java
+++ src/main/java/org/springframework/beans/factory/Util.java
@@ -825,18 +825,13 @@ abstract class Util {
 						&& ArrayUtils.get(ins, 1) instanceof GETFIELD gf
 						&& ArrayUtils.get(ins, 2) instanceof INVOKEINTERFACE) {
 					//
-					if (length(ins) == 4 && ArrayUtils.get(ins, 3) instanceof ARETURN
-							&& FieldUtils.readDeclaredField(instance, gf.getFieldName(cpg), true) == null) {
-						//
-						// com.helger.commons.log.InMemoryLogger
-						//
-						return;
-						//
-					} else if (length(ins) == 5 && ArrayUtils.get(ins, 3) instanceof INVOKEINTERFACE
-							&& ArrayUtils.get(ins, 4) instanceof ARETURN
-							&& FieldUtils.readDeclaredField(instance, gf.getFieldName(cpg), true) == null) {
-						//
-						// com.healthmarketscience.jackcess.impl.PropertyMapImpl
+					if (Boolean.logicalOr(length(ins) == 4 && ArrayUtils.get(ins, 3) instanceof ARETURN
+							&& FieldUtils.readDeclaredField(instance, gf.getFieldName(cpg), true) == null// com.helger.commons.log.InMemoryLogger
+							,
+							length(ins) == 5 && ArrayUtils.get(ins, 3) instanceof INVOKEINTERFACE
+									&& ArrayUtils.get(ins, 4) instanceof ARETURN
+									&& FieldUtils.readDeclaredField(instance, gf.getFieldName(cpg), true) == null// com.healthmarketscience.jackcess.impl.PropertyMapImpl
+					)) {
 						//
 						return;
 						//
