2dd94897081c753f670baf4ac2faa226513aec8d
asdf913 <asdf913@gmail.com>
2025-04-17 23:22:08 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/jsoup/nodes/ElementUtil.java
+++ src/main/java/org/jsoup/nodes/ElementUtil.java
@@ -235,7 +235,8 @@ public final class ElementUtil {
 	}
 
 	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
-			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse) throws E {
+			final FailableFunction<T, R, E> functionTrue, @Nullable final FailableFunction<T, R, E> functionFalse)
+			throws E {
 		return test(predicate, value) ? FailableFunctionUtil.apply(functionTrue, value)
 				: FailableFunctionUtil.apply(functionFalse, value);
 	}
