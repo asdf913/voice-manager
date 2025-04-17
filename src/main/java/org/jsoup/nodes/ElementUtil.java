0beaabe8e2635652eced4f59cbe849192dcd293e
asdf913 <asdf913@gmail.com>
2025-04-17 23:48:53 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/jsoup/nodes/ElementUtil.java
+++ src/main/java/org/jsoup/nodes/ElementUtil.java
@@ -246,7 +246,7 @@ public final class ElementUtil {
 				: FailableFunctionUtil.apply(functionFalse, value);
 	}
 
-	private static <T> boolean test(@Nullable final Predicate<T> instance, final T value) {
+	private static <T> boolean test(@Nullable final Predicate<T> instance, @Nullable final T value) {
 		return instance != null && instance.test(value);
 	}
 
