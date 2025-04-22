c7990c69bf99f970447da1c1f849c670da99a66d
asdf913 <asdf913@gmail.com>
2025-04-22 17:44:21 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/context/support/VoiceManagerOnlineTtsPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerOnlineTtsPanel.java
@@ -20,7 +20,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 import java.util.regex.Matcher;
