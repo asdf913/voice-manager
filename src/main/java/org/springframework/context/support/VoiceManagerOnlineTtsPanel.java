c2f55c3c6b6a5b123e07b6dacded347ad889637c
asdf913 <asdf913@gmail.com>
2025-04-24 16:02:23 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/springframework/context/support/VoiceManagerOnlineTtsPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerOnlineTtsPanel.java
@@ -461,7 +461,7 @@ public class VoiceManagerOnlineTtsPanel extends JPanel
 	}
 
 	private static <T, U, R> R testAndApply(final BiPredicate<T, U> predicate, final T t, final U u,
-			final BiFunction<T, U, R> functionTrue, final BiFunction<T, U, R> functionFalse) {
+			final BiFunction<T, U, R> functionTrue, @Nullable final BiFunction<T, U, R> functionFalse) {
 		return test(predicate, t, u) ? apply(functionTrue, t, u) : apply(functionFalse, t, u);
 	}
 
