b5a1c967740a70a4c4ba9cd6dc23a771f6d7ca62
asdf913 <asdf913@gmail.com>
2025-04-24 16:34:03 +0900

    Apply rule ""@NonNull" values should not be set to null"

--- src/main/java/org/springframework/context/support/VoiceManagerOnlineTtsPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerOnlineTtsPanel.java
@@ -459,6 +459,7 @@ public class VoiceManagerOnlineTtsPanel extends JPanel
 				: FailableFunctionUtil.apply(functionFalse, value);
 	}
 
+	@Nullable
 	private static <T, U, R> R testAndApply(final BiPredicate<T, U> predicate, @Nullable final T t, final U u,
 			final BiFunction<T, U, R> functionTrue, @Nullable final BiFunction<T, U, R> functionFalse) {
 		return Util.test(predicate, t, u) ? apply(functionTrue, t, u) : apply(functionFalse, t, u);
