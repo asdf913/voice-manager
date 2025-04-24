58e15e983cae6db71c4718f9aff4e74b9e2b34c3
asdf913 <asdf913@gmail.com>
2025-04-24 18:16:06 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/springframework/context/support/VoiceManagerOnlineTtsPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerOnlineTtsPanel.java
@@ -514,7 +514,7 @@ public class VoiceManagerOnlineTtsPanel extends JPanel
 	}
 
 	@Nullable
-	private static <T, U, R> R apply(@Nullable final BiFunction<T, U, R> instance, final T t, final U u) {
+	private static <T, U, R> R apply(@Nullable final BiFunction<T, U, R> instance, @Nullable final T t, final U u) {
 		return instance != null ? instance.apply(t, u) : null;
 	}
 
