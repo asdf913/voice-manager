3395820c501e226504ff585ffceb737d12ff1de0
asdf913 <asdf913@gmail.com>
2025-05-04 00:33:57 +0900

    Use "java.nio.file.Paths.get(java.lang.String,java.lang.String...)" method to replace usage of "java.io.File.File(java.lang.String)" constructor

--- src/test/java/org/springframework/context/support/VoiceManagerOnlineTtsPanelTest.java
+++ src/test/java/org/springframework/context/support/VoiceManagerOnlineTtsPanelTest.java
@@ -15,6 +15,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -1226,9 +1227,9 @@ class VoiceManagerOnlineTtsPanelTest {
 	@Test
 	void testCreateInputStreamSource() throws Throwable {
 		//
-		Assertions.assertNull(createInputStreamSource(new File(".")));
+		Assertions.assertNull(createInputStreamSource(Util.toFile(Paths.get("."))));
 		//
-		final File file = new File("pom.xml");
+		final File file = Util.toFile(Paths.get("pom.xml"));
 		//
 		if (Util.exists(file)) {
 			//
