1b12109e91ede3070dd522bd3193295cb0673ff6
asdf913 <asdf913@gmail.com>
2025-05-05 20:46:45 +0900

    Apply rule "Lambdas containing only one statement should not nest this statement in a block"

--- src/main/java/org/springframework/context/support/VoiceManagerOnlineTtsPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerOnlineTtsPanel.java
@@ -982,9 +982,8 @@ public class VoiceManagerOnlineTtsPanel extends JPanel
 									return null;
 									//
 								}), Objects::nonNull)
-						.collect(LinkedHashMap::new, (k, v) -> {
-							Util.put(k, Util.getKey(v), Util.getValue(v));
-						}, Util::putAll);
+						.collect(LinkedHashMap::new, (k, v) -> Util.put(k, Util.getKey(v), Util.getValue(v)),
+								Util::putAll);
 				//
 				speak(speechApi, Util.getText(taText),
 						testAndApply(x -> IterableUtils.size(x) == 1, keys, x -> IterableUtils.get(x, 0), null),
