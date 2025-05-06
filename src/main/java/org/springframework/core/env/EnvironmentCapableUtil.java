8faaa18086d251377ceb18c269ccd0a3b8f56495
asdf913 <asdf913@gmail.com>
2025-05-06 17:38:33 +0900

    Apply rule "Conditionally executed code should be reachable"

--- src/main/java/org/springframework/core/env/EnvironmentCapableUtil.java
+++ src/main/java/org/springframework/core/env/EnvironmentCapableUtil.java
@@ -1,8 +1,10 @@
 package org.springframework.core.env;
 
+import javax.annotation.Nullable;
+
 public interface EnvironmentCapableUtil {
 
-	static Environment getEnvironment(final EnvironmentCapable instance) {
+	static Environment getEnvironment(@Nullable final EnvironmentCapable instance) {
 		return instance != null ? instance.getEnvironment() : null;
 	}
 
