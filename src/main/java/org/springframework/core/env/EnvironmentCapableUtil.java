b8eb79e541650cdbab5dba3f2c12be8326492467
asdf913 <asdf913@gmail.com>
2025-05-06 17:50:06 +0900

    Apply rule ""@NonNull" values should not be set to null"

--- src/main/java/org/springframework/core/env/EnvironmentCapableUtil.java
+++ src/main/java/org/springframework/core/env/EnvironmentCapableUtil.java
@@ -4,6 +4,7 @@ import javax.annotation.Nullable;
 
 public interface EnvironmentCapableUtil {
 
+	@Nullable
 	static Environment getEnvironment(@Nullable final EnvironmentCapable instance) {
 		return instance != null ? instance.getEnvironment() : null;
 	}
