66c143aa9078e55c5552469af08c09df88d18f00
asdf913 <asdf913@gmail.com>
2025-05-13 23:30:50 +0900

    Apply rule "Boolean expressions should not be gratuitous"

--- src/main/java/org/springframework/context/support/VoiceManagerImageToPdfPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerImageToPdfPanel.java
@@ -316,7 +316,7 @@ public class VoiceManagerImageToPdfPanel extends JPanel implements InitializingB
 			//
 	}
 
-	private static void writeVoiceToFile(final SpeechApi instance, final String text, final String voiceId,
+	private static void writeVoiceToFile(@Nullable final SpeechApi instance, final String text, final String voiceId,
 			final int rate, final int volume, @Nullable final Map<String, Object> map, final File file) {
 		if (instance != null) {
 			instance.writeVoiceToFile(text, voiceId, rate, volume, map, file);
