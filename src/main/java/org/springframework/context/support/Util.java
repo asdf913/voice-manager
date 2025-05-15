c68baa6fa64927470506d931869c2208b54cd998
asdf913 <asdf913@gmail.com>
2025-05-15 20:54:59 +0900

    Apply rule "Boolean expressions should not be gratuitous"

--- src/main/java/org/springframework/context/support/Util.java
+++ src/main/java/org/springframework/context/support/Util.java
@@ -1869,7 +1869,7 @@ public abstract class Util {
 		}
 	}
 
-	static <T, U> void accept(final BiConsumer<T, U> instance, final T t, final U u) {
+	static <T, U> void accept(@Nullable final BiConsumer<T, U> instance, final T t, final U u) {
 		if (instance != null) {
 			instance.accept(t, u);
 		}
