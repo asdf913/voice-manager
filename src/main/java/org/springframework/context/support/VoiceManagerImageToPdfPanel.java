2ae10c78f94ef2bb37b64d4c3ddda7e500a986a4
asdf913 <asdf913@gmail.com>
2025-05-19 21:37:56 +0900

    Apply rule ""@NonNull" values should not be set to null"

--- src/main/java/org/springframework/context/support/VoiceManagerImageToPdfPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerImageToPdfPanel.java
@@ -908,6 +908,7 @@ public class VoiceManagerImageToPdfPanel extends JPanel
 
 		<T> void setObject(final Class<T> key, final T value);
 
+		@Nullable
 		static <T> T getObject(@Nullable final ObjectMap instance, final Class<?> key) {
 			return instance != null ? instance.getObject(key) : null;
 		}
