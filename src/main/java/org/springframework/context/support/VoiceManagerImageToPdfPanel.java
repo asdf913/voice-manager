7271f0833c88b876dcd3954a8a73e7627715d86a
asdf913 <asdf913@gmail.com>
2025-05-24 08:49:57 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/context/support/VoiceManagerImageToPdfPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerImageToPdfPanel.java
@@ -44,7 +44,6 @@ import java.util.Objects;
 import java.util.function.BiConsumer;
 import java.util.function.BiPredicate;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
