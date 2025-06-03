fa2baa595e7d923d52f4de4acbc15f897bd3369c
asdf913 <asdf913@gmail.com>
2025-06-03 19:22:35 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/context/support/VoiceManagerSpreadsheetToPdfPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerSpreadsheetToPdfPanel.java
@@ -53,7 +53,6 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
