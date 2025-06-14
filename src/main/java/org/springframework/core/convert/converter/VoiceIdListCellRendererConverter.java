41ed148916f6f19522649c5dfc5092d20e48ea5c
asdf913 <asdf913@gmail.com>
2025-06-14 19:23:10 +0900

    Add "org.springframework.core.convert.converter.VoiceIdListCellRendererConverter" class

---
+++ src/main/java/org/springframework/core/convert/converter/VoiceIdListCellRendererConverter.java
@@ -0,0 +1,70 @@
+package org.springframework.core.convert.converter;
+
+import java.awt.Component;
+
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+import org.apache.commons.lang3.StringUtils;
+import org.oxbow.swingbits.dialog.task.TaskDialogsUtil;
+import org.springframework.context.support.SpeechApi;
+
+public class VoiceIdListCellRendererConverter implements Converter<ListCellRenderer<Object>, ListCellRenderer<Object>> {
+
+	private SpeechApi speechApi = null;
+
+	@Override
+	public ListCellRenderer<Object> convert(final ListCellRenderer<Object> lcr) {
+		//
+		return new ListCellRenderer<>() {
+
+			@Override
+			public Component getListCellRendererComponent(final JList<? extends Object> list, final Object value,
+					final int index, final boolean isSelected, final boolean cellHasFocus) {
+				//
+				final String s = toString(value);
+				//
+				try {
+					//
+					final String name = SpeechApi.getVoiceAttribute(speechApi, s, "Name");
+					//
+					if (StringUtils.isNotBlank(name)) {
+						//
+						return getListCellRendererComponent(lcr, list, name, index, isSelected, cellHasFocus);
+						//
+					} // if
+						//
+				} catch (final Error e) {
+					//
+					TaskDialogsUtil.errorOrPrintStackTraceOrAssertOrShowException(e);
+					//
+				} // try
+					//
+				final String commonPrefix = String.join("", StringUtils.substringBeforeLast(
+						StringUtils.getCommonPrefix(SpeechApi.getVoiceIds(speechApi)), "\\"), "\\");
+				//
+				return getListCellRendererComponent(lcr, list,
+						StringUtils.startsWith(s, commonPrefix) ? StringUtils.substringAfter(s, commonPrefix) : value,
+						index, isSelected, cellHasFocus);
+				//
+			}
+
+			private static String toString(final Object instance) {
+				return instance != null ? instance.toString() : null;
+			}
+
+			private static <E> Component getListCellRendererComponent(final ListCellRenderer<E> instance,
+					final JList<? extends E> list, final E value, final int index, final boolean isSelected,
+					final boolean cellHasFocus) {
+				//
+				return instance != null
+						? instance.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus)
+						: null;
+				//
+			}
+
+		};
+
+	}
+
+}
---
+++ src/test/java/org/springframework/core/convert/converter/VoiceIdListCellRendererConverterTest.java
@@ -0,0 +1,105 @@
+package org.springframework.core.convert.converter;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Objects;
+
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.SpeechApi;
+
+import com.google.common.reflect.Reflection;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+class VoiceIdListCellRendererConverterTest {
+
+	private static class IH implements InvocationHandler {
+
+		private String voiceAttribute = null;
+
+		private String[] voiceIds = null;
+
+		@Override
+		public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+			//
+			final String name = method != null ? method.getName() : null;
+			//
+			if (proxy instanceof SpeechApi) {
+				//
+				if (Objects.equals(name, "getVoiceAttribute")) {
+					//
+					return voiceAttribute;
+					//
+				} else if (Objects.equals(name, "getVoiceIds")) {
+					//
+					return voiceIds;
+					//
+				} // if
+					//
+			} else if (proxy instanceof ListCellRenderer) {
+				//
+				if (Objects.equals(name, "getListCellRendererComponent")) {
+					//
+					return null;
+					//
+				} // if
+					//
+			} // if
+				//
+			throw new Throwable(name);
+			//
+		}
+
+	}
+
+	@Test
+	void testConvert() throws ReflectiveOperationException {
+		//
+		final VoiceIdListCellRendererConverter instance = new VoiceIdListCellRendererConverter();
+		//
+		ListCellRenderer<?> lcr = instance.convert(null);
+		//
+		Assertions.assertNotNull(lcr);
+		//
+		final IH ih = new IH();
+		//
+		if (lcr != null) {
+			//
+			Assertions.assertNull(lcr.getListCellRendererComponent(null, null, 0, false, false));
+			//
+			final Class<?> clz = lcr.getClass();
+			//
+			final Method method = clz != null ? clz.getDeclaredMethod("getListCellRendererComponent", JList.class,
+					Object.class, Integer.TYPE, Boolean.TYPE, Boolean.TYPE) : null;
+			//
+			Assertions.assertNull(Narcissus.invokeMethod(lcr, method, null, "", 0, false, false));
+			//
+			ih.voiceIds = new String[] { ih.voiceAttribute = "voiceAttribute" };
+			//
+			FieldUtils.writeDeclaredField(instance, "speechApi", Reflection.newProxy(SpeechApi.class, ih), true);
+			//
+			Assertions.assertNull(Narcissus.invokeMethod(lcr, method, null, "", 0, false, false));
+			//
+		} // if
+			//
+		if ((lcr = cast(ListCellRenderer.class,
+				Narcissus.invokeMethod(instance,
+						VoiceIdListCellRendererConverter.class.getDeclaredMethod("convert", ListCellRenderer.class),
+						Reflection.newProxy(ListCellRenderer.class, ih)))) != null) {
+			//
+			Assertions.assertNull(lcr.getListCellRendererComponent(null, null, 0, false, false));
+			//
+		} // if
+			//
+	}
+
+	private static <T> T cast(final Class<T> clz, final Object instance) {
+		return clz != null && clz.isInstance(instance) ? clz.cast(instance) : null;
+	}
+
+}
