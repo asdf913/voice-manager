b1c43e2f5816fad9edf4e846ba39429c980e7ef9
asdf913 <asdf913@gmail.com>
2025-06-15 10:02:00 +0900

    Apply rule ""@NonNull" values should not be set to null"

--- src/main/java/org/springframework/core/convert/converter/ConverterUtil.java
+++ src/main/java/org/springframework/core/convert/converter/ConverterUtil.java
@@ -4,6 +4,7 @@ import javax.annotation.Nullable;
 
 public interface ConverterUtil {
 
+	@Nullable
 	static <S, T> T convert(@Nullable final Converter<S, T> instance, final S source) {
 		return instance != null ? instance.convert(source) : null;
 	}
