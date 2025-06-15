0d414cc529e9f5648109a797f5b4f9c7ce58dda7
asdf913 <asdf913@gmail.com>
2025-06-15 10:01:36 +0900

    Apply rule "Conditionally executed code should be reachable"

--- src/main/java/org/springframework/core/convert/converter/ConverterUtil.java
+++ src/main/java/org/springframework/core/convert/converter/ConverterUtil.java
@@ -1,8 +1,10 @@
 package org.springframework.core.convert.converter;
 
+import javax.annotation.Nullable;
+
 public interface ConverterUtil {
 
-	static <S, T> T convert(final Converter<S, T> instance, final S source) {
+	static <S, T> T convert(@Nullable final Converter<S, T> instance, final S source) {
 		return instance != null ? instance.convert(source) : null;
 	}
 
