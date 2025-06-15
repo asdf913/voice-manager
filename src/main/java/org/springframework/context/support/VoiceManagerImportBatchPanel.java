62225002bb5d315ccaeb14da3c8462fbe2b2de68
asdf913 <asdf913@gmail.com>
2025-06-16 07:21:43 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/context/support/VoiceManagerImportBatchPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerImportBatchPanel.java
@@ -79,7 +79,6 @@ import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
