5544e24ccf73ffc7548239aa1ce2a64b28f7a101
asdf913 <asdf913@gmail.com>
2025-06-26 23:38:32 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/context/support/VoiceManagerOjadAccentPanel.java
+++ src/main/java/org/springframework/context/support/VoiceManagerOjadAccentPanel.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.net.URLEncoder;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Objects;
