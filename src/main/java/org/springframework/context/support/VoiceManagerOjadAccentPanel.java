41155e4e64501a24ad264adc396838d0138fa41f
asdf913 <asdf913@gmail.com>
2025-06-26 20:05:59 +0900

    Add "org.springframework.context.support.VoiceManagerOjadAccentPanel" class

---
+++ src/main/java/org/springframework/context/support/VoiceManagerOjadAccentPanel.java
@@ -0,0 +1,81 @@
+package org.springframework.context.support;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.collections4.IterableUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import com.j256.simplemagic.ContentInfo;
+import com.j256.simplemagic.ContentInfoUtil;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.BrowserTypeUtil;
+import com.microsoft.playwright.ElementHandle;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.PageUtil;
+import com.microsoft.playwright.Playwright;
+
+public class VoiceManagerOjadAccentPanel {
+
+	public static void main(final String[] args) throws IOException {
+		//
+		final Page page = newPage(BrowserTypeUtil.launch(chromium(Playwright.create())));
+		//
+		PageUtil.navigate(page, "https://www.gavo.t.u-tokyo.ac.jp/ojad/search/index/word:%E6%97%A5%E6%9C%AC");
+		//
+		final List<ElementHandle> ehs = querySelectorAll(page, ".katsuyo_accent");
+		//
+		byte[] bs = null;
+		//
+		File file = null;
+		//
+		ContentInfoUtil cic = null;
+		//
+		String[] fileExtensions = null;
+		//
+		for (int i = 0; i < IterableUtils.size(ehs); i++) {
+			//
+			FileUtils
+					.writeByteArrayToFile(
+							file = new File(StringUtils.joinWith(".", Integer.toString(i),
+									StringUtils.defaultIfBlank((fileExtensions = getFileExtensions(
+											findMatch(cic = ObjectUtils.getIfNull(cic, ContentInfoUtil::new),
+													bs = screenshot(IterableUtils.get(ehs, i))))) != null
+											&& fileExtensions.length == 1 ? fileExtensions[0] : null, "png"))),
+							bs);
+			//
+			System.out.println(file.getAbsolutePath());
+			//
+		} // for
+			//
+	}
+
+	private static String[] getFileExtensions(final ContentInfo instance) {
+		return instance != null ? instance.getFileExtensions() : null;
+	}
+
+	private static ContentInfo findMatch(final ContentInfoUtil instance, final byte[] bytes) {
+		return instance != null && bytes != null ? instance.findMatch(bytes) : null;
+	}
+
+	private static BrowserType chromium(final Playwright instance) {
+		return instance != null ? instance.chromium() : null;
+	}
+
+	private static Page newPage(final Browser instance) {
+		return instance != null ? instance.newPage() : null;
+	}
+
+	private static List<ElementHandle> querySelectorAll(final Page instance, final String selector) {
+		return instance != null ? instance.querySelectorAll(selector) : null;
+	}
+
+	private static byte[] screenshot(final ElementHandle instance) {
+		return instance != null ? instance.screenshot() : null;
+	}
+
+}
---
+++ src/test/java/org/springframework/context/support/VoiceManagerOjadAccentPanelTest.java
@@ -0,0 +1,225 @@
+package org.springframework.context.support;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.google.common.reflect.Reflection;
+import com.j256.simplemagic.ContentInfo;
+import com.j256.simplemagic.ContentInfoUtil;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.ElementHandle;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+class VoiceManagerOjadAccentPanelTest {
+
+	private static Method METHOD_GET_FILE_EXTENSIONS, METHOD_FIND_MATCH, METHOD_QUERY_SELECTOR_ALL = null;
+
+	@BeforeAll
+	static void beforeAll() throws NoSuchMethodException {
+		//
+		final Class<?> clz = VoiceManagerOjadAccentPanel.class;
+		//
+		(METHOD_GET_FILE_EXTENSIONS = clz.getDeclaredMethod("getFileExtensions", ContentInfo.class))
+				.setAccessible(true);
+		//
+		(METHOD_FIND_MATCH = clz.getDeclaredMethod("findMatch", ContentInfoUtil.class, byte[].class))
+				.setAccessible(true);
+		//
+		(METHOD_QUERY_SELECTOR_ALL = clz.getDeclaredMethod("querySelectorAll", Page.class, String.class))
+				.setAccessible(true);
+		//
+	}
+
+	private static class IH implements InvocationHandler {
+
+		@Override
+		public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+			//
+			final String methodName = Util.getName(method);
+			//
+			if (proxy instanceof ElementHandle) {
+				//
+				if (Objects.equals(methodName, "screenshot")) {
+					//
+					return null;
+					//
+				} // if
+					//
+			} else if (proxy instanceof Playwright) {
+				//
+				if (Objects.equals(methodName, "chromium")) {
+					//
+					return null;
+					//
+				} // if
+					//
+			} else if (proxy instanceof Browser) {
+				//
+				if (Objects.equals(methodName, "newPage")) {
+					//
+					return null;
+					//
+				} // if
+					//
+			} else if (proxy instanceof Page) {
+				//
+				if (Objects.equals(methodName, "querySelectorAll")) {
+					//
+					return null;
+					//
+				} // if
+					//
+			} // if
+				//
+			throw new Throwable(methodName);
+			//
+		}
+
+	}
+
+	private IH ih = null;
+
+	@BeforeEach
+	void beforeEach() {
+		//
+		ih = new IH();
+		//
+	}
+
+	@Test
+	void testNull() {
+		//
+		final Method[] ms = VoiceManagerOjadAccentPanel.class.getDeclaredMethods();
+		//
+		Method m = null;
+		//
+		for (int i = 0; ms != null && i < ms.length; i++) {
+			//
+			if ((m = ms[i]) == null || m.isSynthetic() || Objects.equals(Util.getName(m), "main")
+					&& Arrays.equals(Util.getParameterTypes(m), new Class<?>[] { String[].class })) {
+				//
+				continue;
+				//
+			} // if
+				//
+			Assertions.assertNull(
+					Narcissus.invokeStaticMethod(m, Util.toArray(Collections.nCopies(m.getParameterCount(), null))),
+					Objects.toString(m));
+			//
+		} // for
+			//
+	}
+
+	@Test
+	void testMethodWithOneInterfaceParameter() {
+		//
+		final Method[] ms = VoiceManagerOjadAccentPanel.class.getDeclaredMethods();
+		//
+		Method m = null;
+		//
+		Class<?>[] parameterTypes = null;
+		//
+		Class<?> parameterType = null;
+		//
+		for (int i = 0; ms != null && i < ms.length; i++) {
+			//
+			if ((m = ms[i]) == null || (parameterTypes = Util.getParameterTypes(m)) == null
+					|| parameterTypes.length != 1 || (parameterType = ArrayUtils.get(parameterTypes, 0)) == null
+					|| !Modifier.isInterface(parameterType.getModifiers())) {
+				//
+				continue;
+				//
+			} // if
+				//
+			Assertions.assertNull(
+					Narcissus.invokeStaticMethod(m, new Object[] { Reflection.newProxy(parameterType, ih) }),
+					Objects.toString(m));
+			//
+		} // for
+			//
+	}
+
+	@Test
+	void testGetFileExtensions() throws Throwable {
+		//
+		Assertions.assertNull(
+				getFileExtensions(Util.cast(ContentInfo.class, Narcissus.allocateInstance(ContentInfo.class))));
+		//
+	}
+
+	private static String[] getFileExtensions(final ContentInfo instance) throws Throwable {
+		try {
+			final Object obj = METHOD_GET_FILE_EXTENSIONS.invoke(null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof String[]) {
+				return (String[]) obj;
+			}
+			throw new Throwable(Util.toString(Util.getClass(obj)));
+		} catch (InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testFindMatch() throws Throwable {
+		//
+		final ContentInfoUtil cic = new ContentInfoUtil();
+		//
+		Assertions.assertNull(findMatch(cic, null));
+		//
+		Assertions.assertSame(ContentInfo.EMPTY_INFO, findMatch(cic, new byte[] {}));
+		//
+	}
+
+	private static ContentInfo findMatch(final ContentInfoUtil instance, final byte[] bytes) throws Throwable {
+		try {
+			final Object obj = METHOD_FIND_MATCH.invoke(null, instance, bytes);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof ContentInfo) {
+				return (ContentInfo) obj;
+			}
+			throw new Throwable(Util.toString(Util.getClass(obj)));
+		} catch (InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+	void testQuerySelectorAll() throws Throwable {
+		//
+		Assertions.assertNull(querySelectorAll(Reflection.newProxy(Page.class, ih), null));
+		//
+	}
+
+	private static List<ElementHandle> querySelectorAll(final Page instance, final String selector) throws Throwable {
+		try {
+			final Object obj = METHOD_QUERY_SELECTOR_ALL.invoke(null, instance, selector);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof List) {
+				return (List) obj;
+			}
+			throw new Throwable(Util.toString(Util.getClass(obj)));
+		} catch (InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+}
