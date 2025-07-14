23f35a68ee59bdb4ec3fffffcb145e1714ca9e09
lau-ka-ho <lau-ka-ho@lau-ka-ho-HP-EliteBook-830-G7-Notebook-PC>
2025-07-14 19:44:00 +0900

    Apply rule "Mergeable "if" statements should be combined"

--- src/test/java/org/springframework/context/support/VoiceManagerOjadAccentPanelTest.java
+++ src/test/java/org/springframework/context/support/VoiceManagerOjadAccentPanelTest.java
@@ -374,14 +374,10 @@ class VoiceManagerOjadAccentPanelTest {
 					//
 				} // if
 					//
-			} else if (proxy instanceof Stream) {
+			} else if (proxy instanceof Stream && Util.contains(Arrays.asList("mapToDouble", "mapToInt"), methodName)) {
 				//
-				if (Util.contains(Arrays.asList("mapToDouble", "mapToInt"), methodName)) {
-					//
-					return null;
-					//
-				} // if
-					//
+				return null;
+				//
 			} // if
 				//
 			throw new Throwable(methodName);
