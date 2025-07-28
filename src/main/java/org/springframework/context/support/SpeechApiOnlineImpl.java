548f8cd992d899eb14650d11087e8dececeef8b9
lau-ka-ho <asdf913@gmail.com>
2025-07-28 17:26:18 +0900

    Apply rule ""@NonNull" values should not be set to null"

--- src/main/java/org/springframework/context/support/SpeechApiOnlineImpl.java
+++ src/main/java/org/springframework/context/support/SpeechApiOnlineImpl.java
@@ -398,6 +398,7 @@ public class SpeechApiOnlineImpl implements SpeechApi {
 		//
 	}
 
+	@Nullable
 	private static String getAttribute(@Nullable final Element instance, final String name) {
 		return instance != null ? instance.getAttribute(name) : null;
 	}
