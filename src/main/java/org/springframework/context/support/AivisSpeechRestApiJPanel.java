9a25e6ff4c9c154ed1577a7daafc631e95e5e312
lau-ka-ho <asdf913@gmail.com>
2025-10-23 16:08:16 +0900

    Apply rule "Nullness of parameters should be guaranteed"

--- src/main/java/org/springframework/context/support/AivisSpeechRestApiJPanel.java
+++ src/main/java/org/springframework/context/support/AivisSpeechRestApiJPanel.java
@@ -651,7 +651,7 @@ public class AivisSpeechRestApiJPanel extends JPanel implements InitializingBean
 				: FailableFunctionUtil.apply(functionFalse, value);
 	}
 
-	private static <T> boolean test(@Nullable final Predicate<T> instance, final T value) {
+	private static <T> boolean test(@Nullable final Predicate<T> instance, @Nullable final T value) {
 		return instance != null && instance.test(value);
 	}
 
