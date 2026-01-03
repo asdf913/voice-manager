c69127efbfea056159847e49b5a687fd917dbd76
lau-ka-ho <asdf913@gmail.com>
2026-01-03 09:48:46 +0900

    Add "apply(java.util.function.Function,java.lang.Object)","filter(java.util.stream.Stream,java.util.function.Predicate)","getClass(java.lang.Object)","getType(java.lang.reflect.Field)","stream(java.util.Collection)","test(java.util.function.Predicate,java.lang.Object)","testAndApply(java.util.function.Predicate,java.lang.Object,java.util.function.Function,java.util.function.Function)" and "toList(java.util.stream.Stream)" methods in "org.apache.commons.validator.routines.UrlValidatorUtil" interface Update "isValid(org.apache.commons.validator.routines.UrlValidator,java.lang.String)" method in "org.apache.commons.validator.routines.UrlValidatorUtil" class

--- src/main/java/org/apache/commons/validator/routines/UrlValidatorUtil.java
+++ src/main/java/org/apache/commons/validator/routines/UrlValidatorUtil.java
@@ -1,9 +1,80 @@
 package org.apache.commons.validator.routines;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Proxy;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import org.apache.commons.collections4.IterableUtils;
+import org.apache.commons.lang3.reflect.FieldUtils;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
 public interface UrlValidatorUtil {
 
 	static boolean isValid(final UrlValidator instance, final String value) {
-		return instance != null && instance.isValid(value);
+		//
+		if (instance == null) {
+			//
+			return false;
+			//
+		} // if
+			//
+		final Iterable<Field> fs = toList(
+				filter(stream(testAndApply(Objects::nonNull, getClass(value), FieldUtils::getAllFieldsList, null)),
+						x -> Objects.equals(getType(x), byte[].class)));
+		//
+		if (IterableUtils.size(fs) > 1) {
+			//
+			throw new IllegalStateException();
+			//
+		} // if
+			//
+		return (value == null || (Narcissus.getField(value,
+				testAndApply(x -> IterableUtils.size(x) == 1, fs, x -> IterableUtils.get(x, 0), null)) != null))
+				&& instance.isValid(value);
+		//
+	}
+
+	private static <T> List<T> toList(final Stream<T> instance) {
+		return instance != null ? instance.toList() : null;
+	}
+
+	private static Class<?> getType(final Field instance) {
+		return instance != null ? instance.getType() : null;
+	}
+
+	private static <T> Stream<T> filter(final Stream<T> instance, final Predicate<? super T> predicate) {
+		//
+		return instance != null && (predicate != null || Proxy.isProxyClass(getClass(instance)))
+				? instance.filter(predicate)
+				: instance;
+		//
+	}
+
+	private static <E> Stream<E> stream(final Collection<E> instance) {
+		return instance != null ? instance.stream() : null;
+	}
+
+	private static <T, R> R testAndApply(final Predicate<T> predicate, final T value, final Function<T, R> functionTrue,
+			final Function<T, R> functionFalse) {
+		return test(predicate, value) ? apply(functionTrue, value) : apply(functionFalse, value);
+	}
+
+	private static <T> boolean test(final Predicate<T> instance, final T value) {
+		return instance != null && instance.test(value);
+	}
+
+	private static <T, R> R apply(final Function<T, R> instance, final T value) {
+		return instance != null ? instance.apply(value) : null;
+	}
+
+	private static Class<?> getClass(final Object instance) {
+		return instance != null ? instance.getClass() : null;
 	}
 
 }
--- src/test/java/org/apache/commons/validator/routines/UrlValidatorUtilTest.java
+++ src/test/java/org/apache/commons/validator/routines/UrlValidatorUtilTest.java
@@ -1,16 +1,63 @@
 package org.apache.commons.validator.routines;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Member;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import com.google.common.reflect.Reflection;
 
 import io.github.toolfactory.narcissus.Narcissus;
 import javassist.util.proxy.MethodHandler;
 import javassist.util.proxy.ProxyUtil;
 
 class UrlValidatorUtilTest {
+
+	private static Method METHOD_FILTER, METHOD_GET_CLASS = null;
+
+	@BeforeAll
+	static void beforeAll() throws NoSuchMethodException {
+		//
+		final Class<?> clz = UrlValidatorUtil.class;
+		//
+		(METHOD_FILTER = clz.getDeclaredMethod("filter", Stream.class, Predicate.class)).setAccessible(true);
+		//
+		(METHOD_GET_CLASS = clz.getDeclaredMethod("getClass", Object.class)).setAccessible(true);
+		//
+	}
+
+	private static class IH implements InvocationHandler {
+
+		public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+			//
+			final String name = getName(method);
+			//
+			if (proxy instanceof Stream) {
+				//
+				if (Objects.equals(name, "filter")) {
+					//
+					return proxy;
+					//
+				} // if
+					//
+			} // if
+				//
+			throw new Throwable(name);
+			//
+		}
+
+	}
 
 	private static class MH implements MethodHandler {
 
@@ -20,7 +67,7 @@ class UrlValidatorUtilTest {
 		public Object invoke(final Object self, final Method thisMethod, final Method proceed, final Object[] args)
 				throws Throwable {
 			//
-			final String methodName = thisMethod != null ? thisMethod.getName() : null;
+			final String methodName = getName(thisMethod);
 			//
 			if (self instanceof UrlValidator && Objects.equals(methodName, "isValid")) {
 				//
@@ -32,6 +79,55 @@ class UrlValidatorUtilTest {
 			//
 		}
 
+	}
+
+	private static String getName(final Member instance) {
+		return instance != null ? instance.getName() : null;
+	}
+
+	@Test
+	void testNull() {
+		//
+		final Method[] ms = UrlValidatorUtil.class.getDeclaredMethods();
+		//
+		Method m = null;
+		//
+		Object result = null;
+		//
+		String toString = null;
+		//
+		for (int i = 0; ms != null && i < ms.length; i++) {
+			//
+			if ((m = ArrayUtils.get(ms, i)) == null || m.isSynthetic() || !Modifier.isStatic(m.getModifiers())) {
+				//
+				continue;
+				//
+			} // if
+				//
+			result = Narcissus.invokeStaticMethod(m, toArray(Collections.nCopies(m.getParameterCount(), null)));
+			//
+			toString = m.toString();
+			//
+			if (isPrimitive(m.getReturnType())) {
+				//
+				Assertions.assertNotNull(result, toString);
+				//
+			} else {
+				//
+				Assertions.assertNull(result, toString);
+				//
+			} // if
+				//
+		} // for
+			//
+	}
+
+	private static boolean isPrimitive(final Class<?> instance) {
+		return instance != null && instance.isPrimitive();
+	}
+
+	private static Object[] toArray(final Collection<?> instance) {
+		return instance != null ? instance.toArray() : null;
 	}
 
 	@Test
@@ -46,12 +142,62 @@ class UrlValidatorUtilTest {
 		//
 		mh.isValid = Boolean.TRUE;
 		//
-		Assertions.assertTrue(UrlValidatorUtil.isValid(ProxyUtil.createProxy(UrlValidator.class, mh), null));
+		final UrlValidator urlValidator = ProxyUtil.createProxy(UrlValidator.class, mh);
+		//
+		Assertions.assertTrue(UrlValidatorUtil.isValid(urlValidator, null));
+		//
+		Assertions.assertFalse(
+				UrlValidatorUtil.isValid(urlValidator, cast(String.class, Narcissus.allocateInstance(String.class))));
 		//
 	}
 
 	private static <T> T cast(final Class<T> clz, final Object instance) {
 		return clz != null && clz.isInstance(instance) ? clz.cast(instance) : null;
+	}
+
+	@Test
+	void testFilter() throws Throwable {
+		//
+		Stream<?> stream = Stream.empty();
+		//
+		Assertions.assertSame(stream, filter(stream, null));
+		//
+		Assertions.assertSame(stream = Reflection.newProxy(Stream.class, new IH()), filter(stream, null));
+		//
+	}
+
+	private static <T> Stream<T> filter(final Stream<T> instance, final Predicate<? super T> predicate)
+			throws Throwable {
+		try {
+			final Object obj = invoke(METHOD_FILTER, null, instance, predicate);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Stream s) {
+				return s;
+			}
+			throw new Throwable(Objects.toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	private static Class<?> getClass(final Object instance) throws Throwable {
+		try {
+			final Object obj = invoke(METHOD_GET_CLASS, null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof Class c) {
+				return c;
+			}
+			throw new Throwable(Objects.toString(getClass(obj)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	private static Object invoke(final Method method, final Object instance, final Object... args)
+			throws IllegalAccessException, InvocationTargetException {
+		return method != null ? method.invoke(instance, args) : null;
 	}
 
 }
