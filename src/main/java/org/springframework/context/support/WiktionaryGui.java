16330275987f5263b312b1987543335da7ad3254
lau-ka-ho <asdf913@gmail.com>
2026-03-17 19:58:28 +0900

    Apply rule ""@NonNull" values should not be set to null"

--- src/main/java/org/springframework/context/support/WiktionaryGui.java
+++ src/main/java/org/springframework/context/support/WiktionaryGui.java
@@ -427,6 +427,7 @@ public class WiktionaryGui extends JPanel implements InitializingBean, ActionLis
 			return hiraganaImage;
 		}
 
+		@Nullable
 		private static byte[] getHiraganaImage(@Nullable final WiktionaryEntry instance) {
 			return instance != null ? instance.getHiraganaImage() : null;
 		}
