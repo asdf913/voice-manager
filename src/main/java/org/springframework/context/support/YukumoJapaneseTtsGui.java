00a5eb2c38b17a207302af54c805bd9ef3270009
lau-ka-ho <asdf913@gmail.com>
2026-05-05 16:28:06 +0900

    Add "org.springframework.context.support.YukumoJapaneseTtsGui" class

---
+++ src/main/java/org/springframework/context/support/YukumoJapaneseTtsGui.java
@@ -0,0 +1,286 @@
+package org.springframework.context.support;
+
+import java.awt.GraphicsEnvironment;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+import javax.swing.text.JTextComponent;
+
+import org.apache.commons.collections4.IterableUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.function.FailableFunctionUtil;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerUtil;
+import org.springframework.beans.factory.InitializingBean;
+
+import com.j256.simplemagic.ContentInfo;
+import com.j256.simplemagic.ContentInfoUtil;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserTypeUtil;
+import com.microsoft.playwright.BrowserUtil;
+import com.microsoft.playwright.ElementHandle;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.PageUtil;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.PlaywrightUtil;
+
+import io.github.toolfactory.narcissus.Narcissus;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+import javazoom.jl.player.PlayerUtil;
+import net.miginfocom.swing.MigLayout;
+
+public class YukumoJapaneseTtsGui extends JPanel implements InitializingBean, ActionListener {
+
+	private static final long serialVersionUID = 8233800467364858829L;
+
+	private static final Logger LOG = LoggerFactory.getLogger(YukumoJapaneseTtsGui.class);
+
+	private static final String URL = "https://www.yukumo.net";
+
+	private JTextComponent tfText = null;
+
+	private AbstractButton btnPlay, btnDownload = null;
+
+	private YukumoJapaneseTtsGui() {
+		//
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		//
+		setLayout(new MigLayout());
+		//
+		if (Narcissus.getField(this, Narcissus.findField(getClass(), "component")) == null) {
+			//
+			return;
+			//
+		} // if
+			//
+		add(new JLabel("Text"));
+		//
+		add(tfText = new JTextField(), String.format("wmin %1$s", 100));
+		//
+		add(btnPlay = new JButton("Play"));
+		//
+		add(btnDownload = new JButton("Download"), "wrap");
+		//
+		Util.forEach(
+				Util.map(
+						Util.filter(Util.stream(FieldUtils.getAllFieldsList(getClass())),
+								f -> Util.isAssignableFrom(AbstractButton.class, Util.getType(f))),
+						f -> Util.cast(AbstractButton.class, Narcissus.getField(this, f))),
+				x -> Util.addActionListener(x, this));
+		//
+	}
+
+	@Override
+	public void actionPerformed(final ActionEvent evt) {
+		//
+		final Object source = Util.getSource(evt);
+		//
+		if (Objects.equals(source, btnPlay)) {
+			//
+			try (final Playwright playwright = Playwright.create();
+					final Browser browser = BrowserTypeUtil.launch(PlaywrightUtil.chromium(playwright));
+					final Page page = BrowserUtil.newPage(browser)) {
+				//
+				if (page != null) {
+					//
+					page.onRequest(x -> {
+						//
+						if (x != null && Objects.equals(x.resourceType(), "media")) {
+							//
+							byte[] bs = null;
+							//
+							try (final InputStream is = Util.openStream(new URL(x.url()))) {
+								//
+								bs = IOUtils.toByteArray(is);
+								//
+							} catch (final IOException e) {
+								//
+								LoggerUtil.error(LOG, e.getMessage(), e);
+								//
+							} // try
+								//
+							final ContentInfo ci = testAndApply(Objects::nonNull, bs,
+									y -> new ContentInfoUtil().findMatch(y), null);
+							//
+							if (ci != null && Objects.equals(ci.getMessage(),
+									"Audio file with ID3 version 2.4, MP3 encoding")) {
+								//
+								try (final InputStream is = testAndApply(Objects::nonNull, bs,
+										ByteArrayInputStream::new, null)) {
+									//
+									PlayerUtil.play(testAndApply(Objects::nonNull, is, Player::new, null));
+									//
+								} catch (final IOException | JavaLayerException e) {
+									//
+									LoggerUtil.error(LOG, e.getMessage(), e);
+									//
+								} // try
+									//
+							} // if
+								//
+						} // if
+							//
+					});
+					//
+				} // if
+					//
+				clickPlayButton(URL, page, Util.getText(tfText));
+				//
+			} // try
+				//
+		} else if (Objects.equals(source, btnDownload)) {
+			//
+			try (final Playwright playwright = Playwright.create();
+					final Browser browser = BrowserTypeUtil.launch(PlaywrightUtil.chromium(playwright));
+					final Page page = BrowserUtil.newPage(browser)) {
+				//
+				final String text = Util.getText(tfText);
+				//
+				if (page != null) {
+					//
+					page.onRequest(x -> {
+						//
+						if (x != null && Objects.equals(x.resourceType(), "media")) {
+							//
+							byte[] bs = null;
+							//
+							try (final InputStream is = Util.openStream(new URL(x.url()))) {
+								//
+								bs = IOUtils.toByteArray(is);
+								//
+							} catch (final IOException e) {
+								//
+								LoggerUtil.error(LOG, e.getMessage(), e);
+								//
+							} // try
+								//
+							final ContentInfo ci = testAndApply(Objects::nonNull, bs,
+									y -> new ContentInfoUtil().findMatch(y), null);
+							//
+							if (ci != null && Objects.equals(ci.getMessage(),
+									"Audio file with ID3 version 2.4, MP3 encoding")) {
+								//
+								final JFileChooser jfc = new JFileChooser();
+								//
+								final String[] fileExtensions = ci.getFileExtensions();
+								//
+								jfc.setSelectedFile(new File(".",
+										String.join(".", text,
+												fileExtensions != null && fileExtensions.length == 1
+														? ArrayUtils.get(fileExtensions, 0)
+														: "mp3")));
+								//
+								if (jfc.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+									//
+									try {
+										//
+										FileUtils.writeByteArrayToFile(jfc.getSelectedFile(), bs);
+										//
+									} catch (final IOException e) {
+										//
+										LoggerUtil.error(LOG, e.getMessage(), e);
+										//
+									} // try
+										//
+								} // if
+									//
+							} // if
+								//
+						} // if
+							//
+					});
+					//
+				} // if
+					//
+				clickPlayButton(URL, page, text);
+				//
+			} // try
+				//
+		} // if
+			//
+	}
+
+	private static void clickPlayButton(final String url, final Page page, final String text) {
+		//
+		PageUtil.navigate(page, url);
+		//
+		ElementHandle element = testAndApply(x -> IterableUtils.size(x) == 1,
+				PageUtil.querySelectorAll(page, "input[type=\"text\"]"), x -> IterableUtils.get(x, 0), null);
+		//
+		if (element != null && text != null) {
+			//
+			element.fill(text);
+			//
+		} // if
+			//
+		if ((element = testAndApply(x -> IterableUtils.size(x) == 1,
+				PageUtil.querySelectorAll(page, "button:has(i.fa-play)"), x -> IterableUtils.get(x, 0),
+				null)) != null) {
+			//
+			element.click();
+			//
+		} // if
+			//
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, FailableFunction<T, R, E> functionFalse) throws E {
+		return Util.test(predicate, value) ? FailableFunctionUtil.apply(functionTrue, value)
+				: FailableFunctionUtil.apply(functionFalse, value);
+	}
+
+	public static void main(final String[] args) throws Exception {
+		//
+		final YukumoJapaneseTtsGui instance = new YukumoJapaneseTtsGui();
+		//
+		instance.afterPropertiesSet();
+		//
+		final JFrame jFrame = !GraphicsEnvironment.isHeadless() ? new JFrame() : null;
+		//
+		if (jFrame != null) {
+			//
+			jFrame.add(instance);
+			//
+			jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+			//
+			jFrame.pack();
+			//
+			if (!isTestMode()) {
+				//
+				jFrame.setVisible(true);
+				//
+			} // if
+				//
+		} // if
+			//
+	}
+
+	private static boolean isTestMode() {
+		return Util.forName("org.junit.jupiter.api.Test") != null;
+	}
+
+}
---
+++ src/test/java/org/springframework/context/support/YukumoJapaneseTtsGuiTest.java
@@ -0,0 +1,354 @@
+package org.springframework.context.support;
+
+import java.awt.GraphicsEnvironment;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
+
+import org.apache.commons.collections4.IterableUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Strings;
+import org.apache.commons.lang3.StringsUtil;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.oxbow.swingbits.util.OperatingSystem;
+import org.oxbow.swingbits.util.OperatingSystemUtil;
+
+import com.google.common.reflect.Reflection;
+import com.microsoft.playwright.Page;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+class YukumoJapaneseTtsGuiTest {
+
+	private static Method METHOD_TEST_AND_APPLY = null;
+
+	@BeforeAll
+	static void beforeAll() throws NoSuchMethodException {
+		//
+		(METHOD_TEST_AND_APPLY = Util.getDeclaredMethod(JapanDictGui.class, "testAndApply", Predicate.class,
+				Object.class, FailableFunction.class, FailableFunction.class)).setAccessible(true);
+		//
+	}
+
+	private static class IH implements InvocationHandler {
+
+		private Boolean test = null;
+
+		public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+			//
+			if (Objects.equals(Util.getReturnType(method), Void.TYPE)) {
+				//
+				return null;
+				//
+			} // if
+				//
+			final String name = Util.getName(method);
+			//
+			if (proxy instanceof Predicate && Objects.equals(name, "test")) {
+				//
+				return test;
+				//
+			} else if (proxy instanceof FailableFunction && Objects.equals(name, "apply")) {
+				//
+				return null;
+				//
+			} else if (proxy instanceof Page && Util.contains(Arrays.asList("navigate", "querySelectorAll"), name)) {
+				//
+				return null;
+				//
+			} // if
+				//
+			throw new Throwable(name);
+			//
+		}
+
+	}
+
+	private static <T, R, E extends Exception> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse)
+			throws Throwable {
+		try {
+			return (R) METHOD_TEST_AND_APPLY.invoke(null, predicate, value, functionTrue, functionFalse);
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	private static Process start(final ProcessBuilder instance) throws IOException {
+		return instance != null ? instance.start() : null;
+	}
+
+	private static InputStream getInputStream(final Process instance) {
+		return instance != null ? instance.getInputStream() : null;
+	}
+
+	private YukumoJapaneseTtsGui instance = null;
+
+	private IH ih = null;
+
+	private OperatingSystem operatingSystem = null;
+
+	private boolean nmcliExists = false;
+
+	private String connectivity = null;
+
+	private final boolean isHeadless = GraphicsEnvironment.isHeadless();
+
+	@BeforeEach
+	void beforeEach() throws Throwable {
+		//
+		instance = Util.cast(YukumoJapaneseTtsGui.class, Narcissus.allocateInstance(YukumoJapaneseTtsGui.class));
+		//
+		ih = new IH();
+		//
+		if (Objects.equals(operatingSystem = OperatingSystemUtil.getOperatingSystem(), OperatingSystem.LINUX)) {
+			//
+			final Charset charset = StandardCharsets.UTF_8;
+			//
+			try (final InputStream is = getInputStream(start(new ProcessBuilder(new String[] { "which", "nmcli" })))) {
+				//
+				nmcliExists = Util.exists(testAndApply(Objects::nonNull,
+						StringUtils.trim(IOUtils.toString(is, charset)), File::new, null));
+				//
+			} // try
+				//
+			try (final InputStream is = getInputStream(
+					start(nmcliExists ? new ProcessBuilder(new String[] { "nmcli", "-mode", "multiline", "general" })
+							: null))) {
+				//
+				final Collection<String> collection = testAndApply(Objects::nonNull, is,
+						x -> IOUtils.readLines(x, charset), null);
+				//
+				connectivity = Util
+						.toString(testAndApply(
+								x -> IterableUtils
+										.size(x) == 1,
+								Util.toList(Util.map(
+										Util.filter(Util.stream(collection),
+												x -> StringsUtil.startsWith(Strings.CI, x, "CONNECTIVITY:")),
+										x -> StringUtils.trim(StringUtils.substringAfter(x, ':')))),
+								x -> IterableUtils.get(x, 0), null));
+				//
+			} // try
+				//
+		} // if
+			//
+	}
+
+	@Test
+	void testNull() throws Throwable {
+		//
+		final Method[] ms = Util.getDeclaredMethods(YukumoJapaneseTtsGui.class);
+		//
+		Method m = null;
+		//
+		Object[] os = null;
+		//
+		String toString = null;
+		//
+		Object result = null;
+		//
+		Class<?>[] parameterTypes = null;
+		//
+		for (int i = 0; ms != null && i < ms.length; i++) {
+			//
+			if ((m = ArrayUtils.get(ms, i)) == null || m.isSynthetic()
+					|| (parameterTypes = m.getParameterTypes()) == null) {
+				//
+				continue;
+				//
+			} // if
+				//
+			os = Util.toArray(Collections.nCopies(parameterTypes.length, null));
+			//
+			toString = Util.toString(m);
+			//
+			if (Util.isStatic(m)) {
+				//
+				result = Narcissus.invokeStaticMethod(m, os);
+				//
+				if (Objects.equals(Util.getReturnType(m), Boolean.TYPE)) {
+					//
+					Assertions.assertNotNull(result, toString);
+					//
+				} else {
+					//
+					Assertions.assertNull(result, toString);
+					//
+				} // if
+					//
+			} else {
+				//
+				if (Objects.equals(Util.getName(m), "actionPerformed")
+						&& Arrays.equals(parameterTypes, new Class<?>[] { ActionEvent.class })
+						&& Objects.equals(operatingSystem, OperatingSystem.LINUX)
+						&& !StringsUtil.equals(Strings.CI, connectivity, "full") && nmcliExists) {
+					//
+					final Method m1 = m;
+					//
+					final Object[] os1 = os;
+					//
+					Assertions.assertThrows(RuntimeException.class, () -> Narcissus.invokeMethod(instance, m1, os1));
+					//
+					continue;
+					//
+				} // if
+					//
+				Assertions.assertNull(Narcissus.invokeMethod(instance, m, os), toString);
+				//
+			} // if
+				//
+		} // for
+			//
+	}
+
+	@Test
+	void testNonNull() throws Throwable {
+		//
+		final Method[] ms = Util.getDeclaredMethods(YukumoJapaneseTtsGui.class);
+		//
+		Method m = null;
+		//
+		Class<?>[] parameterTypes = null;
+		//
+		Class<?> parameterType = null;
+		//
+		Collection<Object> collection = null;
+		//
+		if (ih != null) {
+			//
+			ih.test = Boolean.FALSE;
+			//
+		} // if
+			//
+		Object[] os = null;
+		//
+		String toString = null;
+		//
+		Object result = null;
+		//
+		for (int i = 0; ms != null && i < ms.length; i++) {
+			//
+			if ((m = ArrayUtils.get(ms, i)) == null || m.isSynthetic()
+					|| (parameterTypes = m.getParameterTypes()) == null) {
+				//
+				continue;
+				//
+			} // if
+				//
+			Util.clear(collection = ObjectUtils.getIfNull(collection, ArrayList::new));
+			//
+			for (int j = 0; j < parameterTypes.length; j++) {
+				//
+				if (isInterface(parameterType = ArrayUtils.get(parameterTypes, j))) {
+					//
+					Util.add(collection, Reflection.newProxy(parameterType, ih));
+					//
+				} else if (isArray(parameterType) && parameterType != null) {
+					//
+					Util.add(collection, Array.newInstance(parameterType.getComponentType(), 0));
+					//
+				} else {
+					//
+					Util.add(collection, Narcissus.allocateInstance(parameterType));
+					//
+				} // if
+					//
+			} // for
+				//
+			os = Util.toArray(collection);
+			//
+			toString = Objects.toString(m);
+			//
+			if (Util.isStatic(m)) {
+				//
+				result = Narcissus.invokeStaticMethod(m, os);
+				//
+				if (Objects.equals(Util.getReturnType(m), Boolean.TYPE)) {
+					//
+					Assertions.assertNotNull(result, toString);
+					//
+				} else {
+					//
+					Assertions.assertNull(result, toString);
+					//
+				} // if
+					//
+			} else {
+				//
+				if (Objects.equals(Util.getName(m), "actionPerformed")
+						&& Arrays.equals(parameterTypes, new Class<?>[] { ActionEvent.class })
+						&& Objects.equals(operatingSystem, OperatingSystem.LINUX)
+						&& !StringsUtil.equals(Strings.CI, connectivity, "full") && nmcliExists) {
+					//
+					final Method m1 = m;
+					//
+					final Object[] os1 = os;
+					//
+					Assertions.assertThrows(RuntimeException.class, () -> Narcissus.invokeMethod(instance, m1, os1));
+					//
+					continue;
+					//
+				} // if
+					//
+				Assertions.assertNull(Narcissus.invokeMethod(instance, m, os), toString);
+				//
+			} // if
+				//
+		} // for
+			//
+	}
+
+	private static boolean isInterface(final Class<?> instance) {
+		return instance != null && instance.isInterface();
+	}
+
+	private static boolean isArray(final Class<?> instance) {
+		return instance != null && instance.isArray();
+	}
+
+	@Test
+	void testActionPerformed() throws IllegalAccessException {
+		//
+		if (instance == null || (Objects.equals(operatingSystem, OperatingSystem.LINUX)
+				&& Boolean.logicalAnd(!StringsUtil.equals(Strings.CI, connectivity, "full"), nmcliExists))) {
+			//
+			return;
+			//
+		} // if
+			//
+			// btnDownload
+			//
+		final AbstractButton btnDownload = new JButton();
+		//
+		FieldUtils.writeDeclaredField(instance, "btnDownload", btnDownload, true);
+		//
+		Assertions.assertDoesNotThrow(() -> instance.actionPerformed(new ActionEvent(btnDownload, 0, null)));
+		//
+	}
+
+}
