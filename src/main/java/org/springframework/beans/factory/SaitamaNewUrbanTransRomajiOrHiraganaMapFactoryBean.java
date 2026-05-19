9afa5e441b3340908aa0b520a3290c58ae4a3f1d
lau-ka-ho <asdf913@gmail.com>
2026-05-19 14:32:37 +0900

    Apply rule "Unnecessary imports should be removed"

--- src/main/java/org/springframework/beans/factory/SaitamaNewUrbanTransRomajiOrHiraganaMapFactoryBean.java
+++ src/main/java/org/springframework/beans/factory/SaitamaNewUrbanTransRomajiOrHiraganaMapFactoryBean.java
@@ -5,7 +5,6 @@ import java.lang.reflect.Field;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
